ae6d6b9bc88b36719e0a456ed4cb637d906671ed
dpanozzo <devnull@localhost>
2011-09-03 12:56:02 +0000

    fixed compilation errors in tt and edge topology, added basic example of usage

--- tt.h
+++ tt.h
@@ -8,14 +8,17 @@
 
 #include <Eigen/Core>
 #include <string>
+#include <ismanifold.h>
+
+#include <vector>
 
 namespace igl 
 {
     // Compute triangle-triangle adjacency
-    void tt(Eigen::MatrixXd& V, Eigen::MatrixXd& F, Eigen::MatrixXd& TT)
+    void tt(Eigen::MatrixXd& V, Eigen::MatrixXi& F, Eigen::MatrixXi& TT)
     {
-        assert(isManifold());
-        vector<vector<int> > TTT;
+        assert(igl::isManifold(V,F));
+        std::vector<std::vector<int> > TTT;
         for(int f=0;f<F.rows();++f)
             for (int i=0;i<3;++i)
             {
@@ -23,18 +26,18 @@ namespace igl
                 int v1 = F(f,i);
                 int v2 = F(f,(i+1)%3);
                 if (v1 > v2) std::swap(v1,v2);
-                vector<int> r(4);
+                std::vector<int> r(4);
                 r[0] = v1; r[1] = v2;
                 r[2] = f;  r[3] = i;
                 TTT.push_back(r);
             }
         std::sort(TTT.begin(),TTT.end());
-        TT = MatrixXi::Constant((int)(F.rows()),3,-1);
+        TT = Eigen::MatrixXi::Constant((int)(F.rows()),3,-1);
         
         for(int i=1;i<TTT.size();++i)
         {
-            vector<int>& r1 = TTT[i-1];
-            vector<int>& r2 = TTT[i];
+            std::vector<int>& r1 = TTT[i-1];
+            std::vector<int>& r2 = TTT[i];
             if ((r1[0] == r2[0]) && (r1[1] == r2[1]))
             {
                 TT(r1[2],r1[3]) = r2[2];
