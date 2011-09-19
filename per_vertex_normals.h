2f788a9fe6901035ddc419d3c6cf6199d3f91dfd
jalec <devnull@localhost>
2011-09-19 07:07:58 +0000

    VBO (Vertex buffer object) handlers for eigen style V and F, normalize vector lists, compute normals

---
+++ per_vertex_normals.h
@@ -0,0 +1,54 @@
+#ifndef IGL_PER_VERTEX_NORMALS_H
+#define IGL_PER_VERTEX_NORMALS_H
+#include <Eigen/Core>
+namespace igl
+{
+  // Compute vertex normals via vertex position list, face list
+  // Inputs:
+  //   V  #V by 3 eigen Matrix of mesh vertex 3D positions
+  //   F  #F by 3 eigne Matrix of face (triangle) indices
+  // Output:
+  //   N  #V by 3 eigen Matrix of mesh vertex 3D normals
+  void per_vertex_normals(
+    const Eigen::MatrixXd & V,
+    const Eigen::MatrixXi & F,
+    Eigen::MatrixXd & N);
+}
+
+// Implementation
+#include "per_face_normals.h"
+#include "normalize_rows.h"
+
+void igl::per_vertex_normals(
+  const Eigen::MatrixXd & V,
+  const Eigen::MatrixXi & F,
+  Eigen::MatrixXd & N)
+{
+  Eigen::MatrixXd PFN;
+  igl::per_face_normals(V,F,PFN);
+
+  N = Eigen::MatrixXd(V.rows(),3);
+  // loop over vertices, setting normalize to 0
+  for(int i = 0; i < N.rows();i++)
+  {
+    N(i,0) = 0;
+    N(i,1) = 0;
+    N(i,2) = 0;
+  }
+
+  // loop over faces
+  for(int i = 0; i < F.rows();i++)
+  {
+    // throw normal at each corner
+    for(int j = 0; j < 3;j++)
+    {
+      N(F(i,j),0) += PFN(i,0);
+      N(F(i,j),1) += PFN(i,1);
+      N(F(i,j),2) += PFN(i,2);
+    }
+  }
+  // normalize each row
+  igl::normalize_rows(N,N);
+}
+
+#endif
