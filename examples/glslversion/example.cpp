fa03862cee375f8e43be196d0128fbdbf9929a57
jalec <devnull@localhost>
2011-09-28 10:11:55 +0000

    dmat matrix file format reader, writer and example sort, reorder vectors and eigen (dynamic) matrices like matlabs sort (with example) function to retrieve shader uniform names and locations, function to report open gl errors to stderr example that prints opengl version and glsl version,
    Former-commit-id: 729936710fd24a26f7467622109c2c8c8110cd0a

---
+++ examples/glslversion/example.cpp
@@ -0,0 +1,18 @@
+#include <GLUT/glut.h>
+#include <cstdio>
+
+int main(int argc,char * argv[])
+{
+  // Make GLUT window just to get a valid OpenGL context
+  glutInit(&argc,argv);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+  glutInitWindowSize(640, 480);
+  glutCreateWindow("Dummy");
+  glutCreateMenu(NULL);
+
+  printf("GL_VERSION: %s\n", 
+    glGetString(GL_VERSION));
+  printf("GL_SHADING_LANGUAGE_VERSION: %s\n", 
+    glGetString(GL_SHADING_LANGUAGE_VERSION));
+  return 0;
+}
---
+++ readDMAT.h
@@ -0,0 +1,91 @@
+#ifndef IGL_READDMAT_H
+#define IGL_READDMAT_H
+// .dmat is a simple ascii matrix file type, defined as follows. The first line
+// is always:
+// <#columns> <#rows>
+// Then the coefficients of the matrix are given separated by whitespace with
+// columns running fastest.
+//
+// Example:
+//   The matrix m = [1 2 3; 4 5 6];
+//   corresponds to a .dmat file containing:
+//   3 2
+//   1 4 2 5 3 6
+#include <Eigen/Core>
+#include <string>
+namespace igl
+{
+  // Read a matrix from an ascii dmat file
+  //
+  // Inputs:
+  //   file_name  path to .dmat file
+  // Outputs:
+  //   W  eigen matrix containing read-in coefficients
+  // Returns true on success, false on error
+  //
+  inline bool readDMAT(const std::string file_name, Eigen::MatrixXd & W);
+}
+
+// Implementation
+#include "verbose.h"
+#include <cstdio>
+
+inline bool igl::readDMAT(const std::string file_name, Eigen::MatrixXd & W)
+{
+  FILE * fp = fopen(file_name.c_str(),"r");
+  if(fp == NULL)
+  {
+    fclose(fp);
+    fprintf(stderr,"IOError: readDMAT() could not open %s...\n",file_name.c_str());
+    return false; 
+  }
+  // first line contains number of rows and number of columns
+  int num_cols, num_rows;
+  int res = fscanf(fp,"%d %d\n",&num_cols,&num_rows);
+  if(res != 2)
+  {
+    fclose(fp);
+    fprintf(stderr,"IOError: readDMAT() first row should be [num cols] [num rows]...\n");
+    return false;
+  }
+  verbose("Number of rows: %d\n",num_rows);
+  verbose("Number of cols: %d\n",num_cols);
+
+  // check that number of columns and rows are sane
+  if(num_cols < 0)
+  {
+    fclose(fp);
+    fprintf(stderr,"IOError: readDMAT() number of columns %d < 0\n",num_cols);
+    return false;
+  }
+  if(num_rows < 0)
+  {
+    fclose(fp);
+    fprintf(stderr,"IOError: readDMAT() number of rows %d < 0\n",num_rows);
+    return false;
+  }
+
+  // Resize output to fit matrix
+  W.resize(num_rows,num_cols);
+
+  // Loop over columns slowly
+  for(int j = 0;j < num_cols;j++)
+  {
+    // loop over rows (down columns) quickly
+    for(int i = 0;i < num_rows;i++)
+    {
+      if(fscanf(fp," %lg",&W(i,j)) != 1)
+      {
+        fclose(fp);
+        fprintf(
+          stderr,
+          "IOError: readDMAT() bad format after reading %d entries\n",
+          j*num_rows + i);
+        return false;
+      }
+    }
+  }
+  fclose(fp);
+  return true;
+}
+#endif
