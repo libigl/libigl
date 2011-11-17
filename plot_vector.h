514e61136a941b0809b5b3d38e32c92de4d6ba73
dpanozzo <devnull@localhost>
2011-11-17 20:27:33 +0000

    fixed bug in adjacency_list for open meshes
    Former-commit-id: be31eda316517296ca37b418edd824d8ba60b3c4

--- plot_vector.h
+++ plot_vector.h
@@ -10,6 +10,18 @@ namespace igl
       cerr << v[i] << " ";
     cerr << endl;
   }
+
+  template <typename T>
+  inline void plot_vector( vector< vector<T> >& v)
+  {
+    for (int i=0; i<v.size(); ++i)
+    {
+      for (int j=0; j<v[i].size(); ++j)
+        cerr << v[i][j] << " ";
+      cerr << endl;
+    }
+  }
+
 }
 
 #endif
