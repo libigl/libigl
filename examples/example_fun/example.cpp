3d66bab1e7628d1b0df8bcb9b78edf8eab5244ce
jalec <devnull@localhost>
2012-01-25 10:23:43 +0000

    example fun cpp

---
+++ examples/example_fun/example.cpp
@@ -0,0 +1,12 @@
+#include "example_fun.h"
+using namespace igl;
+using namespace std;
+
+int main(int argc, char * argv[])
+{
+  double d = 4.4;
+  example_fun(d);
+  int i = 4;
+  example_fun(i);
+  return 0;
+}
