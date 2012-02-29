778c042ccfe1fe048d8bfdb9270eac3dcaf4bbd4
jalec <devnull@localhost>
2012-02-29 15:07:01 +0000

    explicit templates matlab workspace and last commit before hunting bug in alec's skinning
    Former-commit-id: fd074032e762ace3333801b0fb8488fdb9cec8e1

---
+++ examples/MatlabWorkspace/example.cpp
@@ -0,0 +1,32 @@
+#include <Eigen/Core>
+
+#include <igl/readDMAT.h>
+#include <cstdio>
+
+#ifndef IGL_HEADER_ONLY
+#  define IGL_HEADER_ONLY
+#  define IGL_HEADER_ONLY_WAS_NOT_DEFINED
+#endif
+#include <igl/MatlabWorkspace.h>
+#include <igl/on_boundary.h>
+#ifdef IGL_HEADER_ONLY_WAS_NOT_DEFINED
+#  undef IGL_HEADER_ONLY
+#endif
+
+int main(int argc, char * argv[])
+{
+  using namespace igl;
+  using namespace Eigen;
+  if(argc <= 2)
+  {
+    printf("USAGE:\n  ./example [input path] [output path]\n");
+    return 1;
+  }
+  MatrixXd M;
+  readDMAT(argv[1],M);
+  MatlabWorkspace mat;
+  mat.save(M,"M");
+  mat.write(argv[2]);
+
+  return 0;
+}
