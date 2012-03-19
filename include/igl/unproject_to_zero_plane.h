1d90da86f8704414b4e86fc62a94a2f440c91b8c
jalec <devnull@localhost>
2012-03-19 16:22:14 +0000

    unproject to zero plane
    Former-commit-id: f04453a7abdb35dbfc03c77aabcf7ca5c5e47dba

--- include/igl/unproject_to_zero_plane.h
+++ include/igl/unproject_to_zero_plane.h
@@ -1,26 +1,27 @@
-#ifndef IGL_UNPROJECT_H
-#define IGL_UNPROJECT_H
+#ifndef IGL_UNPROJECT_TO_ZERO_PLANE_H
+#define IGL_UNPROJECT_TO_ZERO_PLANE_H
 #include "igl_inline.h"
 namespace igl
 {
   // Wrapper for gluUnproject that uses the current GL_MODELVIEW_MATRIX,
-  // GL_PROJECTION_MATRIX, and GL_VIEWPORT
+  // GL_PROJECTION_MATRIX, and GL_VIEWPORT to unproject a screen postion
+  // (winX,winY) to a 3d location at same depth as the current origin.
   // Inputs:
   //   win*  screen space x, y, and z coordinates respectively
   // Outputs:
   //   obj*  pointers to 3D objects' x, y, and z coordinates respectively
   // Returns return value of gluUnProject call
-  IGL_INLINE int unproject(
+  IGL_INLINE int unproject_to_zero_plane(
     const double winX,
     const double winY,
-    const double winZ,
     double* objX,
     double* objY,
     double* objZ);
 }
 
 #ifdef IGL_HEADER_ONLY
-#  include "unproject.cpp"
+#  include "unproject_to_zero_plane.cpp"
 #endif
 
 #endif
+
