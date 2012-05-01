f4d352d143dedd5aa0361628cead3c82a8ea60e8
jalec <devnull@localhost>
2012-05-01 06:47:33 +0000

    render to tga

---
+++ include/igl/render_to_tga.h
@@ -0,0 +1,26 @@
+#ifndef IGL_RENDER_TO_TGA_H
+#define IGL_RENDER_TO_TGA_H
+#include "igl_inline.h"
+
+#include <string>
+namespace igl
+{
+  // Render current open GL image to .tga file
+  // Inputs:
+  //   tga_file  path to output .tga file
+  //   width  width of scene and resulting image
+  //   height height of scene and resulting image
+  ///  alpha  whether to include alpha channel
+  // Returns true only if no errors occured
+  IGL_INLINE bool render_to_tga(
+    const std::string tga_file,
+    const int width,
+    const int height,
+    const bool alpha);
+}
+
+#ifdef IGL_HEADER_ONLY
+#  include "render_to_tga.cpp"
+#endif
+
+#endif
