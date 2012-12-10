82115a689483f7bb0153d3ad7ec71e468dfbdc13
Alec Jacobson (jalec <Alec Jacobson (jalec@inf.ethz.ch)>
2012-12-10 22:53:40 +0100

    generalized boundary_faces and fixed mat4_to_quat
    Former-commit-id: 00ee86081b11a89935ff537778528c6844286ee5

--- include/igl/boundary_faces.cpp
+++ include/igl/boundary_faces.cpp
@@ -16,33 +16,57 @@ IGL_INLINE void igl::boundary_faces(
   using namespace std;
   using namespace igl;
 
+  if(T.size() == 0)
+  {
+    F.clear();
+    return;
+  }
+
+  int simplex_size = T[0].size();
   // Get a list of all faces
-  vector<vector<IntegerF> > allF(T.size()*4,vector<IntegerF>(3));
+  vector<vector<IntegerF> > allF(
+    T.size()*simplex_size,
+    vector<IntegerF>(simplex_size-1));
+
   // Gather faces, loop over tets
   for(int i = 0; i< (int)T.size();i++)
   {
-    assert(T[i].size() == 4);
-    // get face in correct order
-    allF[i*4+0][0] = T[i][1];
-    allF[i*4+0][1] = T[i][3];
-    allF[i*4+0][2] = T[i][2];
-    // get face in correct order
-    allF[i*4+1][0] = T[i][0];
-    allF[i*4+1][1] = T[i][2];
-    allF[i*4+1][2] = T[i][3];
-    // get face in correct order
-    allF[i*4+2][0] = T[i][0];
-    allF[i*4+2][1] = T[i][3];
-    allF[i*4+2][2] = T[i][1];
-    // get face in correct order
-    allF[i*4+3][0] = T[i][0];
-    allF[i*4+3][1] = T[i][1];
-    allF[i*4+3][2] = T[i][2];
+    assert(T[i].size() == simplex_size);
+    switch(simplex_size)
+    {
+      case 4:
+        // get face in correct order
+        allF[i*simplex_size+0][0] = T[i][1];
+        allF[i*simplex_size+0][1] = T[i][3];
+        allF[i*simplex_size+0][2] = T[i][2];
+        // get face in correct order
+        allF[i*simplex_size+1][0] = T[i][0];
+        allF[i*simplex_size+1][1] = T[i][2];
+        allF[i*simplex_size+1][2] = T[i][3];
+        // get face in correct order
+        allF[i*simplex_size+2][0] = T[i][0];
+        allF[i*simplex_size+2][1] = T[i][3];
+        allF[i*simplex_size+2][2] = T[i][1];
+        // get face in correct order
+        allF[i*simplex_size+3][0] = T[i][0];
+        allF[i*simplex_size+3][1] = T[i][1];
+        allF[i*simplex_size+3][2] = T[i][2];
+        break;
+      case 3:
+        allF[i*simplex_size+0][0] = T[i][0];
+        allF[i*simplex_size+0][1] = T[i][1];
+        allF[i*simplex_size+1][0] = T[i][1];
+        allF[i*simplex_size+1][1] = T[i][2];
+        allF[i*simplex_size+2][0] = T[i][2];
+        allF[i*simplex_size+2][1] = T[i][0];
+        break;
+    }
   }
   // Counts
   vector<int> C;
   face_occurences(allF,C);
 
+  // Q: Why not just count the number of ones?
   int twos = (int) count(C.begin(),C.end(),2);
   // Resize output to fit number of ones
   F.resize(allF.size() - twos);
@@ -68,7 +92,7 @@ IGL_INLINE void igl::boundary_faces(
   const Eigen::PlainObjectBase<DerivedT>& T,
   Eigen::PlainObjectBase<DerivedF>& F)
 {
-  assert(T.cols() == 0 || T.cols() == 4);
+  assert(T.cols() == 0 || T.cols() == 4 || T.cols() == 3);
   using namespace std;
   using namespace Eigen;
   using namespace igl;
@@ -79,6 +103,15 @@ IGL_INLINE void igl::boundary_faces(
   boundary_faces(vT,vF);
   list_to_matrix(vF,F);
 }
+
+template <typename DerivedT, typename DerivedF>
+IGL_INLINE Eigen::PlainObjectBase<DerivedF> igl::boundary_faces(
+  const Eigen::PlainObjectBase<DerivedT>& T)
+{
+  Eigen::Matrix<typename DerivedF::Scalar, Eigen::Dynamic, Eigen::Dynamic> F;
+  igl::boundary_faces(T,F);
+  return F;
+}
 #endif
 
 
@@ -86,5 +119,6 @@ IGL_INLINE void igl::boundary_faces(
 // Explicit template specialization
 template void igl::boundary_faces<Eigen::Matrix<int, -1, -1, 0, -1, -1>, Eigen::Matrix<int, -1, -1, 0, -1, -1> >(Eigen::PlainObjectBase<Eigen::Matrix<int, -1, -1, 0, -1, -1> > const&, Eigen::PlainObjectBase<Eigen::Matrix<int, -1, -1, 0, -1, -1> >&);
 template void igl::boundary_faces<int, int>(std::vector<std::vector<int, std::allocator<int> >, std::allocator<std::vector<int, std::allocator<int> > > > const&, std::vector<std::vector<int, std::allocator<int> >, std::allocator<std::vector<int, std::allocator<int> > > >&);
+template Eigen::PlainObjectBase<Eigen::Matrix<int, -1, -1, 0, -1, -1> > igl::boundary_faces(Eigen::PlainObjectBase<Eigen::Matrix<int, -1, -1, 0, -1, -1> > const&);
 #endif
 
--- include/igl/boundary_faces.h
+++ include/igl/boundary_faces.h
@@ -10,15 +10,16 @@
 
 namespace igl
 {
-  // BOUNDARY_FACES Determine boundary faces of tetrahedra stored in T
+  // BOUNDARY_FACES Determine boundary faces (edges) of tetrahedra (triangles)
+  // stored in T
   //
   // Templates:
   //   IntegerT  integer-value: e.g. int
   //   IntegerF  integer-value: e.g. int
   // Input:
-  //  T  tetrahedron index list, m by 4, where m is the number of tetrahedra
+  //  T  tetrahedron (triangle) index list, m by 4 (3), where m is the number of tetrahedra
   // Output:
-  //  F  list of boundary faces, n by 3, where n is the number of boundary faces
+  //  F  list of boundary faces, n by 3 (2), where n is the number of boundary faces
   //
   //
   template <typename IntegerT, typename IntegerF>
@@ -34,6 +35,10 @@ namespace igl
   IGL_INLINE void boundary_faces(
     const Eigen::PlainObjectBase<DerivedT>& T,
     Eigen::PlainObjectBase<DerivedF>& F);
+  // Same as above but returns F
+  template <typename DerivedT, typename DerivedF>
+  IGL_INLINE Eigen::PlainObjectBase<DerivedF> boundary_faces(
+    const Eigen::PlainObjectBase<DerivedT>& T);
 #endif
 }
 
