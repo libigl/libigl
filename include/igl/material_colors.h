3802b91d8f29d8ef976144296aee910e37745370
Alec Jacobson (jalec <Alec Jacobson (jalec@inf.ethz.ch)>
2013-05-12 16:05:03 +0200

    more mat colors

--- include/igl/material_colors.h
+++ include/igl/material_colors.h
@@ -3,6 +3,7 @@
 // Define som constant material colors for use with opengl glMaterialfv
 namespace igl
 {
+  // Gold/Silver used in BBW/MONO/STBS/FAST
   const float GOLD_AMBIENT[4] =   {  51.0/255.0, 43.0/255.0,33.3/255.0,1.0f };
   const float GOLD_DIFFUSE[4] =   { 255.0/255.0,228.0/255.0,58.0/255.0,1.0f };
   const float GOLD_SPECULAR[4] =  { 255.0/255.0,235.0/255.0,80.0/255.0,1.0f };
@@ -20,10 +21,12 @@ namespace igl
   const float FAST_RED_DIFFUSE[4]   = { 255.0f/255.0f,  65.0f/255.0f,  46.0f/255.0f, 1.0f};
   const float FAST_BLUE_DIFFUSE[4]  = { 106.0f/255.0f, 106.0f/255.0f, 255.0f/255.0f, 1.0f};
   const float FAST_GRAY_DIFFUSE[4]  = { 150.0f/255.0f, 150.0f/255.0f, 150.0f/255.0f, 1.0f};
+  // Basic colors
   const float WHITE_AMBIENT[4] =   { 255.0/255.0,255.0/255.0,255.0/255.0,1.0f };
   const float WHITE_DIFFUSE[4] =   { 255.0/255.0,255.0/255.0,255.0/255.0,1.0f };
   const float WHITE_SPECULAR[4] =  { 255.0/255.0,255.0/255.0,255.0/255.0,1.0f };
   const float BBW_POINT_COLOR[4] = {239./255.,213./255.,46./255.,255.0/255.0};
   const float BBW_LINE_COLOR[4] = {106./255.,106./255.,255./255.,255./255.};
+  const float MIDNIGHT_BLUE_DIFFUSE[4]  = { 21.0f/255.0f, 27.0f/255.0f, 84.0f/255.0f, 1.0f};
 }
 #endif
