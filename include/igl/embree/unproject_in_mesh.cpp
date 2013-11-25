a78b1701460a9e50cbf5f87dbec8874b0abe07a2
Alec Jacobson (jalec <Alec Jacobson (jalec@inf.ethz.ch)>
2013-11-25 14:25:22 +0100

    preprend license header to every file

--- include/igl/embree/unproject_in_mesh.cpp
+++ include/igl/embree/unproject_in_mesh.cpp
@@ -1,3 +1,10 @@
+// This file is part of libigl, a simple c++ geometry processing library.
+// 
+// Copyright (C) 2013 Alec Jacobson <alecjacobson@gmail.com>
+// 
+// This Source Code Form is subject to the terms of the Mozilla Public License 
+// v. 2.0. If a copy of the MPL was not distributed with this file, You can 
+// obtain one at http://mozilla.org/MPL/2.0/.
 #include "unproject_in_mesh.h"
 #include "EmbreeIntersector.h"
 #include <igl/unproject.h>
--- include/igl/point_in_poly.cpp
+++ include/igl/point_in_poly.cpp
@@ -1,3 +1,10 @@
+// This file is part of libigl, a simple c++ geometry processing library.
+// 
+// Copyright (C) 2013 Alec Jacobson <alecjacobson@gmail.com>
+// 
+// This Source Code Form is subject to the terms of the Mozilla Public License 
+// v. 2.0. If a copy of the MPL was not distributed with this file, You can 
+// obtain one at http://mozilla.org/MPL/2.0/.
 #include "point_in_poly.h"
 
 bool IGL_INLINE igl::point_in_poly( const std::vector<std::vector<unsigned int > >&poly, 
