d0ad87e2c27b6eb554b8a1a6d89d41d9dcc47ee3
Alec Jacobson (jalec <Alec Jacobson (jalec@inf.ethz.ch)>
2013-12-15 14:28:40 +0100

    lbs_matrix overloads for different style output

--- include/igl/lbs_matrix.cpp
+++ include/igl/lbs_matrix.cpp
@@ -3,6 +3,40 @@
 IGL_INLINE void igl::lbs_matrix(
   const Eigen::MatrixXd & V, 
   const Eigen::MatrixXd & W,
+  Eigen::MatrixXd & M)
+{
+  using namespace Eigen;
+  // Number of dimensions
+  const int dim = V.cols();
+  // Number of model points
+  const int n = V.rows();
+  // Number of skinning transformations/weights
+  const int m = W.cols();
+
+  // Assumes that first n rows of weights correspond to V
+  assert(W.rows() >= n);
+
+  M.resize(n,(dim+1)*m);
+  for(int j = 0;j<m;j++)
+  {
+    VectorXd Wj = W.block(0,j,V.rows(),1);
+    for(int i = 0;i<(dim+1);i++)
+    {
+      if(i<dim)
+      {
+        M.col(i + j*(dim+1)) = 
+          Wj.cwiseProduct(V.col(i));
+      }else
+      {
+        M.col(i + j*(dim+1)).array() = W.block(0,j,V.rows(),1).array();
+      }
+    }
+  }
+}
+
+IGL_INLINE void igl::lbs_matrix_column(
+  const Eigen::MatrixXd & V, 
+  const Eigen::MatrixXd & W,
   Eigen::SparseMatrix<double>& M)
 {
   // number of mesh vertices
@@ -41,7 +75,7 @@ IGL_INLINE void igl::lbs_matrix(
   M.makeCompressed();
 }
 
-IGL_INLINE void igl::lbs_matrix(
+IGL_INLINE void igl::lbs_matrix_column(
   const Eigen::MatrixXd & V, 
   const Eigen::MatrixXd & W,
   Eigen::MatrixXd & M)
@@ -79,7 +113,7 @@ IGL_INLINE void igl::lbs_matrix(
   }
 }
 
-IGL_INLINE void igl::lbs_matrix(
+IGL_INLINE void igl::lbs_matrix_column(
   const Eigen::MatrixXd & V, 
   const Eigen::MatrixXd & W,
   const Eigen::MatrixXi & WI,
@@ -129,7 +163,7 @@ IGL_INLINE void igl::lbs_matrix(
 }
 
 
-IGL_INLINE void igl::lbs_matrix(
+IGL_INLINE void igl::lbs_matrix_column(
   const Eigen::MatrixXd & V, 
   const Eigen::MatrixXd & W,
   const Eigen::MatrixXi & WI,
@@ -138,40 +172,6 @@ IGL_INLINE void igl::lbs_matrix(
   // Cheapskate wrapper
   using namespace Eigen;
   SparseMatrix<double> sM;
-  lbs_matrix(V,W,WI,sM);
+  lbs_matrix_column(V,W,WI,sM);
   M = MatrixXd(sM);
-}
-
-IGL_INLINE void igl::lbs_matrix(
-  const Eigen::MatrixXd & V, 
-  const Eigen::MatrixXd & W,
-  Eigen::MatrixXd & M)
-{
-  using namespace Eigen;
-  // Number of dimensions
-  const int dim = V.cols();
-  // Number of model points
-  const int n = V.rows();
-  // Number of skinning transformations/weights
-  const int m = W.cols();
-
-  // Assumes that first n rows of weights correspond to V
-  assert(W.rows() >= n);
-
-  M.resize(n,(dim+1)*m);
-  for(int j = 0;j<m;j++)
-  {
-    VectorXd Wj = W.block(0,j,V.rows(),1);
-    for(int i = 0;i<(dim+1);i++)
-    {
-      if(i<dim)
-      {
-        M.col(i + j*(dim+1)) = 
-          Wj.cwiseProduct(V.col(i));
-      }else
-      {
-        M.col(i + j*(dim+1)).array() = W.block(0,j,V.rows(),1).array();
-      }
-    }
-  }
 }
