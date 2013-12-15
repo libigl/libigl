db7b0550dbf0d4bfb80f7067e0b545eec6977d22
Alec Jacobson (jalec <Alec Jacobson (jalec@inf.ethz.ch)>
2013-12-15 23:07:03 +0100

    dqs and lbs_matrix from puppet

---
+++ include/igl/dqs.h
@@ -0,0 +1,34 @@
+#ifndef IGL_DQS_H
+#define IGL_DQS_H
+#include "igl_inline.h"
+#include <vector>
+#include <Eigen/Core>
+namespace igl
+{
+  // Dual quaternion skinning
+  //
+  // Inputs:
+  //   V  #V by 3 list of rest positions
+  //   W  #W by #C list of weights
+  //   vQ  #C list of rotation quaternions 
+  //   vT  #C list of translation vectors
+  // Outputs:
+  //   U  #V by 3 list of new positions
+  template <
+    typename DerivedV,
+    typename DerivedW,
+    typename Q,
+    typename T,
+    typename DerivedU>
+  IGL_INLINE void dqs(
+    const Eigen::PlainObjectBase<DerivedV> & V,
+    const Eigen::PlainObjectBase<DerivedW> & W,
+    const std::vector<Q> & vQ,
+    const std::vector<T> & vT,
+    Eigen::PlainObjectBase<DerivedU> & U);
+};
+
+#ifdef IGL_HEADER_ONLY
+#  include "dqs.cpp"
+#endif
+#endif
