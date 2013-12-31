642a3f8c4ce2ad9ddfae8729cd09b995e80d900d
Daniele Panozzo <daniele.panozzo@gmail.com>
2013-12-31 12:08:43 +0100

    fix in principal curvature and some duplicated symbols in antweakbar

--- include/igl/ReAntTweakBar.h
+++ include/igl/ReAntTweakBar.h
@@ -242,8 +242,8 @@ namespace igl
         const char * name, 
         TwType type, 
         const char * value_str);
-      const std::vector<ReTwRWItem> & get_rw_items();
-      const std::vector<ReTwCBItem> & get_cb_items();
+      IGL_INLINE const std::vector<ReTwRWItem> & get_rw_items();
+      IGL_INLINE const std::vector<ReTwCBItem> & get_cb_items();
   };
 }
 
