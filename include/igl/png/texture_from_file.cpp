cd7774d356d536ed1a260f4f8706db3b5342c609
Alec Jacobson (jalec <Alec Jacobson (jalec@inf.ethz.ch)>
2014-01-10 19:40:12 +0100

    textured mesh example and loading from any file type (via convert on apple only)'
    Former-commit-id: f11d25b5c2c7e5d4aa8f60d482e3f0fa829bced0

---
+++ include/igl/png/texture_from_file.cpp
@@ -0,0 +1,57 @@
+#include "texture_from_file.h"
+#ifndef IGL_NO_OPENGL
+
+#include "texture_from_png.h"
+#include <igl/report_gl_error.h>
+#include <igl/STR.h>
+#include <igl/pathinfo.h>
+#include <igl/texture_from_tga.h>
+#include <YImage.hpp>
+#include <string>
+#include <algorithm>
+#include <iostream>
+
+IGL_INLINE bool igl::texture_from_file(const std::string filename, GLuint & id)
+{
+  using namespace igl;
+  using namespace std;
+  // dirname, basename, extension and filename
+  string d,b,ext,f;
+  pathinfo(filename,d,b,ext,f);
+  // Convert extension to lower case
+  transform(ext.begin(), ext.end(), ext.begin(), ::tolower);
+  if(ext == "tga")
+  {
+    return texture_from_tga(filename,id);
+  }else if(ext == "png")
+  {
+    return texture_from_png(filename,id);
+  }else
+  {
+#ifdef __APPLE__
+    // Convert to a temporary png file
+    string tmp = "/var/tmp/.texture_from_file.png";
+    string command = STR("/opt/local/bin/convert "<<filename<<" "<<tmp);
+    try
+    {
+      if(system(command.c_str())==0)
+      {
+        return texture_from_file(tmp.c_str(),id);
+      }else
+      {
+        cerr<<"texture_from_file: calling to convert ('"<<command<<"') failed..."<<endl;
+        return false;
+      }
+    }catch(int e)
+    {
+      cerr<<"^"<<__FUNCTION__<<": Calling to convert crashed..."<<endl;
+      return false;
+    }
+#else
+    return false;
+#endif
+  }
+}
+#endif
+
+
---
+++ include/igl/png/texture_from_file.h
@@ -0,0 +1,30 @@
+#ifndef IGL_TEXTURE_FROM_FILE_H
+#define IGL_TEXTURE_FROM_FILE_H
+#ifndef IGL_NO_OPENGL
+#include "../igl_inline.h"
+
+#include "../OpenGL_convenience.h"
+
+#include <string>
+
+namespace igl
+{
+  // Read an image from an image  ile and use it as a texture
+  //
+  // Input:
+  //  filename  path to image file
+  // Output:
+  //  id  of generated openGL texture
+  // Returns true on success, false on failure
+  IGL_INLINE bool texture_from_file(const std::string filename, GLuint & id);
+}
+
+#ifdef IGL_HEADER_ONLY
+#  include "texture_from_file.cpp"
+#endif
+
+#endif
+#endif
+
+
+
