d1ff1f56ee0f7933ff8e8ad6973929f24456dfcd
Alec Jacobson (jalec <Alec Jacobson (jalec@inf.ethz.ch)>
2014-01-18 16:19:53 +0100

    harmonic prototype
    Former-commit-id: a40926a308a33046e6a3a09b1176bd42b76facce

---
+++ include/igl/harmonic.h
@@ -0,0 +1,15 @@
+// This file is part of libigl, a simple c++ geometry processing library.
+// 
+// Copyright (C) 2013 Alec Jacobson <alecjacobson@gmail.com>
+// 
+// This Source Code Form is subject to the terms of the Mozilla Public License 
+// v. 2.0. If a copy of the MPL was not distributed with this file, You can 
+// obtain one at http://mozilla.org/MPL/2.0/.
+#ifndef IGL_HARMONIC_H
+#define IGL_HARMONIC_H
+#include "igl_inline.h"
+#include <Eigen/Core>
+
+namespace igl 
+{
+};
