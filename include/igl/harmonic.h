059c36d5b3d31f00fc6e2b0a2ed4dfd4303a1232
Alec Jacobson (jalec <Alec Jacobson (jalec@inf.ethz.ch)>
2014-01-18 16:33:05 +0100

    harmonic weights

--- include/igl/harmonic.h
+++ include/igl/harmonic.h
@@ -9,7 +9,29 @@
 #define IGL_HARMONIC_H
 #include "igl_inline.h"
 #include <Eigen/Core>
-
 namespace igl 
 {
+  // Compute k-harmonic weight functions "coordinates".
+  //
+  //
+  // Inputs:
+  //   V  #V by dim vertex positions
+  //   F  #F by simplex-size list of element indices
+  //   b  #b boundary indices into V
+  //   bc #b by #W list of boundary values
+  //   k  power of harmonic operation (1: harmonic, 2: biharmonic, etc)
+  // Outputs:
+  //   W  #V by #W list of weights
+  //
+  IGL_INLINE bool harmonic(
+    const Eigen::MatrixXd & V,
+    const Eigen::MatrixXi & F,
+    const Eigen::VectorXi & b,
+    const Eigen::MatrixXd & bc,
+    const int k,
+    Eigen::MatrixXd & W);
 };
+#ifdef IGL_HEADER_ONLY
+#include "harmonic.cpp"
+#endif
+#endif
