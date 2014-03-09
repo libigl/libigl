f315b0ba780b85b05d5b30eaec6bc70195d03041
Alec Jacobson <alecjacobson@gmail.com>
2014-03-09 12:43:30 +0100

    note that cotangent is actually 1/2 cotangent
    Former-commit-id: f035e4b5a111af80732a77f766dde88de37aca13

--- include/igl/cotangent.h
+++ include/igl/cotangent.h
@@ -23,6 +23,9 @@ namespace igl
   //   C  #F by {3|6} list of cotangents corresponding angles
   //     for triangles, columns correspond to edges [1,2],[2,0],[0,1]
   //     for tets, columns correspond to edges [1,2],[2,0],[0,1],[3,0],[3,1],[3,2]
+  //
+  // Known bug:
+  //   This computes 0.5*cotangent
   template <class MatV, class MatF, class MatC>
   IGL_INLINE void cotangent(const MatV & V, const MatF & F, MatC & C);
 }
