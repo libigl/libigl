c47c2706cdb6779722489883422f920cb33d5ae0
Alec Jacobson <alecjacobson@gmail.com>
2014-06-17 09:22:06 -0400

    missing rhs lhs files
    Former-commit-id: bfddf7f7e040f42f0e9a9f7e4bcdc359b59bd372

---
+++ include/igl/matlab/prepare_lhs.cpp
@@ -0,0 +1,29 @@
+#include "prepare_lhs.h"
+#include <algorithm>
+template <typename DerivedV>
+IGL_INLINE void igl::prepare_lhs_double(
+  const Eigen::PlainObjectBase<DerivedV> & V,
+  mxArray *plhs[])
+{
+  using namespace std;
+  plhs[0] = mxCreateDoubleMatrix(V.rows(),V.cols(), mxREAL);
+  double * Vp = mxGetPr(plhs[0]);
+  copy(&V.data()[0],&V.data()[0]+V.size(),Vp);
+}
+
+template <typename DerivedV>
+IGL_INLINE void igl::prepare_lhs_index(
+  const Eigen::PlainObjectBase<DerivedV> & V,
+  mxArray *plhs[])
+{
+  // Treat indices as reals
+  
+  const auto Vd = (V.template cast<double>().array()+1).eval();
+  return prepare_lhs_double(Vd,plhs);
+}
+
+#ifndef IGL_HEADER_ONLY
+template void igl::prepare_lhs_index<Eigen::Matrix<double, -1, 1, 0, -1, 1> >(Eigen::PlainObjectBase<Eigen::Matrix<double, -1, 1, 0, -1, 1> > const&, mxArray_tag**);
+template void igl::prepare_lhs_index<Eigen::Matrix<int, -1, 1, 0, -1, 1> >(Eigen::PlainObjectBase<Eigen::Matrix<int, -1, 1, 0, -1, 1> > const&, mxArray_tag**);
+template void igl::prepare_lhs_double<Eigen::Matrix<double, -1, -1, 0, -1, -1> >(Eigen::PlainObjectBase<Eigen::Matrix<double, -1, -1, 0, -1, -1> > const&, mxArray_tag**);
+#endif
