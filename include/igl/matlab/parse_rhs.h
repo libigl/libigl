bfddf7f7e040f42f0e9a9f7e4bcdc359b59bd372
Alec Jacobson <alecjacobson@gmail.com>
2014-06-17 09:22:06 -0400

    missing rhs lhs files

---
+++ include/igl/matlab/parse_rhs.h
@@ -0,0 +1,27 @@
+#ifndef IGL_PARSE_RHS_H
+#define IGL_PARSE_RHS_H
+#include <igl/igl_inline.h>
+#include <mex.h>
+#include <Eigen/Dense>
+namespace igl
+{
+  // Reads in a matrix as a double
+  //
+  // Inputs:
+  //   prhs  points to rhs argument
+  // Outputs:
+  //   V  M by N matrix 
+  template <typename DerivedV>
+  IGL_INLINE void parse_rhs_double(
+    const mxArray *prhs[], 
+    Eigen::PlainObjectBase<DerivedV> & V);
+  // Reads in a matrix and subtracts 1
+  template <typename DerivedV>
+  IGL_INLINE void parse_rhs_index(
+    const mxArray *prhs[], 
+    Eigen::PlainObjectBase<DerivedV> & V);
+};
+#ifdef IGL_HEADER_ONLY
+#  include "parse_rhs.cpp"
+#endif
+#endif
