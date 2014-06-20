15a0c8f54997e299f4f85aecaa156f706a763d33
Alec Jacobson <alecjacobson@gmail.com>
2014-06-20 11:45:27 -0400

    clean up principal curvature example

--- tutorial/203_CurvatureDirections/main.cpp
+++ tutorial/203_CurvatureDirections/main.cpp
@@ -6,32 +6,42 @@
 #include <igl/avg_edge_length.h>
 #include <igl/barycenter.h>
 #include <igl/principal_curvature.h>
+#include <igl/jet.h>
 
 Eigen::MatrixXd V;
 Eigen::MatrixXi F;
 
 int main(int argc, char *argv[])
 {
+  using namespace Eigen;
   // Load a mesh in OFF format
   igl::readOFF("../shared/fertility.off", V, F);
 
   // Compute curvature directions via quadric fitting
-  Eigen::MatrixXd PD1,PD2,PV1,PV2;
+  MatrixXd PD1,PD2;
+  VectorXd PV1,PV2;
   igl::principal_curvature(V,F,PD1,PD2,PV1,PV2);
 
-  // Plot the mesh
   igl::Viewer viewer;
   viewer.set_mesh(V, F);
 
-  // Find the average edge length
-  double avg = igl::avg_edge_length(V,F);
+  // Compute pseudocolor
+  MatrixXd C;
+  igl::jet(PV1,true,C);
+  viewer.set_colors(C);
+
+  // Average edge length for sizing
+  const double avg = igl::avg_edge_length(V,F);
   
-  // Draw a red segment on each vertex parallel to the minimal curvature direction
-  viewer.add_edges(V + PD1*avg, V - PD1*avg, Eigen::RowVector3d(0,0,1));
+  // Draw a red segment parallel to the minimal curvature direction
+  const RowVector3d red(1,0,0),blue(0,0,1);
+  viewer.add_edges(V + PD1*avg, V - PD1*avg, red);
 
-  // Draw a blue segment on each vertex parallel to the maximal curvature direction
-  viewer.add_edges(V + PD2*avg, V - PD2*avg, Eigen::RowVector3d(1,0,0));
+  // Draw a blue segment parallel to the maximal curvature direction
+  viewer.add_edges(V + PD2*avg, V - PD2*avg, blue);
 
-  // Launch the viewer
+  // Hide wireframe
+  viewer.options.show_lines = false;
+
   viewer.launch();
 }
