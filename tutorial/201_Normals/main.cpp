de279a738556b4163b920296f44d49a91f3e6bf7
Alec Jacobson <alecjacobson@gmail.com>
2014-06-21 14:30:56 -0400

    normals example

--- tutorial/201_Normals/main.cpp
+++ tutorial/201_Normals/main.cpp
@@ -15,15 +15,19 @@ Eigen::MatrixXd N_corners;
 // This function is called every time a keyboard button is pressed
 bool key_down(igl::Viewer& viewer, unsigned char key, int modifier)
 {
-  if (key == '1')
-    viewer.set_normals(N_vertices);
-
-  if (key == '2')
-    viewer.set_normals(N_faces);
-
-  if (key == '3')
-    viewer.set_normals(N_corners);
-
+  switch(key)
+  {
+    case '1':
+      viewer.set_normals(N_faces);
+      return true;
+    case '2':
+      viewer.set_normals(N_vertices);
+      return true;
+    case '3':
+      viewer.set_normals(N_corners);
+      return true;
+    default: break;
+  }
   return false;
 }
 
@@ -32,18 +36,19 @@ int main(int argc, char *argv[])
   // Load a mesh in OFF format
   igl::readOFF("../shared/fandisk.off", V, F);
 
-  // Compute per-vertex normals
-  igl::per_vertex_normals(V,F,N_vertices);
-
   // Compute per-face normals
   igl::per_face_normals(V,F,N_faces);
 
-  // Compute per-corner normals
+  // Compute per-vertex normals
+  igl::per_vertex_normals(V,F,N_vertices);
+
+  // Compute per-corner normals, |dihedral angle| > 20 degrees --> crease
   igl::per_corner_normals(V,F,20,N_corners);
 
   // Plot the mesh
   igl::Viewer viewer;
   viewer.callback_key_down = &key_down;
+  viewer.options.show_lines = false;
   viewer.set_mesh(V, F);
   viewer.set_normals(N_vertices);
   viewer.launch();
