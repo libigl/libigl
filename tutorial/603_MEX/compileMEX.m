12fce281cc8c9d0779770d6dde9ecb110ee1296d
Daniele Panozzo <panozzo@inf.ethz.ch>
2014-06-23 22:11:34 +0200

    * cleanup on MEX tutorial example
    Former-commit-id: c8bd92ff256b77811e550991981b8e983f998d42

--- tutorial/603_MEX/compileMEX.m
+++ tutorial/603_MEX/compileMEX.m
@@ -1,3 +1,11 @@
-mex readOBJ.cpp ...
+%% Compile the wrapper for readOBJ
+mex readOBJ_mex.cpp ...
     -I../../include ...
     -I/opt/local/include/eigen3 %% Change this path to point to your copy of Eigen
+
+%% Load an OBJ mesh
+[V,F] = readOBJ_mex('../shared/cube.obj');
+
+%% Plot the mesh
+trimesh(F,V(:,1),V(:,2),V(:,3));
+axis vis3d;
