7460ef10345cd45524861660656304f0691f78d3
Daniele Panozzo <panozzo@inf.ethz.ch>
2014-06-24 17:03:53 +0200

    - added wrapper for triangle library - added tutorial example for triangle
    Former-commit-id: a7029dfc2a82727ee1289962701cc1a1e5846403

---
+++ tutorial/604_Triangle/main.cpp
@@ -0,0 +1,37 @@
+#include <igl/viewer/Viewer.h>
+#include <igl/triangle/triangle_wrapper.h>
+// Input polygon
+Eigen::MatrixXd V;
+Eigen::MatrixXi E;
+Eigen::MatrixXd H;
+
+// Triangulated interior
+Eigen::MatrixXd V2;
+Eigen::MatrixXi F2;
+
+int main(int argc, char *argv[])
+{
+  using namespace Eigen;
+  using namespace std;
+
+  // Create the boundary of a square
+  V.resize(8,2);
+  E.resize(8,2);
+  H.resize(1,2);
+
+  V << -1,-1, 1,-1, 1,1, -1, 1,
+       -2,-2, 2,-2, 2,2, -2, 2;
+
+  E << 0,1, 1,2, 2,3, 3,0,
+       4,5, 5,6, 6,7, 7,4;
+
+  H << 0,0;
+
+  // Triangulate the interior
+  igl::triangle_wrapper(V,E,H,V2,F2,"a0.005q");
+
+  // Plot the generated mesh
+  igl::Viewer viewer;
+  viewer.set_mesh(V2,F2);
+  viewer.launch();
+}
