4372f57c551983644df552c023d89d999e5811c5
Alec Jacobson <alecjacobson@gmail.com>
2014-06-24 23:50:32 -0400

    plot_vector --> print_vector
    Former-commit-id: d5254d4218cfe5e85ce83a14cc7bdd6ee48c7523

--- include/igl/print_vector.h
+++ include/igl/print_vector.h
@@ -5,8 +5,8 @@
 // This Source Code Form is subject to the terms of the Mozilla Public License 
 // v. 2.0. If a copy of the MPL was not distributed with this file, You can 
 // obtain one at http://mozilla.org/MPL/2.0/.
-#ifndef IGL_PLOT_VECTOR_H
-#define IGL_PLOT_VECTOR_H
+#ifndef IGL_PRINT_VECTOR_H
+#define IGL_PRINT_VECTOR_H
 #include "igl_inline.h"
 
 #include <vector>
@@ -15,15 +15,15 @@ namespace igl
   // Not clear what these are supposed to be doing. Currently they print
   // vectors to standard error...
   template <typename T>
-  IGL_INLINE void plot_vector( std::vector<T>& v);
+  IGL_INLINE void print_vector( std::vector<T>& v);
   template <typename T>
-  IGL_INLINE void plot_vector( std::vector< std::vector<T> >& v);
+  IGL_INLINE void print_vector( std::vector< std::vector<T> >& v);
   template <typename T>
-  IGL_INLINE void plot_vector(std::vector< std::vector< std::vector<T> > >& v);
+  IGL_INLINE void print_vector(std::vector< std::vector< std::vector<T> > >& v);
 }
 
 #ifndef IGL_STATIC_LIBRARY
-#  include "plot_vector.cpp"
+#  include "print_vector.cpp"
 #endif
 
 #endif
