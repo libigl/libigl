98e3dc6a1121a4e7f3ffd0a5fb378b068d9fa5fa
Alec Jacobson <alecjacobson@gmail.com>
2014-06-24 23:49:20 -0400

    moveVF --> average_onto_faces

--- include/igl/average_onto_faces.h
+++ include/igl/average_onto_faces.h
@@ -5,8 +5,8 @@
 // This Source Code Form is subject to the terms of the Mozilla Public License 
 // v. 2.0. If a copy of the MPL was not distributed with this file, You can 
 // obtain one at http://mozilla.org/MPL/2.0/.
-#ifndef IGL_MOVEVF_H
-#define IGL_MOVEVF_H
+#ifndef IGL_AVERAGE_ONTO_FACES_H
+#define IGL_AVERAGE_ONTO_FACES_H
 #include "igl_inline.h"
 
 #include <Eigen/Dense>
@@ -22,7 +22,7 @@ namespace igl
   // Output:
   // SV: scalar field defined on faces
   template <typename T, typename I>
-  IGL_INLINE void moveVF(
+  IGL_INLINE void average_onto_faces(
     const Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> &V,
     const Eigen::Matrix<I, Eigen::Dynamic, Eigen::Dynamic> &F,
     const Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> &S,
@@ -30,7 +30,7 @@ namespace igl
 }
 
 #ifndef IGL_STATIC_LIBRARY
-#  include "moveVF.cpp"
+#  include "average_onto_faces.cpp"
 #endif
 
 #endif
