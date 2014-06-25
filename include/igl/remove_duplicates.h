69808310c144a5c7f23cf523fe8104ff9bc1b815
Alec Jacobson <alecjacobson@gmail.com>
2014-06-24 23:54:38 -0400

    removeDuplicates --> remove_duplicates
    Former-commit-id: fb2c4ad15a35e79f4001b86fdf661f6a6aa6369d

--- include/igl/remove_duplicates.h
+++ include/igl/remove_duplicates.h
@@ -5,14 +5,14 @@
 // This Source Code Form is subject to the terms of the Mozilla Public License 
 // v. 2.0. If a copy of the MPL was not distributed with this file, You can 
 // obtain one at http://mozilla.org/MPL/2.0/.
-#ifndef IGL_REMOVEDUPLICATES_H
-#define IGL_REMOVEDUPLICATES_H
+#ifndef IGL_REMOVE_DUPLICATES_H
+#define IGL_REMOVE_DUPLICATES_H
 #include "igl_inline.h"
 
 #include <Eigen/Core>
 namespace igl 
 {
-  // [ NV, NF ] = removeDuplicates( V,F,epsilon )
+  // [ NV, NF ] = remove_duplicates( V,F,epsilon )
   // Merge the duplicate vertices from V, fixing the topology accordingly
   //
   // Input:
@@ -23,7 +23,7 @@ namespace igl
   // NV, NF: new mesh without duplicate vertices
   
 //  template <typename T, typename S>
-//  IGL_INLINE void removeDuplicates(
+//  IGL_INLINE void remove_duplicates(
 //                                   const Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> &V,
 //                                   const Eigen::Matrix<S, Eigen::Dynamic, Eigen::Dynamic> &F,
 //                                   Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> &NV,
@@ -32,7 +32,7 @@ namespace igl
 //                                   const double epsilon = 2.2204e-15);
   
   template <typename DerivedV, typename DerivedF>
-  IGL_INLINE void removeDuplicates(
+  IGL_INLINE void remove_duplicates(
     const Eigen::PlainObjectBase<DerivedV> &V,
     const Eigen::PlainObjectBase<DerivedF> &F,
     Eigen::PlainObjectBase<DerivedV> &NV,
@@ -43,7 +43,7 @@ namespace igl
 }
 
 #ifndef IGL_STATIC_LIBRARY
-#  include "removeDuplicates.cpp"
+#  include "remove_duplicates.cpp"
 #endif
 
 #endif
