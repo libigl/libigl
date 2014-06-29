0b45f144267c1f47a68b9f8c9703b7a49d60068a
Daniele Panozzo <daniele.panozzo@gmail.com>
2014-06-29 16:04:54 +0200

    - moved TextRendering code outside the viewer

--- include/igl/viewer/Viewer.cpp
+++ include/igl/viewer/Viewer.cpp
@@ -169,6 +169,7 @@ Eigen::Matrix4f translate(
 #include <igl/snap_to_canonical_view_quat.h>
 #include <igl/unproject.h>
 #include <TwOpenGLCore.h>
+#include <igl/viewer/TextRenderer.h>
 
 // Plugin manager (exported to other compilation units)
 igl::Plugin_manager igl_viewer_plugin_manager;
@@ -179,129 +180,7 @@ static double highdpi = 1;
 static double scroll_x = 0;
 static double scroll_y = 0;
 
-/* This class extends the font rendering code in AntTweakBar
-   so that it can be used to render text at arbitrary 3D positions */
-class TextRenderer : public CTwGraphOpenGLCore {
-public:
-  TextRenderer() : m_shaderHandleBackup(0) { }
-
-  virtual int Init()
-  {
-    int retval = CTwGraphOpenGLCore::Init();
-    if (retval == 1)
-    {
-      std::string vertexShader =
-          "#version 150\n"
-          "uniform vec2 offset;"
-          "uniform vec2 wndSize;"
-          "uniform vec4 color;"
-          "uniform float depth;"
-          "in vec2 vertex;"
-          "in vec2 uv;"
-          "out vec4 fcolor;"
-          "out vec2 fuv;"
-          "void main() {"
-          "  gl_Position = vec4(2.0*(vertex.x+offset.x-0.5)/wndSize.x - 1.0,"
-          "                     1.0 - 2.0*(vertex.y+offset.y-0.5)/wndSize.y,"
-          "                     depth, 1);"
-          " fuv = uv;"
-          " fcolor = color;"
-          "}";
-
-      std::string fragmentShader =
-        "#version 150\n"
-        "uniform sampler2D tex;"
-        "in vec2 fuv;"
-        "in vec4 fcolor;"
-        "out vec4 outColor;"
-        "void main() { outColor.rgb = fcolor.bgr; outColor.a = fcolor.a * texture(tex, fuv).r; }";
-
-      if (!m_shader.init(vertexShader, fragmentShader, "outColor"))
-        return 0;
-
-      /* Adjust location bindings */
-      glBindAttribLocation(m_shader.program_shader, 0, "vertex");
-      glBindAttribLocation(m_shader.program_shader, 1, "uv");
-      glBindAttribLocation(m_shader.program_shader, 2, "color");
-      glLinkProgram(m_shader.program_shader);
-
-      m_shaderHandleBackup = m_TriTexUniProgram;
-      m_TriTexUniProgram = m_shader.program_shader;
-      m_TriTexUniLocationOffset = m_shader.uniform("offset");
-      m_TriTexUniLocationWndSize = m_shader.uniform("wndSize");
-      m_TriTexUniLocationColor = m_shader.uniform("color");
-      m_TriTexUniLocationTexture = m_shader.uniform("tex");
-      m_TriTexUniLocationDepth = m_shader.uniform("depth");
-    }
-    return retval;
-  }
-
-  virtual int Shut()
-  {
-    for (auto kv : m_textObjects)
-      DeleteTextObj(kv.second);
-    m_shader.free();
-    m_TriTexUniProgram = m_shaderHandleBackup;
-    return CTwGraphOpenGLCore::Shut();
-  }
-
-  void BeginDraw(const Eigen::Matrix4f &view, const Eigen::Matrix4f &proj,
-    const Eigen::Vector4f &_viewport, float _object_scale)
-  {
-    viewport = _viewport;
-    proj_matrix = proj;
-    view_matrix = view;
-    CTwGraphOpenGLCore::BeginDraw(viewport[2], viewport[3]);
-    glEnable(GL_DEPTH_TEST);
-    glDepthMask(GL_FALSE);
-    object_scale = _object_scale;
-  }
-
-  void EndDraw()
-  {
-    /* Limit the number of cached text objects */
-    for (auto it = m_textObjects.cbegin(); it != m_textObjects.cend(); )
-    {
-      if (m_textObjects.size() < 1000000)
-        break;
-      DeleteTextObj(it->second);
-      m_textObjects.erase(it++);
-    }
-
-    glDepthMask(GL_TRUE);
-    CTwGraphOpenGLCore::EndDraw();
-  }
-
-  void DrawText(Eigen::Vector3d pos, Eigen::Vector3d normal, const std::string &text)
-  {
-    pos += normal * 0.005f * object_scale;
-    Eigen::Vector3f coord = project(Eigen::Vector3f(pos(0), pos(1), pos(2)),
-        view_matrix, proj_matrix, viewport);
-    auto it = m_textObjects.find(text);
-    void *text_obj = nullptr;
-    if (it == m_textObjects.end())
-    {
-      text_obj = NewTextObj();
-      BuildText(text_obj, &text, NULL, NULL, 1, g_DefaultNormalFont, 0, 0);
-      m_textObjects[text] = text_obj;
-    } else {
-      text_obj = it->second;
-    }
-    m_shader.bind();
-    glUniform1f(m_TriTexUniLocationDepth, 2*(coord(2)-0.5f));
-    CTwGraphOpenGLCore::DrawText(text_obj, coord[0], viewport[3] - coord[1], COLOR32_BLUE, 0);
-  }
-protected:
-  igl::Viewer::OpenGL_shader m_shader;
-  std::map<std::string, void *> m_textObjects;
-  GLuint m_shaderHandleBackup;
-  GLuint m_TriTexUniLocationDepth;
-  Eigen::Matrix4f view_matrix, proj_matrix;
-  Eigen::Vector4f viewport;
-  float object_scale;
-};
-
-static TextRenderer __font_renderer;
+static igl::TextRenderer __font_renderer;
 
 static void glfw_mouse_press(GLFWwindow* window, int button, int action, int modifier)
 {
@@ -2344,7 +2223,7 @@ namespace igl
   }
 
   void Viewer::set_edges(
-    const Eigen::MatrixXd& P, 
+    const Eigen::MatrixXd& P,
     const Eigen::MatrixXi& E,
     const Eigen::MatrixXd& C)
   {
