2e979ab8a22ef652ff47905b42debf5ffcbbb851
Alec Jacobson <alecjacobson@gmail.com>
2014-11-09 12:50:50 -0500

    header needs relative link
    Former-commit-id: 74d9ecff42158c50475dbd69cb4276877fd9574f

--- scripts/update_gh-pages.sh
+++ scripts/update_gh-pages.sh
@@ -14,6 +14,15 @@ html header:   <script type='text/javascript' src='http://cdn.mathjax.org/mathja
 echo "$HEADER" \
   | cat - README.md | multimarkdown -o index.html && \
   git commit -m "update index.html to match README.md" README.md index.html
+HEADER="title: libigl
+author: Alec Jacobson and Daniele Panozzo and others
+css: ../tutorial/style.css
+html header:   <script type='text/javascript' src='http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'></script>
+<link rel='stylesheet' href='http://yandex.st/highlightjs/7.3/styles/default.min.css'>
+<script src='http://yandex.st/highlightjs/7.3/highlight.min.js'></script>
+<script>hljs.initHighlightingOnLoad();</script>
+
+"
 echo "$HEADER" \
   | cat - build/README.md | multimarkdown -o build/index.html && \
   git commit -m "update index.html to match README.md" build/README.md \
