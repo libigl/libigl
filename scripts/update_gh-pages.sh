cd8b8123d578cfd09faae247f2f8ef1756f6472f
Alec Jacobson <alecjacobson@gmail.com>
2014-11-09 12:48:30 -0500

    build index page

--- scripts/update_gh-pages.sh
+++ scripts/update_gh-pages.sh
@@ -2,7 +2,7 @@
 # Usage: cd $LIBIGL; scripts/update_gh-pages.sh
 set -o xtrace
 git pull && git checkout gh-pages && git rebase master && git pull
-echo "title: libigl
+HEADER="title: libigl
 author: Alec Jacobson and Daniele Panozzo and others
 css: tutorial/style.css
 html header:   <script type='text/javascript' src='http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'></script>
@@ -10,7 +10,12 @@ html header:   <script type='text/javascript' src='http://cdn.mathjax.org/mathja
 <script src='http://yandex.st/highlightjs/7.3/highlight.min.js'></script>
 <script>hljs.initHighlightingOnLoad();</script>
 
-" \
+"
+echo "$HEADER" \
   | cat - README.md | multimarkdown -o index.html && \
   git commit -m "update index.html to match README.md" README.md index.html
+echo "$HEADER" \
+  | cat - build/README.md | multimarkdown -o build/index.html && \
+  git commit -m "update index.html to match README.md" build/README.md \
+  build/index.html
 git push origin gh-pages && git checkout master && git merge gh-pages && git push
