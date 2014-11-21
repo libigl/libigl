772e721ce8452cb239aca831a2959cbc838a1198
Olga Diamanti <olga.diamanti@inf.ethz.ch>
2014-11-21 15:09:20 +0100

    restored threshold for cutting (it might not be needed at all)
    Former-commit-id: fc9cfcfca883460f143fcbd39c58acfe0d27e85e

--- include/igl/cut_mesh_from_singularities.cpp
+++ include/igl/cut_mesh_from_singularities.cpp
@@ -132,7 +132,7 @@ namespace igl {
           }
         }
 
-        if (guard++>0)
+        if (guard++>10000)
           over = true;
 
       } while (!over);
@@ -196,4 +196,5 @@ IGL_INLINE void igl::cut_mesh_from_singularities(const Eigen::PlainObjectBase<De
 #ifdef IGL_STATIC_LIBRARY
 // Explicit template specialization
 template void igl::cut_mesh_from_singularities<Eigen::Matrix<double, -1, 3, 0, -1, 3>, Eigen::Matrix<int, -1, 3, 0, -1, 3>, Eigen::Matrix<int, -1, -1, 0, -1, -1>, Eigen::Matrix<int, -1, -1, 0, -1, -1> >(Eigen::PlainObjectBase<Eigen::Matrix<double, -1, 3, 0, -1, 3> > const&, Eigen::PlainObjectBase<Eigen::Matrix<int, -1, 3, 0, -1, 3> > const&, Eigen::PlainObjectBase<Eigen::Matrix<int, -1, -1, 0, -1, -1> > const&, Eigen::PlainObjectBase<Eigen::Matrix<int, -1, -1, 0, -1, -1> >&);
+template void igl::cut_mesh_from_singularities<Eigen::Matrix<double, -1, 3, 0, -1, 3>, Eigen::Matrix<int, -1, 3, 0, -1, 3>, Eigen::Matrix<double, -1, 3, 0, -1, 3>, Eigen::Matrix<int, -1, -1, 0, -1, -1> >(Eigen::PlainObjectBase<Eigen::Matrix<double, -1, 3, 0, -1, 3> > const&, Eigen::PlainObjectBase<Eigen::Matrix<int, -1, 3, 0, -1, 3> > const&, Eigen::PlainObjectBase<Eigen::Matrix<double, -1, 3, 0, -1, 3> > const&, Eigen::PlainObjectBase<Eigen::Matrix<int, -1, -1, 0, -1, -1> >&);
 #endif
