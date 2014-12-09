fc5bc35bc35c660a51102c2845b8990becefd448
Alec Jacobson <alecjacobson@gmail.com>
2014-12-09 08:18:35 +0800

    fixed header guard

--- include/igl/boundary_loop.h
+++ include/igl/boundary_loop.h
@@ -5,8 +5,8 @@
 // This Source Code Form is subject to the terms of the Mozilla Public License 
 // v. 2.0. If a copy of the MPL was not distributed with this file, You can 
 // obtain one at http://mozilla.org/MPL/2.0/.
-#ifndef IGL_BOUNDARY_VERTICES_SORTED_H
-#define IGL_BOUNDARY_VERTICES_SORTED_H
+#ifndef IGL_BOUNDARY_LOOP_H
+#define IGL_BOUNDARY_LOOP_H
 #include <igl/igl_inline.h>
 
 #include <Eigen/Dense>
