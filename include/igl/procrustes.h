a9b2221de12113a7c002ce7bbb172914c1867e15
Stefan Brugger <stefanbrugger@gmail.com>
2014-12-13 10:37:18 +0100

    Added overloads
    Former-commit-id: 742258c3ae881c7f3a1d335f777905175c43a4d4

--- include/igl/procrustes.h
+++ include/igl/procrustes.h
@@ -15,16 +15,50 @@
 namespace igl
 {
     // Solve Procrustes problem in d dimensions.
-    // Given two point sets X,Y in R^d find best scale s, rotation/reflection R  and translation t 
+    // Given two point sets X,Y in R^d find best scale s, orthogonal R  and translation t 
     // s.t. |s*X*R + t - Y|^2 is minimized.
+    //
+    // Example:
+    // MatrixXd X, Y; (containing 3d points as rows)
+    // double scale;
+    // MatrixXd R;
+    // VectorXd t;
+    // igl::procrustes(X,Y,true,false,scale,R,t);
+    // R *= scale;
+    // MatrixXd Xprime = (X * R).rowwise() + t.transpose();
+    //
+    // Templates:
+    //    DerivedV point type
+    //    Scalar   scalar type
+    //    DerivedR type of R
+    //    DerivedT type of t
+    // Inputs:
+    //    X  #V by DIM first list of points
+    //    Y  #V by DIM second list of points
+    //    includeScaling  if scaling should be allowed
+    //    includeReflections  if R is allowed to be a reflection
+    // Outputs:
+    //    scale  scaling
+    //    R      orthogonal matrix
+    //    t      translation
+    template <typename DerivedV, typename Scalar, typename DerivedR, typename DerivedT>
+    IGL_INLINE void procrustes(
+        const Eigen::PlainObjectBase<DerivedV>& X,
+        const Eigen::PlainObjectBase<DerivedV>& Y,
+        bool includeScaling,
+        bool includeReflections,
+        Scalar& scale,
+        Eigen::PlainObjectBase<DerivedR>& R,
+        Eigen::PlainObjectBase<DerivedT>& t);
+
+
+    // Same as above but returns Eigen transformation object.
     //
     // Example:
     // MatrixXd X, Y; (containing 3d points as rows)
     // AffineCompact3d T;
     // igl::procrustes(X,Y,true,false,T);
     // MatrixXd Xprime = (X * T.linear()).rowwise() + T.translation().transpose();
-    //     
-    //
     // Templates:
     //    DerivedV point type
     //    Scalar   scalar type
@@ -44,7 +78,34 @@ namespace igl
         bool includeScaling,
         bool includeReflections,
         Eigen::Transform<Scalar,DIM,TType>& T);
-    
+
+
+    // Convenient wrapper that returns S=scale*R instead of scale and R separately
+    template <typename DerivedV, typename DerivedR, typename DerivedT>
+    IGL_INLINE void procrustes(
+        const Eigen::PlainObjectBase<DerivedV>& X,
+        const Eigen::PlainObjectBase<DerivedV>& Y,
+        bool includeScaling,
+        bool includeReflections,
+        Eigen::PlainObjectBase<DerivedR>& S,
+        Eigen::PlainObjectBase<DerivedT>& t);
+
+    // Convenient wrapper for rigid case (no scaling, no reflections)
+    template <typename DerivedV, typename DerivedR, typename DerivedT>
+    IGL_INLINE void procrustes(
+        const Eigen::PlainObjectBase<DerivedV>& X,
+        const Eigen::PlainObjectBase<DerivedV>& Y,
+        Eigen::PlainObjectBase<DerivedR>& R,
+        Eigen::PlainObjectBase<DerivedT>& t);
+
+    // Convenient wrapper for 2D case.
+    template <typename DerivedV, typename Scalar, typename DerivedT>
+    IGL_INLINE void procrustes(
+        const Eigen::PlainObjectBase<DerivedV>& X,
+        const Eigen::PlainObjectBase<DerivedV>& Y,
+        Eigen::Rotation2D<Scalar>& R,
+        Eigen::PlainObjectBase<DerivedT>& t);
+
 }
 
 #ifndef IGL_STATIC_LIBRARY
