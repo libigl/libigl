17ac6342dace6ce559be1686ef24d0a4ae7f0fb2
schuellc <schuellchr@gmail.com>
2015-02-20 10:41:00 +0100

    Reimplemented slice function -> huge speedup

--- include/igl/slice.h
+++ include/igl/slice.h
@@ -1,7 +1,7 @@
 // This file is part of libigl, a simple c++ geometry processing library.
 // 
 // Copyright (C) 2013 Alec Jacobson <alecjacobson@gmail.com>
-// 
+//
 // This Source Code Form is subject to the terms of the Mozilla Public License 
 // v. 2.0. If a copy of the MPL was not distributed with this file, You can 
 // obtain one at http://mozilla.org/MPL/2.0/.
@@ -14,8 +14,6 @@
 
 namespace igl
 {
-  // THIS MAY HAVE BEEN SUPERSEDED BY EIGEN'S select FUNCTION
-  // 
   // Act like the matlab X(row_indices,col_indices) operator
   // 
   // Inputs:
