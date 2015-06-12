5c7f41a78e7b4a8ad5af4a25d3eb95c2e91085fd
Alec Jacobson <alecjacobson@gmail.com>
2015-06-12 11:04:12 -0400

    bug fix in transpose for row major

--- include/igl/matlab/parse_rhs.cpp
+++ include/igl/matlab/parse_rhs.cpp
@@ -7,13 +7,30 @@ IGL_INLINE void igl::parse_rhs_double(
     Eigen::PlainObjectBase<DerivedV> & V)
 {
   using namespace std;
+  using namespace Eigen;
   // set number of mesh vertices
   const int n = mxGetM(prhs[0]);
   // set vertex position pointers
   double * Vp = mxGetPr(prhs[0]);
   const int dim = mxGetN(prhs[0]);
-  V.resize(n,dim);
-  copy(Vp,Vp+n*dim,&V.data()[0]);
+
+  typedef typename DerivedV::Scalar Scalar;
+  Matrix<Scalar, DerivedV::ColsAtCompileTime, DerivedV::RowsAtCompileTime, RowMajor> VT;
+  Scalar * V_data;
+  if(DerivedV::IsRowMajor)
+  {
+    VT.resize(dim,n);
+    V_data = VT.data();
+  }else
+  {
+    V.resize(n,dim);
+    V_data = V.data();
+  }
+  copy(Vp,Vp+n*dim,V_data);
+  if(DerivedV::IsRowMajor)
+  {
+    V = VT.transpose();
+  }
 }
 
 template <typename DerivedV>
