7b9284c5d415ce6ccf37ea3757dd28ee868f737f
Daniele Panozzo <daniele.panozzo@gmail.com>
2015-09-17 00:45:35 +0200

    trying to make the tcpwrapper python3+ compatible
    Former-commit-id: 0160faea45f0abf9785bd47492dc98ecc44cce45

--- python/102_DrawMeshTCP.py
+++ python/102_DrawMeshTCP.py
@@ -10,7 +10,7 @@ igl.read_triangle_mesh("../tutorial/shared/armadillo.obj", V, F)
 time2 = time.time()
 
 
-print 'Loading mesh (%d vertices) %0.3f ms' % (V.rows(),(time2-time1)*1000.0)
+print('Loading mesh (%d vertices) %0.3f ms' % (V.rows(),(time2-time1)*1000.0))
 
 
 # Plot the mesh
@@ -19,4 +19,4 @@ viewer.data.set_mesh(V, F);
 viewer.launch();
 time3 = time.time()
 
-print 'Sending to TCP viewer took %0.3f ms' % ((time3-time2)*1000.0)
+print('Sending to TCP viewer took %0.3f ms' % ((time3-time2)*1000.0))
