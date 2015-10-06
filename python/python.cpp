c861e80a053c1db1c562f7ccccceae89b354300a
Daniele Panozzo <daniele.panozzo@gmail.com>
2015-10-06 15:48:55 +0200

    fixes for python on linux
    Former-commit-id: 9f467b8009778b237cb3c281def485c2e833e909

--- python/python.cpp
+++ python/python.cpp
@@ -5,7 +5,10 @@
 
 extern void python_export_vector(py::module &);
 extern void python_export_igl(py::module &);
+
+#ifdef PY_VIEWER
 extern void python_export_igl_viewer(py::module &);
+#endif
 
 #ifdef PY_COMISO
 extern void python_export_igl_comiso(py::module &);
@@ -17,7 +20,11 @@ PYTHON_PLUGIN(igl) {
 
     python_export_vector(m);
     python_export_igl(m);
+
+
+    #ifdef PY_VIEWER
     python_export_igl_viewer(m);
+    #endif
 
     #ifdef PY_COMISO
     python_export_igl_comiso(m);
