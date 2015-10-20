5b63d63031a04a7cb3a04fc9bf57b9043b77fbb2
Qingnan Zhou <qnzhou@gmail.com>
2015-10-20 16:08:43 -0400

    Meaningful variable names and update doc.

--- include/igl/cgal/propagate_winding_numbers.h
+++ include/igl/cgal/propagate_winding_numbers.h
@@ -4,17 +4,38 @@
 #include <Eigen/Core>
 #include <vector>
 
+// The following methods compute the winding number on each side of each facet
+// or patch of a 3D mesh.  The input mesh is valid if it splits the ambient
+// space, R^3, into subspaces with constant integer winding numbers.  That is
+// the input mesh should be closed and for each directed edge the number of
+// clockwise facing facets should equal the number of counterclockwise facing
+// facets.
+
 namespace igl {
     namespace cgal {
-        // Computer winding number on each side of each patch.
+        // Compute winding number on each side of each patch.  All patches must
+        // be connected.  The per-patch label partitions the patches into
+        // different components, and winding number is computed for each
+        // component.  For example, ``patch_W(i, j*2)`` for ``i`` in [0, #P)
+        // and ``j`` in [0, k) is the winding number on the positive side of
+        // patch ``i`` with respect to component ``j``.  Similarly,
+        // ``patch_W(i, j*2+1)`` is the winding number on the negative side of
+        // patch ``i`` with respect to component ``j``.
+        //
+        // Patch and intersection curve can be generated using:
+        // ``igl::extract_manifold_patches(...)`` and
+        // ``igl::extract_non_manifold_edge_curves(...)``
+        //
         //
         // Inputs:
         //   V  #V by 3 list of vertices.
-        //   F  #F by 3 list of trinalges.
+        //   F  #F by 3 list of trianlges.
         //   uE #uE by 2 list of undirected edges.
         //   uE2E  #uE list of lists mapping each undirected edge to directed
         //         edges.
-        //   C  #P list of component indices.
+        //   labels  #P list of labels, ranging from 0 to k-1.
+        //           These labels partition patches into k components, and
+        //           winding number is computed for each component.
         //   P  #F list of patch indices.
         //   intersection_curves  A list of non-manifold curves that separates
         //                        the mesh into patches.
@@ -22,6 +43,9 @@ namespace igl {
         // Outputs:
         //   patch_W  #P by k*2 list of winding numbers on each side of a
         //            patch for each component.
+        //
+        // Returns:
+        //   True iff integer winding numbers can be consistently assigned.
         template<
             typename DerivedV,
             typename DerivedF,
@@ -35,26 +59,25 @@ namespace igl {
                 const Eigen::PlainObjectBase<DerivedF>& F,
                 const Eigen::PlainObjectBase<DeriveduE>& uE,
                 const std::vector<std::vector<uE2EType> >& uE2E,
-                const Eigen::PlainObjectBase<DerivedC>& C,
+                const Eigen::PlainObjectBase<DerivedC>& labels,
                 const Eigen::PlainObjectBase<DerivedP>& P,
                 const std::vector<std::vector<size_t> >& intersection_curves,
                 Eigen::PlainObjectBase<DerivedW>& patch_W);
 
-        // Compute winding number on each side of the face.
-        //
-        // This method assumes the input mesh (V, F, I) forms a single connected
-        // component.
+        // Compute winding number on each side of the face.  The input mesh must
+        // be a single edge-connected component.
         //
         // Inputs:
         //   V  #V by 3 list of vertex positions.
         //   F  #F by 3 list of triangle indices into V.
-        //   C  #F list of effective face indices ranging from 0 to k-1.
+        //   labels  #F list of facet labels ranging from 0 to k-1.
         //
         // Output:
-        //   W  #F by 2*k list of winding numbers.  W(i,2*j) is the winding
-        //   number of submesh j on the positive side of facet i, and
-        //   W(i, 2*j+1) is the winding number of submesh j on the negative
-        //   side of facet i.
+        //   W  #F by 2*k list of winding numbers.
+        //      ``W(i, 2*j)`` is the winding number on the positive side of
+        //      facet ``i`` with respect to facets labelled ``j``.
+        //      ``W(i, 2*j+1)`` is the winding number on the negative side of
+        //      facet ``i`` with respect to facets labelled ``j``.
         //
         // Returns:
         //   True iff integer winding number can be consistently assigned.
@@ -66,7 +89,7 @@ namespace igl {
         IGL_INLINE bool propagate_winding_numbers_single_component(
                 const Eigen::PlainObjectBase<DerivedV>& V,
                 const Eigen::PlainObjectBase<DerivedF>& F,
-                const Eigen::PlainObjectBase<DerivedC>& C,
+                const Eigen::PlainObjectBase<DerivedC>& labels,
                 Eigen::PlainObjectBase<DerivedW>& W);
 
         // Compute the winding number on each side of the face.
@@ -81,6 +104,9 @@ namespace igl {
         //   W  #F by 2 list of winding numbers.  W(i,0) is the winding number
         //   on the positive side of facet i, and W(i, 1) is the winding
         //   number on the negative side of facet I[i].
+        //
+        // Returns:
+        //   True iff integer winding number can be consistently assigned.
         template<
             typename DerivedV,
             typename DerivedF,
@@ -92,18 +118,20 @@ namespace igl {
 
         // Compute winding number on each side of the face.  The input mesh
         // could contain multiple connected components.  The input mesh must
-        // representa the boundary of a valid 3D volume, which means it is
+        // represent the boundary of a valid 3D volume, which means it is
         // closed, consistently oriented and induces integer winding numbers.
         //
         // Inputs:
         //   V  #V by 3 list of vertex positions.
         //   F  #F by 3 list of triangle indices into V.
-        //   C  #F list of facet labels.
+        //   labels  #F list of facet labels ranging from 0 to k-1.
         //
         // Output:
-        //   W  #I by 2 list of winding numbers.  W(i,0) is the winding number
-        //   on the positive side of facet I[i], and W(i, 1) is the winding
-        //   number on the negative side of facet I[i].
+        //   W  #F by k*2 list of winding numbers.  ``W(i,j*2)`` is the winding
+        //      number on the positive side of facet ``i`` with respect to the
+        //      facets labeled ``j``.  Similarly, ``W(i,j*2+1)`` is the winding
+        //      number on the negative side of facet ``i`` with respect to the
+        //      facets labeled ``j``.
         template<
             typename DerivedV,
             typename DerivedF,
