a42c01822e2868ce28716925b5c39b7155df5463
Qingnan Zhou <qnzhou@gmail.com>
2015-11-04 14:25:26 -0500

    Added copyright comments...

--- include/igl/boolean/mesh_boolean_beta.cpp
+++ include/igl/boolean/mesh_boolean_beta.cpp
@@ -1,3 +1,11 @@
+// This file is part of libigl, a simple c++ geometry processing library.
+// 
+// Copyright (C) 2015 Qingnan Zhou <qnzhou@gmail.com>
+// 
+// This Source Code Form is subject to the terms of the Mozilla Public License 
+// v. 2.0. If a copy of the MPL was not distributed with this file, You can 
+// obtain one at http://mozilla.org/MPL/2.0/.
+//
 #include "mesh_boolean_beta.h"
 #include <igl/cgal/assign_scalar.h>
 #include <igl/cgal/propagate_winding_numbers.h>
