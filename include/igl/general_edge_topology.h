512bf5965ca279865ddd14268dda235fa9cbd9a1
Amir Vaxman <amirvaxman@Amirs-MacBook-Pro.local>
2015-11-10 19:39:59 +0100

    General Edge Toplogy

---
+++ include/igl/general_edge_topology.h
@@ -0,0 +1,36 @@
+// This file is part of libigl, a simple c++ geometry processing library.
+// 
+// Copyright (C) 2013 Alec Jacobson <alecjacobson@gmail.com>
+// 
+// This Source Code Form is subject to the terms of the Mozilla Public License 
+// v. 2.0. If a copy of the MPL was not distributed with this file, You can 
+// obtain one at http://mozilla.org/MPL/2.0/.
+#ifndef IGL_GENERAL_EDGE_TOPOLOGY_H
+#define IGL_GENERAL_EDGE_TOPOLOGY_H
+
+#include "igl_inline.h"
+
+#include <Eigen/Core>
+#include <vector>
+
+namespace igl 
+{
+  // Initialize Edges and their topological relations
+  
+  // Output:
+  // EV  : #Ex2, Stores the edge description as pair of indices to vertices
+  // FE : #Fx3, Stores the Triangle-Edge relation
+  // EF : #Ex2: Stores the Edge-Triangle relation
+template <typename DerivedF>
+  IGL_INLINE void general_edge_topology(
+    const Eigen::PlainObjectBase<DerivedF>& gF,
+    Eigen::MatrixXi& gEV,
+    Eigen::MatrixXi& gFE,
+    Eigen::MatrixXi& gEF);
+}
+
+#ifndef IGL_STATIC_LIBRARY
+#  include "general_edge_topology.cpp"
+#endif
+
+#endif
