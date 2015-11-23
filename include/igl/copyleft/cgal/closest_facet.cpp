9ac853eb951202b18d21024d54f5df75753e3f8a
Qingnan Zhou <qnzhou@gmail.com>
2015-11-23 11:14:51 -0500

    Do not error when query point is coplanar with closest boundary face.
    Former-commit-id: 783e82942e2b29ff3b3beda1a1fc44431251661e

--- include/igl/copyleft/cgal/closest_facet.cpp
+++ include/igl/copyleft/cgal/closest_facet.cpp
@@ -89,8 +89,10 @@ IGL_INLINE void igl::copyleft::cgal::closest_facet(
             case CGAL::NEGATIVE:
                 return false;
             case CGAL::COPLANAR:
-                throw std::runtime_error(
-                        "It seems input mesh contains self intersection");
+                // Warning:
+                // This can only happen if fid contains a boundary edge.
+                // Catergorized this ambiguous case as negative side.
+                return false;
             default:
                 throw std::runtime_error("Unknown CGAL state.");
         }
