efff0ee92816de36b41916643f965b95fae2911e
Alec Jacobson <alecjacobson@gmail.com>
2016-02-23 15:34:45 -0500

    translation widget

--- include/igl/forward_kinematics.cpp
+++ include/igl/forward_kinematics.cpp
@@ -7,6 +7,7 @@
 // obtain one at http://mozilla.org/MPL/2.0/.
 #include "forward_kinematics.h"
 #include <functional>
+#include <iostream>
 
 IGL_INLINE void igl::forward_kinematics(
   const Eigen::MatrixXd & C,
@@ -14,6 +15,7 @@ IGL_INLINE void igl::forward_kinematics(
   const Eigen::VectorXi & P,
   const std::vector<
     Eigen::Quaterniond,Eigen::aligned_allocator<Eigen::Quaterniond> > & dQ,
+  const std::vector<Eigen::Vector3d> & dT,
   std::vector<
     Eigen::Quaterniond,Eigen::aligned_allocator<Eigen::Quaterniond> > & vQ,
   std::vector<Eigen::Vector3d> & vT)
@@ -23,9 +25,11 @@ IGL_INLINE void igl::forward_kinematics(
   const int m = BE.rows(); 
   assert(m == P.rows());
   assert(m == (int)dQ.size());
+  assert(m == (int)dT.size());
   vector<bool> computed(m,false);
   vQ.resize(m);
   vT.resize(m);
+  // Dynamic programming
   function<void (int) > fk_helper = [&] (int b)
   {
     if(!computed[b])
@@ -35,7 +39,7 @@ IGL_INLINE void igl::forward_kinematics(
         // base case for roots
         vQ[b] = dQ[b];
         const Vector3d r = C.row(BE(b,0)).transpose();
-        vT[b] = r-dQ[b]*r;
+        vT[b] = r-dQ[b]*r + dT[b];
       }else
       {
         // Otherwise first compute parent's
@@ -43,7 +47,7 @@ IGL_INLINE void igl::forward_kinematics(
         fk_helper(p);
         vQ[b] = vQ[p] * dQ[b];
         const Vector3d r = C.row(BE(b,0)).transpose();
-        vT[b] = vT[p] - vQ[b]*r + vQ[p]*r;
+        vT[b] = vT[p] - vQ[b]*r + vQ[p]*(r + dT[b]);
       }
       computed[b] = true;
     }
@@ -60,13 +64,28 @@ IGL_INLINE void igl::forward_kinematics(
   const Eigen::VectorXi & P,
   const std::vector<
     Eigen::Quaterniond,Eigen::aligned_allocator<Eigen::Quaterniond> > & dQ,
+  std::vector<
+    Eigen::Quaterniond,Eigen::aligned_allocator<Eigen::Quaterniond> > & vQ,
+  std::vector<Eigen::Vector3d> & vT)
+{
+  std::vector<Eigen::Vector3d> dT(BE.rows(),Eigen::Vector3d(0,0,0));
+  return forward_kinematics(C,BE,P,dQ,dT,vQ,vT);
+}
+
+IGL_INLINE void igl::forward_kinematics(
+  const Eigen::MatrixXd & C,
+  const Eigen::MatrixXi & BE,
+  const Eigen::VectorXi & P,
+  const std::vector<
+    Eigen::Quaterniond,Eigen::aligned_allocator<Eigen::Quaterniond> > & dQ,
+  const std::vector<Eigen::Vector3d> & dT,
   Eigen::MatrixXd & T)
 {
   using namespace Eigen;
   using namespace std;
   vector< Quaterniond,aligned_allocator<Quaterniond> > vQ;
   vector< Vector3d> vT;
-  forward_kinematics(C,BE,P,dQ,vQ,vT);
+  forward_kinematics(C,BE,P,dQ,dT,vQ,vT);
   const int dim = C.cols();
   T.resize(BE.rows()*(dim+1),dim);
   for(int e = 0;e<BE.rows();e++)
@@ -77,6 +96,18 @@ IGL_INLINE void igl::forward_kinematics(
     T.block(e*(dim+1),0,dim+1,dim) =
       a.matrix().transpose().block(0,0,dim+1,dim);
   }
+}
+
+IGL_INLINE void igl::forward_kinematics(
+  const Eigen::MatrixXd & C,
+  const Eigen::MatrixXi & BE,
+  const Eigen::VectorXi & P,
+  const std::vector<
+    Eigen::Quaterniond,Eigen::aligned_allocator<Eigen::Quaterniond> > & dQ,
+  Eigen::MatrixXd & T)
+{
+  std::vector<Eigen::Vector3d> dT(BE.rows(),Eigen::Vector3d(0,0,0));
+  return forward_kinematics(C,BE,P,dQ,dT,T);
 }
 
 #ifdef IGL_STATIC_LIBRARY
