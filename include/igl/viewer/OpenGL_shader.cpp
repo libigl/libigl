5d4e0e7fb9e1b9e8d1e9a64857fe38899d45c01b
Romain Prévost <rprevost@inf.ethz.ch>
2016-03-08 01:32:41 +0100

    glew fix

--- include/igl/viewer/OpenGL_shader.cpp
+++ include/igl/viewer/OpenGL_shader.cpp
@@ -8,21 +8,6 @@
 
 #include "OpenGL_shader.h"
 
-#ifndef __APPLE__
-#  define GLEW_STATIC
-#  include <GL/glew.h>
-#endif
-
-#ifdef __APPLE__
-#   include <OpenGL/gl3.h>
-#   define __gl_h_ /* Prevent inclusion of the old gl.h */
-#else
-#   ifdef _WIN32
-#       include <windows.h>
-#   endif
-#   include <GL/gl.h>
-#endif
-
 #include <iostream>
 #include <fstream>
 
