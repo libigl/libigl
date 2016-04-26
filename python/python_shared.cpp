600e537230b75ab5d7ab1fe46605c72fda263239
Sebastian Koch <s.koch@tu-berlin.de>
2016-04-26 12:59:41 +0200

    Automatically generated python docstrings with parsed documentation from header files

--- python/python_shared.cpp
+++ python/python_shared.cpp
@@ -16,7 +16,65 @@ extern void python_export_igl_comiso(py::module &);
 
 PYTHON_PLUGIN(pyigl) {
     py::init_threading();
-    py::module m("pyigl", "Python wrappers for libigl");
+    py::module m("pyigl", R"pyigldoc(
+        Python wrappers for libigl
+        --------------------------
+
+        .. currentmodule:: pyigl
+
+        .. autosummary::
+           :toctree: _generate
+
+           principal_curvature
+           local_basis
+           cotmatrix
+           floor
+           slice
+           per_face_normals
+           ARAPEnergyType
+           readOFF
+           per_vertex_normals
+           sortrows
+           barycenter
+           jet
+           eigs
+           per_corner_normals
+           massmatrix
+           colon
+           rotate_vectors
+           read_triangle_mesh
+           SolverStatus
+           gaussian_curvature
+           avg_edge_length
+           lscm
+           find_cross_field_singularities
+           parula
+           setdiff
+           comb_frame_field
+           map_vertices_to_circle
+           writeOBJ
+           active_set
+           boundary_facets
+           compute_frame_field_bisectors
+           readOBJ
+           cut_mesh_from_singularities
+           readDMAT
+           doublearea
+           min_quad_with_fixed
+           unique
+           arap
+           cross_field_missmatch
+           grad
+           slice_into
+           n_polyvector
+           harmonic
+           boundary_loop
+           comb_cross_field
+           invert_diag
+           copyleft_comiso_miq
+           copyleft_comiso_nrosy
+
+    )pyigldoc");
 
     python_export_vector(m);
     python_export_igl(m);
