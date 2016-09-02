d3b3d2cd4908232f9b25944da10d81a15960c560
Olga Diamanti <olga.diam@gmail.com>
2016-09-02 15:37:09 -0700

    entry in tutorial file and image for tutorial 511_PolyVectorFieldGeneral

--- tutorial/511_PolyVectorFieldGeneral/main.cpp
+++ tutorial/511_PolyVectorFieldGeneral/main.cpp
@@ -177,5 +177,7 @@ int main(int argc, char *argv[])
   viewer.callback_key_down = &key_down;
   viewer.core.show_lines = false;
   key_down(viewer,'3',0);
+  std::cerr << " *** Press keys 1-9 to select number of vectors per point. ***" << std::endl;
+  
   viewer.launch();
 }
