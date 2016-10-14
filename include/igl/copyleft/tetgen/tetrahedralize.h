ab99df7a649edd3b9d8c081805e237a6bd0ac679
pranav <pranav8iisc@gmail.com>
2016-10-14 10:46:40 +0530

    Update tetrahedralize.h

--- include/igl/copyleft/tetgen/tetrahedralize.h
+++ include/igl/copyleft/tetgen/tetrahedralize.h
@@ -123,6 +123,50 @@ namespace igl
         Eigen::PlainObjectBase<DerivedTT>& TT,
         Eigen::PlainObjectBase<DerivedTF>& TF,
         Eigen::PlainObjectBase<DerivedTM>& TM);
+   
+	
+	// Mesh the interior of a surface mesh (V,F) using tetgen
+      		//
+      		// Inputs:
+      		//   V  #V by 3 vertex position list
+      		//   F  #F list of polygon face indices into V (0-indexed)
+      		//   switches  string of tetgen options (See tetgen documentation) e.g.
+      		//     "pq1.414a0.01" tries to mesh the interior of a given surface with
+      		//       quality and area constraints
+      		//     "" will mesh the convex hull constrained to pass through V (ignores F)
+      		// Outputs:
+      		//   TV  #V by 3 vertex position list
+      		//   TT  #T by 4 list of tet face indices
+      		//   TF  #F by 3 list of triangle face indices
+      		// Returns status:
+      		//   0 success
+      		//   1 tetgen threw exception
+      		//   2 tetgen did not crash but could not create any tets (probably there are
+      		//     holes, duplicate faces etc.)
+      		//   -1 other error      
+    template <
+	typename DerivedV,
+	typename DerivedF,
+	typename DerivedR,
+	typename DerivedH,
+	    			typename DerivedVM,
+				typename DerivedFM,
+	typename DerivedTV,
+	typename DerivedTT,
+	typename DerivedTF,
+	typename DerivedTM>    
+      IGL_INLINE int tetrahedralize(
+	const Eigen::PlainObjectBase<DerivedV>& V,
+	const Eigen::PlainObjectBase<DerivedF>& F,	
+	const Eigen::PlainObjectBase<DerivedR>& R,
+	const Eigen::PlainObjectBase<DerivedH>& H,
+	const std::string switches,      
+	const Eigen::PlainObjectBase<DerivedVM>& VM,
+	const Eigen::PlainObjectBase<DerivedFM>& FM
+	const Eigen::PlainObjectBase<DerivedTV>& TV,
+	const Eigen::PlainObjectBase<DerivedTT>& TT,
+	const Eigen::PlainObjectBase<DerivedTF>& TF
+      )
     }
   }
 }
