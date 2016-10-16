b630bf3aa9ed328eb4be61c50acbbcd8b69420d1
Qingnan Zhou <qzhou@adobe.com>
2016-10-16 14:35:15 -0700

    Minor fix to get library compiled.

--- include/igl/flip_edge.cpp
+++ include/igl/flip_edge.cpp
@@ -6,6 +6,8 @@
 // v. 2.0. If a copy of the MPL was not distributed with this file, You can 
 // obtain one at http://mozilla.org/MPL/2.0/.
 
+#include "flip_edge.h"
+
 template <
   typename DerivedF,
   typename DerivedE,
