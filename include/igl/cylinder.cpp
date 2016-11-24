3686ee61cf8fd23a7d1f9a88d338983cc58c42eb
Alec Jacobson <alecjacobson@gmail.com>
2016-11-24 09:27:33 -0500

    templates

--- include/igl/cylinder.cpp
+++ include/igl/cylinder.cpp
@@ -39,3 +39,7 @@ IGL_INLINE void igl::cylinder(
   }
   assert(f == F.rows());
 }
+
+#ifdef IGL_STATIC_LIBRARY
+template void igl::cylinder<Eigen::Matrix<double, -1, -1, 0, -1, -1>, Eigen::Matrix<int, -1, -1, 0, -1, -1> >(int, int, Eigen::PlainObjectBase<Eigen::Matrix<double, -1, -1, 0, -1, -1> >&, Eigen::PlainObjectBase<Eigen::Matrix<int, -1, -1, 0, -1, -1> >&);
+#endif
