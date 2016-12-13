b00e745985d2602ed0b916762971980579d42b50
Alec Jacobson <alecjacobson@gmail.com>
2016-12-13 18:28:52 -0500

    adding missing copyright/license headers
    Former-commit-id: 9e24934373d32e319d9faeb54374f66f111cee4b

--- include/igl/polyvector_field_matchings.cpp
+++ include/igl/polyvector_field_matchings.cpp
@@ -1,3 +1,10 @@
+// This file is part of libigl, a simple c++ geometry processing library.
+// 
+// Copyright (C) 2016 Alec Jacobson <alecjacobson@gmail.com>
+// 
+// This Source Code Form is subject to the terms of the Mozilla Public License 
+// v. 2.0. If a copy of the MPL was not distributed with this file, You can 
+// obtain one at http://mozilla.org/MPL/2.0/.
 #include <igl/polyvector_field_matchings.h>
 #include <igl/edge_topology.h>
 #include <igl/per_face_normals.h>
