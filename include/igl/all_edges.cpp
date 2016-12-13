9e24934373d32e319d9faeb54374f66f111cee4b
Alec Jacobson <alecjacobson@gmail.com>
2016-12-13 18:28:52 -0500

    adding missing copyright/license headers

--- include/igl/all_edges.cpp
+++ include/igl/all_edges.cpp
@@ -13,32 +13,33 @@ IGL_INLINE void igl::all_edges(
   Eigen::PlainObjectBase<DerivedE> & E)
 {
   E.resize(F.rows()*F.cols(),F.cols()-1);
+  typedef typename DerivedE::Scalar EScalar;
   switch(F.cols())
   {
     case 4:
-      E.block(0*F.rows(),0,F.rows(),1) = F.col(1);
-      E.block(0*F.rows(),1,F.rows(),1) = F.col(3);
-      E.block(0*F.rows(),2,F.rows(),1) = F.col(2);
+      E.block(0*F.rows(),0,F.rows(),1) = F.col(1).template cast<EScalar>();
+      E.block(0*F.rows(),1,F.rows(),1) = F.col(3).template cast<EScalar>();
+      E.block(0*F.rows(),2,F.rows(),1) = F.col(2).template cast<EScalar>();
 
-      E.block(1*F.rows(),0,F.rows(),1) = F.col(0);
-      E.block(1*F.rows(),1,F.rows(),1) = F.col(2);
-      E.block(1*F.rows(),2,F.rows(),1) = F.col(3);
+      E.block(1*F.rows(),0,F.rows(),1) = F.col(0).template cast<EScalar>();
+      E.block(1*F.rows(),1,F.rows(),1) = F.col(2).template cast<EScalar>();
+      E.block(1*F.rows(),2,F.rows(),1) = F.col(3).template cast<EScalar>();
 
-      E.block(2*F.rows(),0,F.rows(),1) = F.col(0);
-      E.block(2*F.rows(),1,F.rows(),1) = F.col(3);
-      E.block(2*F.rows(),2,F.rows(),1) = F.col(1);
+      E.block(2*F.rows(),0,F.rows(),1) = F.col(0).template cast<EScalar>();
+      E.block(2*F.rows(),1,F.rows(),1) = F.col(3).template cast<EScalar>();
+      E.block(2*F.rows(),2,F.rows(),1) = F.col(1).template cast<EScalar>();
 
-      E.block(3*F.rows(),0,F.rows(),1) = F.col(0);
-      E.block(3*F.rows(),1,F.rows(),1) = F.col(1);
-      E.block(3*F.rows(),2,F.rows(),1) = F.col(2);
+      E.block(3*F.rows(),0,F.rows(),1) = F.col(0).template cast<EScalar>();
+      E.block(3*F.rows(),1,F.rows(),1) = F.col(1).template cast<EScalar>();
+      E.block(3*F.rows(),2,F.rows(),1) = F.col(2).template cast<EScalar>();
       return;
     case 3:
-      E.block(0*F.rows(),0,F.rows(),1) = F.col(1);
-      E.block(0*F.rows(),1,F.rows(),1) = F.col(2);
-      E.block(1*F.rows(),0,F.rows(),1) = F.col(2);
-      E.block(1*F.rows(),1,F.rows(),1) = F.col(0);
-      E.block(2*F.rows(),0,F.rows(),1) = F.col(0);
-      E.block(2*F.rows(),1,F.rows(),1) = F.col(1);
+      E.block(0*F.rows(),0,F.rows(),1) = F.col(1).template cast<EScalar>();
+      E.block(0*F.rows(),1,F.rows(),1) = F.col(2).template cast<EScalar>();
+      E.block(1*F.rows(),0,F.rows(),1) = F.col(2).template cast<EScalar>();
+      E.block(1*F.rows(),1,F.rows(),1) = F.col(0).template cast<EScalar>();
+      E.block(2*F.rows(),0,F.rows(),1) = F.col(0).template cast<EScalar>();
+      E.block(2*F.rows(),1,F.rows(),1) = F.col(1).template cast<EScalar>();
       return;
   }
 }
--- include/igl/polyvector_field_cut_mesh_with_singularities.cpp
+++ include/igl/polyvector_field_cut_mesh_with_singularities.cpp
@@ -1,3 +1,10 @@
+// This file is part of libigl, a simple c++ geometry processing library.
+// 
+// Copyright (C) 2016 Alec Jacobson <alecjacobson@gmail.com>
+// 
+// This Source Code Form is subject to the terms of the Mozilla Public License 
+// v. 2.0. If a copy of the MPL was not distributed with this file, You can 
+// obtain one at http://mozilla.org/MPL/2.0/.
 #include <igl/polyvector_field_cut_mesh_with_singularities.h>
 #include <igl/dijkstra.h>
 #include <igl/vertex_triangle_adjacency.h>
