c41afa77eca9fc085744906e4fe8964dcdb12965
Alec Jacobson <alecjacobson@gmail.com>
2017-01-02 11:01:44 -0600

    better templates
    Former-commit-id: 595cd9a87216917e61edf32039d3e06b9000ca44

--- include/igl/setxor.cpp
+++ include/igl/setxor.cpp
@@ -2,8 +2,6 @@
 #include "setdiff.h"
 #include "setunion.h"
 #include "slice.h"
-#include <iostream>
-#include <igl/matlab_format.h>
 
 template <
   typename DerivedA,
@@ -21,19 +19,8 @@ IGL_INLINE void igl::setxor(
   DerivedC AB,BA;
   DerivedIA IAB,IBA;
   setdiff(A,B,AB,IAB);
-  std::cout<<igl::matlab_format(AB.transpose(),"AB")<<std::endl;
-  std::cout<<igl::matlab_format(IAB.transpose().array()+1,"IAB")<<std::endl;
   setdiff(B,A,BA,IBA);
-  std::cout<<igl::matlab_format(BA.transpose(),"BA")<<std::endl;
-  std::cout<<igl::matlab_format(IBA.transpose().array()+1,"IBA")<<std::endl;
   setunion(AB,BA,C,IA,IB);
-
-  std::cout<<igl::matlab_format(C.transpose(),"C")<<std::endl;
-  std::cout<<igl::matlab_format(IA.transpose().array()+1,"IA")<<std::endl;
-  std::cout<<igl::matlab_format(IB.transpose().array()+1,"IB")<<std::endl;
   slice(IAB,DerivedIA(IA),IA);
   slice(IBA,DerivedIB(IB),IB);
-
-  std::cout<<igl::matlab_format(IA.transpose().array()+1,"IA")<<std::endl;
-  std::cout<<igl::matlab_format(IB.transpose().array()+1,"IB")<<std::endl;
 }
