e8b6c4df8edaec3be7e15026096522b2a13a08c4
Alec Jacobson <alecjacobson@gmail.com>
2017-01-03 01:34:09 -0500

    missing includes

--- include/igl/straighten_seams.cpp
+++ include/igl/straighten_seams.cpp
@@ -1,3 +1,10 @@
+// This file is part of libigl, a simple c++ geometry processing library.
+//
+// Copyright (C) 2017 Alec Jacobson <alecjacobson@gmail.com>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License
+// v. 2.0. If a copy of the MPL was not distributed with this file, You can
+// obtain one at http://mozilla.org/MPL/2.0/.
 #include "straighten_seams.h"
 #include "on_boundary.h"
 #include "sparse.h"
@@ -9,6 +16,8 @@
 #include "unique_simplices.h"
 #include "adjacency_matrix.h"
 #include "setxor.h"
+#include "edges_to_path.h"
+#include "ramer_douglas_peucker.h"
 #include "components.h"
 #include "ears.h"
 #include "slice.h"
