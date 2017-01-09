aabf74b28055530131e31cbb59e6e437b41a5ca4
Alec Jacobson <alecjacobson@gmail.com>
2017-01-08 20:49:00 -0500

    use () instead of coeff()
    Former-commit-id: 89a21479ecb345d49d06e7d51ae9a6855f3222a1

--- include/igl/unique.cpp
+++ include/igl/unique.cpp
@@ -222,7 +222,7 @@ IGL_INLINE void igl::unique_rows(
 
   auto index_equal = [&sortA, &num_cols](const size_t i, const size_t j) {
     for (size_t c=0; c<num_cols; c++) {
-      if (sortA.coeff(i,c) != sortA.coeff(j,c))
+      if (sortA(i,c) != sortA(j,c))
         return false;
     }
     return true;
