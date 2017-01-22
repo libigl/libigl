1e68574e5d6f8fca943626e9838644d6a78e8075
Alec Jacobson <alecjacobson@gmail.com>
2017-01-22 18:59:28 -0500

    rm debug debris

--- include/igl/opengl2/project.cpp
+++ include/igl/opengl2/project.cpp
@@ -20,66 +20,14 @@ IGL_INLINE int igl::opengl2::project(
   double* winZ)
 {
   using namespace std;
-#ifdef EXTREME_VERBOSE
-  cout<<"project();"<<endl;
-#endif
   // Put model, projection, and viewport matrices into double arrays
   double MV[16];
   double P[16];
   int VP[4];
   glGetDoublev(GL_MODELVIEW_MATRIX,  MV);
-
-#ifdef EXTREME_VERBOSE
-  cout<<"MV=["<<endl<<
-    MV[0]<<" "<< MV[1]<<" "<< MV[2]<<" "<< MV[3]<<" "<<endl<<
-    MV[4]<<" "<< MV[5]<<" "<< MV[6]<<" "<< MV[7]<<" "<<endl<<
-    MV[8]<<" "<< MV[9]<<" "<< MV[10]<<" "<< MV[11]<<" "<<endl<<
-    MV[12]<<" "<< MV[13]<<" "<< MV[14]<<" "<< MV[15]<<" "<<endl<<
-    "];"<<endl;
-#endif
-#ifndef NDEBUG
-  igl::opengl::report_gl_error();
-#endif
-
   glGetDoublev(GL_PROJECTION_MATRIX, P);
-
-#ifdef EXTREME_VERBOSE
-  cout<<"P=["<<endl<<
-    P[0]<<" "<< P[1]<<" "<< P[2]<<" "<< P[3]<<" "<<endl<<
-    P[4]<<" "<< P[5]<<" "<< P[6]<<" "<< P[7]<<" "<<endl<<
-    P[8]<<" "<< P[9]<<" "<< P[10]<<" "<< P[11]<<" "<<endl<<
-    P[12]<<" "<< P[13]<<" "<< P[14]<<" "<< P[15]<<" "<<endl<<
-    "];"<<endl;
-#endif
-#ifndef NDEBUG
-  igl::opengl::report_gl_error();
-#endif
-
   glGetIntegerv(GL_VIEWPORT, VP);
-
-#ifdef EXTREME_VERBOSE
-  cout<<"VP=["<<endl<<
-    VP[0]<<" "<< VP[1]<<" "<< VP[2]<<" "<< VP[3]<<" "<<endl<<
-    "];"<<endl;
-#endif
-#ifndef NDEBUG
-  igl::opengl::report_gl_error();
-#endif
-
-#ifdef EXTREME_VERBOSE
-  cout<<"obj=["<<endl<<
-    objX<<" "<< objY<<" "<< objZ<<endl<<
-    "];"<<endl;
-#endif
-
-
   int ret = gluProject(objX,objY,objZ,MV,P,VP,winX,winY,winZ);
-
-#ifdef EXTREME_VERBOSE
-  cout<<"win=["<<endl<<
-    *winX<<" "<< *winY<<" "<< *winZ<<endl<<
-    "];"<<endl;
-#endif
   return ret;
 }
 
