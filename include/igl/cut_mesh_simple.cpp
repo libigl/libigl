11a60ece6fff9e2dd283b823b7d82ad945a2dfd2
schuellc <schuellchr@gmail.com>
2017-02-08 12:33:24 +0100

    Add cut_mesh_simple documentation

--- include/igl/cut_mesh_simple.cpp
+++ include/igl/cut_mesh_simple.cpp
@@ -1,3 +1,11 @@
+// This file is part of libigl, a simple c++ geometry processing library.
+// 
+// Copyright (C) 2016 Christian Schüller <schuellchr@gmail.com>
+// 
+// This Source Code Form is subject to the terms of the Mozilla Public License 
+// v. 2.0. If a copy of the MPL was not distributed with this file, You can 
+// obtain one at http://mozilla.org/MPL/2.0/.
+
 #include <igl/cut_mesh_simple.h>
 
 #include <igl/triangle_triangle_adjacency.h>
--- include/igl/cut_mesh_simple.h
+++ include/igl/cut_mesh_simple.h
@@ -1,10 +1,11 @@
 // This file is part of libigl, a simple c++ geometry processing library.
-//
-// Copyright (C) 2015 Christian Schüller <schuellchr@gmail.com>
-//
-// This Source Code Form is subject to the terms of the Mozilla Public License
-// v. 2.0. If a copy of the MPL was not distributed with this file, You can
+// 
+// Copyright (C) 2016 Christian Schüller <schuellchr@gmail.com>
+// 
+// This Source Code Form is subject to the terms of the Mozilla Public License 
+// v. 2.0. If a copy of the MPL was not distributed with this file, You can 
 // obtain one at http://mozilla.org/MPL/2.0/.
+
 #ifndef IGL_CUT_MESH_SIMPLE
 #define IGL_CUT_MESH_SIMPLE
 #include "igl_inline.h"
@@ -17,16 +18,20 @@
 namespace igl
 {
 
-  // Description
-  // 
-  //
+  // Description:
+  // Cuts a triangle mesh along edges. 
+ 
   // Inputs:
-  //   V:
-  //   T:
-  //   cut: needs to be a valid vertex sequence 
-  //
+  //   V: #V by 3 list of the vertex positions
+  //   T: #F by 3 list of the faces (must be triangles)
+  //   cut: A sequence of vertex ids defining the cut along edges (e.g. {1,2,3} defines two edges: {1,2} and {2,3})
+  //   cuts: A list with multiple cuts. Provided edges don't need to be unique, can intersect each other or even be boundary edges.
+  //   
   // Outputs:
-  //
+  //  cutVertices: list of correspponding unique vertex id pairs after cutting
+  //  cutVerticesLink: indices of 'cutVertices' corresponding to vertices in 'cut' 
+  //  cutHalfedges: list of correspponding unique halfedges [triangleAId,edgeInAId,triangleBId,edgeInBId] (e.g. T(triangleAId,edgeInAId))
+  //  cutHalfedgesLink: indices of 'cutHalfedges' corresponding to vertices in 'cut' (1. vertex id in 'cut' corresponds to 1. halfedge in 'cutHalfedges')
   template <typename DerivedS,typename DerivedI>
   void cut_mesh(Eigen::MatrixBase<DerivedS>& V,
                 Eigen::MatrixBase<DerivedI>& F,
