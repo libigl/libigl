3567e2d9b0eda5400e16e6ccb1cd953d6cce788f
skoch <s.koch@tu-berlin.de>
2017-02-14 18:50:50 +0100

    Update vector binding to work with new pybind11
    Former-commit-id: 9bfa4df10758d90d6ec12d49cad2ee9082e360fc

--- python/py_igl/py_read_triangle_mesh.cpp
+++ python/py_igl/py_read_triangle_mesh.cpp
@@ -1,17 +1,6 @@
 m.def("read_triangle_mesh", []
 (
   const std::string str,
-  std::vector<std::vector<double> >& V,
-  std::vector<std::vector<int> >& F
-)
-{
-  return igl::read_triangle_mesh(str,V,F);
-}, __doc_igl_read_triangle_mesh,
-py::arg("str"), py::arg("V"), py::arg("F"));
-
-m.def("read_triangle_mesh", []
-(
-  const std::string str,
   Eigen::MatrixXd& V,
   Eigen::MatrixXi& F
 )
@@ -34,3 +23,14 @@ m.def("read_triangle_mesh", []
   return igl::read_triangle_mesh(str,V,F,dir,base,ext,name);
 }, __doc_igl_read_triangle_mesh,
 py::arg("str"), py::arg("V"), py::arg("F"), py::arg("dir"), py::arg("base"), py::arg("ext"), py::arg("name"));
+
+m.def("read_triangle_mesh", []
+(
+  const std::string str,
+  std::vector<std::vector<double> >& V,
+  std::vector<std::vector<int> >& F
+)
+{
+  return igl::read_triangle_mesh(str,V,F);
+}, __doc_igl_read_triangle_mesh,
+py::arg("str"), py::arg("V"), py::arg("F"));
