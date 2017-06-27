fe60b1dd0c061e541d64e29ca2a0e74a233b95cb
Jérémie Dumas <jeremie.dumas@ens-lyon.org>
2017-06-27 15:59:55 -0400

    Improve viewer resize function.
    Former-commit-id: 64370663c14bc7eb318c606983102f19730acfe2

--- include/igl/viewer/Viewer.h
+++ include/igl/viewer/Viewer.h
@@ -119,7 +119,8 @@ namespace viewer
     IGL_INLINE void draw();
 
     // OpenGL context resize
-    IGL_INLINE void resize(int w,int h);
+    IGL_INLINE void resize(int w,int h); // explicitly set window size
+    IGL_INLINE void resize_callback(int w,int h); // external resize due to user interaction
 
     // Helper functions
     IGL_INLINE void snap_to_canonical_quaternion();
