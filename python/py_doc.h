89248fc18f286566564cd7ab4d8cfd576788e48e
Daniel Crispell <dan@visionsystemsinc.com>
2017-06-30 21:57:22 -0400

    add python binding for writePLY

--- python/py_doc.h
+++ python/py_doc.h
@@ -116,3 +116,4 @@ extern const char *__doc_igl_winding_number_3;
 extern const char *__doc_igl_winding_number_2;
 extern const char *__doc_igl_writeMESH;
 extern const char *__doc_igl_writeOBJ;
+extern const char *__doc_igl_writePLY;
