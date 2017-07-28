409aee509d37ce1e49c1040cbe3ef011d8a0cad1
Francis Williams <francis@fwilliams.org>
2017-07-28 16:36:42 -0400

    Marching cubes over sparse indexed grids
    Former-commit-id: 2774fec8f47d06910631e6f81a18813c1e7b7524

--- include/igl/copyleft/marching_cubes.h
+++ include/igl/copyleft/marching_cubes.h
@@ -39,19 +39,48 @@ namespace igl
     //   faces  #F by 3 list of mesh triangle indices
     //
     template <
-      typename Derivedvalues, 
-      typename Derivedpoints, 
-      typename Derivedvertices, 
-      typename DerivedF>
+      typename DerivedValues,
+      typename DerivedPoints,
+      typename DerivedVertices,
+      typename DerivedFaces>
       IGL_INLINE void marching_cubes(
-        const Eigen::PlainObjectBase<Derivedvalues> &values,
-        const Eigen::PlainObjectBase<Derivedpoints> &points,
+        const Eigen::MatrixBase<DerivedValues> &values,
+        const Eigen::MatrixBase<DerivedPoints> &points,
         const unsigned x_res,
         const unsigned y_res,
         const unsigned z_res,
-        Eigen::PlainObjectBase<Derivedvertices> &vertices,
-        Eigen::PlainObjectBase<DerivedF> &faces);
+        Eigen::PlainObjectBase<DerivedVertices> &vertices,
+        Eigen::PlainObjectBase<DerivedFaces> &faces);
+
+      // marching_cubes( values, points, indices, vertices, faces )
+      //
+      // Perform marching cubes reconstruction on the grid cells defined by indices.
+      // The indices parameter is an nx8 dense array of index values into the points and values arrays.
+      // Each row of indices represents a cube for which to generate vertices and faces over.
+      //
+      // Input:
+      //  values  #number_of_grid_points x 1 array -- the scalar values of an
+      //    implicit function defined on the grid points (<0 in the inside of the
+      //    surface, 0 on the border, >0 outside)
+      //  points  #number_of_grid_points x 3 array -- 3-D positions of the grid
+      //    points, ordered in x,y,z order:
+      //  indices  #cubes x 8 array -- one row for each cube where each value is
+      //    the index of a vertex in points and a scalar in values.
+      //    i.e. points[indices[i, j]] = the position of the j'th vertex of the i'th cube
+      // Output:
+      //   vertices  #V by 3 list of mesh vertex positions
+      //   faces  #F by 3 list of mesh triangle indices
+      // Note: The winding direction of the cube indices will affect the output winding of the faces
+      //
+      template <typename DerivedValues, typename DerivedPoints, typename DerivedVertices, typename DerivedIndices, typename DerivedFaces>
+      IGL_INLINE void marching_cubes(
+        const Eigen::MatrixBase<DerivedValues> &values,
+        const Eigen::MatrixBase<DerivedPoints> &points,
+        const Eigen::MatrixBase<DerivedIndices> &indices,
+        Eigen::PlainObjectBase<DerivedVertices> &vertices,
+        Eigen::PlainObjectBase<DerivedFaces> &faces);
   }
+
 }
 
 #ifndef IGL_STATIC_LIBRARY
