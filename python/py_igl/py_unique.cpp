94028e5dbad8794ff89c3d6401fcfc59e24b3e28
Alec Jacobson <alecjacobson@gmail.com>
2017-08-02 16:19:57 -0700

    try to fix python errors
    Former-commit-id: 56dd532f104e2ae53bcca08d43e58ae1d6f878d3

--- python/py_igl/py_unique.cpp
+++ python/py_igl/py_unique.cpp
@@ -20,18 +20,6 @@ m.def("unique", []
 }, __doc_igl_unique,
 py::arg("A"), py::arg("C"));
 
-m.def("unique_rows", []
-(
-  const Eigen::MatrixXd& A,
-  Eigen::MatrixXd& C,
-  Eigen::MatrixXi& IA,
-  Eigen::MatrixXi& IC
-)
-{
-  return igl::unique_rows(A,C,IA,IC);
-}, __doc_igl_unique,
-py::arg("A"), py::arg("C"), py::arg("IA"), py::arg("IC"));
-
 //m.def("unique", []
 //(
 //  const std::vector<double> & A,
@@ -79,18 +67,6 @@ m.def("unique", []
   return igl::unique(A,C);
 }, __doc_igl_unique,
 py::arg("A"), py::arg("C"));
-
-m.def("unique_rows", []
-(
-  const Eigen::MatrixXi& A,
-  Eigen::MatrixXi& C,
-  Eigen::MatrixXi& IA,
-  Eigen::MatrixXi& IC
-)
-{
-  return igl::unique_rows(A,C,IA,IC);
-}, __doc_igl_unique,
-py::arg("A"), py::arg("C"), py::arg("IA"), py::arg("IC"));
 
 //m.def("unique", []
 //(
