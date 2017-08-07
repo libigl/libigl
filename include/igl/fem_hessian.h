51053408d2518f63b723a9893f06c359ab768c28
Oded Stein <oded.stein@columbia.edu>
2017-08-06 23:41:19 -0700

    split Hessians into two files
    Former-commit-id: b40d1f0e9f30829382c737ee5dd166bac072c1a4

--- include/igl/fem_hessian.h
+++ include/igl/fem_hessian.h
@@ -5,8 +5,9 @@
 // This Source Code Form is subject to the terms of the Mozilla Public License
 // v. 2.0. If a copy of the MPL was not distributed with this file, You can
 // obtain one at http://mozilla.org/MPL/2.0/.
-#ifndef IGL_HESSIAN_ENERGY_H
-#define IGL_HESSIAN_ENERGY_H
+
+#ifndef IGL_FEM_HESSIAN_H
+#define IGL_FEM_HESSIAN_H
 #include "igl_inline.h"
 
 #include <Eigen/Dense>
@@ -15,27 +16,28 @@
 
 namespace igl
 {
-    // Constructs the Hessian energy matrix using mixed FEM
+    // Constructs the finite element Hessian matrix
     // as described in https://arxiv.org/abs/1707.04348
+    // The interior vertices are NOT set to zero yet.
     //
     // Inputs:
     //   V  #V by dim list of mesh vertex positions
     //   F  #F by 3 list of mesh faces (must be triangles)
     // Outputs:
-    //   Q  #V by #V Hessian energy matrix, each row/column i corresponding to V(i,:)
+    //   H  #V by #V Hessian energy matrix, each column i corresponding to V(i,:)
     //
     //
     //
     template <typename DerivedV, typename DerivedF, typename Scalar>
-    IGL_INLINE void hessian_energy(
-                                   const Eigen::PlainObjectBase<DerivedV> & V,
-                                   const Eigen::PlainObjectBase<DerivedF> & F,
-                                   Eigen::SparseMatrix<Scalar>& Q);
+    IGL_INLINE void fem_hessian(
+                                const Eigen::PlainObjectBase<DerivedV> & V,
+                                const Eigen::PlainObjectBase<DerivedF> & F,
+                                Eigen::SparseMatrix<Scalar>& H);
     
 }
 
 #ifndef IGL_STATIC_LIBRARY
-#  include "hessian_energy.cpp"
+#  include "fem_hessian.cpp"
 #endif
 
 #endif
