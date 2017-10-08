58a2146c68eaf2cf2561eb90ef2ad4c341c4f794
Daniele Panozzo <daniele.panozzo@gmail.com>
2017-10-08 16:02:47 -0400

    added comments to python files 1/2
    Former-commit-id: 52d0f356891d1c21ed1cc6d287a4729f19038b61

--- python/py_igl/py_per_edge_normals.cpp
+++ python/py_igl/py_per_edge_normals.cpp
@@ -1,3 +1,10 @@
+// This file is part of libigl, a simple c++ geometry processing library.
+//
+// Copyright (C) 2017 Sebastian Koch <s.koch@tu-berlin.de> and Daniele Panozzo <daniele.panozzo@gmail.com>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License
+// v. 2.0. If a copy of the MPL was not distributed with this file, You can
+// obtain one at http://mozilla.org/MPL/2.0/.
 py::enum_<igl::PerEdgeNormalsWeightingType>(m, "PerEdgeNormalsWeightingType")
     .value("PER_EDGE_NORMALS_WEIGHTING_TYPE_UNIFORM", igl::PER_EDGE_NORMALS_WEIGHTING_TYPE_UNIFORM)
     .value("PER_EDGE_NORMALS_WEIGHTING_TYPE_AREA", igl::PER_EDGE_NORMALS_WEIGHTING_TYPE_AREA)
