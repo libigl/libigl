d28530e2e0068b94ad5c9c7608b7707da98e422e
Alec Jacobson <alecjacobson@gmail.com>
2017-10-13 12:34:21 -0400

    erase_mesh in tutorial
    Former-commit-id: 9067548cb47dfe3dc5fdfa6e63a36f45828e6803

--- tutorial/107_MultipleMeshes/main.cpp
+++ tutorial/107_MultipleMeshes/main.cpp
@@ -1,23 +1,40 @@
 #include "tutorial_shared_path.h"
 #include <igl/opengl/glfw/Viewer.h>
 #include <string>
+#include <iostream>
 
 int main(int argc, char * argv[])
 {
-  igl::opengl::glfw::Viewer v;
-  for(const auto & name : 
-    {"cube.obj","sphere.obj","xcylinder.obj","ycylinder.obj","zcylinder.obj"})
+  igl::opengl::glfw::Viewer viewer;
+  const auto names = 
+    {"cube.obj","sphere.obj","xcylinder.obj","ycylinder.obj","zcylinder.obj"};
+  for(const auto & name : names)
   {
-    v.load_mesh_from_file(std::string(TUTORIAL_SHARED_PATH) + "/" + name);
+    viewer.load_mesh_from_file(std::string(TUTORIAL_SHARED_PATH) + "/" + name);
   }
-  v.selected_data_index = 0;
-  v.selected_data().set_colors(Eigen::RowVector3d(0.8,0.47,0.22));
-  v.selected_data_index = 1;
-  v.selected_data().set_colors(Eigen::RowVector3d(0.6,0.01,0.11));
-  v.selected_data_index = 2;
-  v.selected_data().set_colors(Eigen::RowVector3d(0.37,0.06,0.25));
-  v.selected_data_index = 3;
-  v.selected_data().set_colors(Eigen::RowVector3d(1,1,1));
-  v.launch();
+
+  // Set colors of each mesh by selecting its index first
+  viewer.selected_data_index = 0;
+  viewer.selected_data().set_colors(Eigen::RowVector3d(0.8,0.47,0.22));
+  viewer.selected_data_index = 1;
+  viewer.selected_data().set_colors(Eigen::RowVector3d(0.6,0.01,0.11));
+  viewer.selected_data_index = 2;
+  viewer.selected_data().set_colors(Eigen::RowVector3d(0.37,0.06,0.25));
+  viewer.selected_data_index = 3;
+  viewer.selected_data().set_colors(Eigen::RowVector3d(1,1,1));
+
+  viewer.callback_key_down = 
+    [&](igl::opengl::glfw::Viewer &, unsigned int key, int mod)
+  {
+    // Delete
+    if(key == 3)
+    {
+      viewer.erase_mesh(viewer.selected_data_index);
+      return true;
+    }
+    return false;
+  };
+
+  viewer.launch();
   return EXIT_SUCCESS;
 }
