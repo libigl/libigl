0bef47c705886b1508cc7f3d63036f11f086a150
Daniele Panozzo <daniele.panozzo@gmail.com>
2018-02-14 16:27:58 -0400

    finished pass over all tutorials

--- python/tutorial/604_Triangle.py
+++ python/tutorial/604_Triangle.py
@@ -13,13 +13,13 @@ import pyigl as igl
 
 from shared import check_dependencies
 
-dependencies = ["triangle", "viewer"]
+dependencies = ["triangle", "glfw"]
 check_dependencies(dependencies)
 
 
 # Input polygon
 V = igl.eigen.MatrixXd([[-1, -1], [1, -1], [1, 1], [-1, 1], [-2, -2], [2, -2], [2, 2], [-2, 2]])
-E = igl.eigen.MatrixXi([[0, 1], [1, 2], [2, 3], [3, 0], [4, 5], [5, 6], [6,7], [7,4]])
+E = igl.eigen.MatrixXd([[0, 1], [1, 2], [2, 3], [3, 0], [4, 5], [5, 6], [6,7], [7,4]]).castint()
 H = igl.eigen.MatrixXd([[0, 0]])
 
 # Triangulated Interior
@@ -29,6 +29,6 @@ F2 = igl.eigen.MatrixXi()
 igl.triangle.triangulate(V, E, H, "a0.005q", V2, F2)
 
 # Plot the mesh
-viewer = igl.viewer.Viewer()
-viewer.data.set_mesh(V2, F2)
+viewer = igl.glfw.Viewer()
+viewer.data().set_mesh(V2, F2)
 viewer.launch()
