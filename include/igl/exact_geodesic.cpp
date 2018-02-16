5215a21524285575064eb89355ce59e15ea6769d
Daniele Panozzo <daniele.panozzo@gmail.com>
2018-02-16 13:52:17 -0400

    fixed compilation problems added tutorial removed debug prints

--- include/igl/exact_geodesic.cpp
+++ include/igl/exact_geodesic.cpp
@@ -1069,10 +1069,10 @@ inline bool Mesh::verify()		//verifies connectivity of the mesh and prints some 
 	assert(std::find(map.begin(), map.end(), false) == map.end());
 
 	//print some mesh statistics that can be useful in debugging
-	std::cout << "mesh has "	<< m_vertices.size() 
-			  << " vertices, "	<< m_faces.size() 
-			  << " faces, "		<< m_edges.size() 
-			  << " edges\n";
+	// std::cout << "mesh has "	<< m_vertices.size() 
+	// 		  << " vertices, "	<< m_faces.size() 
+	// 		  << " faces, "		<< m_edges.size() 
+	// 		  << " edges\n";
 	
 	unsigned total_boundary_edges = 0;
 	double longest_edge = 0;
@@ -1084,12 +1084,12 @@ inline bool Mesh::verify()		//verifies connectivity of the mesh and prints some 
 		longest_edge = std::max(longest_edge, e.length());
 		shortest_edge = std::min(shortest_edge, e.length());
 	}
-	std::cout << total_boundary_edges << " edges are boundary edges\n";
-	std::cout << "shortest/longest edges are " 
-			  << shortest_edge << "/"
-			  << longest_edge << " = "
-			  << shortest_edge/longest_edge
-			  << std::endl;
+	// std::cout << total_boundary_edges << " edges are boundary edges\n";
+	// std::cout << "shortest/longest edges are " 
+	// 		  << shortest_edge << "/"
+	// 		  << longest_edge << " = "
+	// 		  << shortest_edge/longest_edge
+	// 		  << std::endl;
 
 	double minx = 1e100;
 	double maxx = -1e100;
@@ -1107,18 +1107,18 @@ inline bool Mesh::verify()		//verifies connectivity of the mesh and prints some 
 		minz = std::min(minz, v.z());
 		maxz = std::max(maxz, v.z());
 	}
-	std::cout << "enclosing XYZ box:"
-			  <<" X[" << minx << "," << maxx << "]"
-			  <<" Y[" << miny << "," << maxy << "]"
-			  <<" Z[" << minz << "," << maxz << "]"
-			  << std::endl;
+	// std::cout << "enclosing XYZ box:"
+	// 		  <<" X[" << minx << "," << maxx << "]"
+	// 		  <<" Y[" << miny << "," << maxy << "]"
+	// 		  <<" Z[" << minz << "," << maxz << "]"
+	// 		  << std::endl;
 
 	double dx = maxx - minx;
 	double dy = maxy - miny;
 	double dz = maxz - minz;
-	std::cout << "approximate diameter of the mesh is "
-			  << sqrt(dx*dx + dy*dy + dz*dz)
-			  << std::endl;
+	// std::cout << "approximate diameter of the mesh is "
+	// 		  << sqrt(dx*dx + dy*dy + dz*dz)
+	// 		  << std::endl;
 
 	double min_angle = 1e100;
 	double max_angle = -1e100;
@@ -1132,12 +1132,12 @@ inline bool Mesh::verify()		//verifies connectivity of the mesh and prints some 
 			max_angle = std::max(max_angle, angle);
 		}
 	}
-	std::cout << "min/max face angles are "
-			  << min_angle/M_PI*180.0 << "/"
-			  << max_angle/M_PI*180.0
-			  << " degrees\n";
+	// std::cout << "min/max face angles are "
+	// 		  << min_angle/M_PI*180.0 << "/"
+	// 		  << max_angle/M_PI*180.0
+	// 		  << " degrees\n";
 
-	std::cout << std::endl;
+	// std::cout << std::endl;
 	return true;
 }
 
@@ -3223,13 +3223,4 @@ IGL_INLINE void igl::exact_geodesic(
 
 #ifdef IGL_STATIC_LIBRARY
 template void igl::exact_geodesic<Eigen::Matrix<double, -1, -1, 0, -1, -1>, Eigen::Matrix<int, -1, -1, 0, -1, -1>, Eigen::Matrix<int, -1, 1, 0, -1, 1>, Eigen::Matrix<int, -1, 1, 0, -1, 1>, Eigen::Matrix<int, -1, 1, 0, -1, 1>, Eigen::Matrix<int, -1, 1, 0, -1, 1>, Eigen::Matrix<double, -1, 1, 0, -1, 1>>(Eigen::MatrixBase<Eigen::Matrix<double, -1, -1, 0, -1, -1>> const &, Eigen::MatrixBase<Eigen::Matrix<int, -1, -1, 0, -1, -1>> const &, Eigen::MatrixBase<Eigen::Matrix<int, -1, 1, 0, -1, 1>> const &, Eigen::MatrixBase<Eigen::Matrix<int, -1, 1, 0, -1, 1>> const &, Eigen::MatrixBase<Eigen::Matrix<int, -1, 1, 0, -1, 1>> const &, Eigen::MatrixBase<Eigen::Matrix<int, -1, 1, 0, -1, 1>> const &, Eigen::PlainObjectBase<Eigen::Matrix<double, -1, 1, 0, -1, 1>> &);
-template void igl::exact_geodesic< Eigen::Matrix<double, -1, -1, 0, -1, -1>, Eigen::Matrix<int, -1, -1, 0, -1, -1>, Eigen::Matrix<int, -1, -1, 0, -1, -1>, Eigen::Matrix<int, -1, -1, 0, -1, -1>, Eigen::Matrix<int, -1, -1, 0, -1, -1>, Eigen::Matrix<int, -1, -1, 0, -1, -1>, Eigen::Matrix<double, -1, -1, 0, -1, -1> >(
-       Eigen::MatrixBase<Eigen::Matrix<double,-1,-1,0,-1,-1>const &, 
-       Eigen::MatrixBase<Eigen::Matrix<int,-1,-1,0,-1,-1> const &, 
-       Eigen::MatrixBase<Eigen::Matrix<int,-1,-1,0,-1,-1> const &, 
-       Eigen::MatrixBase<Eigen::Matrix<int,-1,-1,0,-1,-1> const &, 
-       Eigen::MatrixBase<Eigen::Matrix<int,-1,-1,0,-1,-1> const &, 
-       Eigen::MatrixBase<Eigen::Matrix<int,-1,-1,0,-1,-1> const &, 
-       Eigen::PlainObjectBase<Eigen::Matrix<double,-1,-1,0,-1,-1>&);
-
 #endif
