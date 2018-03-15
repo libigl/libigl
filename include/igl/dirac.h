afe0be9b7d8a1ea413bfe9c90766a46cce3411ad
Zhongshi Jiang <jiangzs@nyu.edu>
2018-03-15 17:31:28 -0400

    igl_dirac_h

--- include/igl/dirac.h
+++ include/igl/dirac.h
@@ -5,8 +5,8 @@
 // This Source Code Form is subject to the terms of the Mozilla Public License 
 // v. 2.0. If a copy of the MPL was not distributed with this file, You can 
 // obtain one at http://mozilla.org/MPL/2.0/.
-#ifndef IGL_DIRAC
-#define IGL_DIRAC
+#ifndef IGL_DIRAC_H
+#define IGL_DIRAC_H
 #include <igl/igl_inline.h>
 
 #include <Eigen/Dense>
