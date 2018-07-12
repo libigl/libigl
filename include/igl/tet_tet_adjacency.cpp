8984bc69526b9e620cf63bfade8d902affb61dad
Oded Stein <oded.stein@columbia.edu>
2018-07-12 16:01:51 -0400

    Add tet_tet_adjacency

--- include/igl/tet_tet_adjacency.cpp
+++ include/igl/tet_tet_adjacency.cpp
@@ -8,6 +8,7 @@
 
 
 #include "tet_tet_adjacency.h"
+
 #include "parallel_for.h"
 
 #include <array>
@@ -20,39 +21,39 @@ igl::tet_tet_adjacency(
                        Eigen::PlainObjectBase<DerivedTT>& TT,
                        Eigen::PlainObjectBase<DerivedTTi>& TTi)
 {
-    assert(T.cols()==4 && "Tets have four vertices.");
-    
-    //Preprocess
-    using Array = std::array<int, 5>;
-    std::vector<Array> TTT(4*T.rows());
-    const auto loop_f = [&](const int t) {
-        TTT[4*t] = {T(t,0),T(t,1),T(t,2),t,0};
-        TTT[4*t+1] = {T(t,0),T(t,1),T(t,3),t,1};
-        TTT[4*t+2] = {T(t,1),T(t,2),T(t,3),t,2};
-        TTT[4*t+3] = {T(t,2),T(t,0),T(t,3),t,3};
-        for(int i=0; i<4; ++i)
-            std::sort(TTT[4*t+i].begin(), TTT[4*t+i].begin()+3);
-    };
-    
-    //for(int t=0; t<T.rows(); ++t)
-    //    loop_f(t);
-    igl::parallel_for(T.rows(), loop_f);
-    
-    std::sort(TTT.begin(),TTT.end());
-    
-    //Compute TT and TTi
-    TT.setConstant(T.rows(), T.cols(), -1);
-    TTi.setConstant(T.rows(), T.cols(), -1);
-    for(int i=1; i<TTT.size(); ++i) {
-        const Array& r1 = TTT[i-1];
-        const Array& r2 = TTT[i];
-        if((r1[0]==r2[0]) && (r1[1]==r2[1]) && (r1[2]==r2[2])) {
-            TT(r1[3],r1[4]) = r2[3];
-            TT(r2[3],r2[4]) = r1[3];
-            TTi(r1[3],r1[4]) = r2[4];
-            TTi(r2[3],r2[4]) = r1[4];
-        }
+  assert(T.cols()==4 && "Tets have four vertices.");
+  
+  //Preprocess
+  using Array = std::array<int, 5>;
+  std::vector<Array> TTT(4*T.rows());
+  const auto loop_f = [&](const int t) {
+    TTT[4*t] = {T(t,0),T(t,1),T(t,2),t,0};
+    TTT[4*t+1] = {T(t,0),T(t,1),T(t,3),t,1};
+    TTT[4*t+2] = {T(t,1),T(t,2),T(t,3),t,2};
+    TTT[4*t+3] = {T(t,2),T(t,0),T(t,3),t,3};
+    for(int i=0; i<4; ++i)
+      std::sort(TTT[4*t+i].begin(), TTT[4*t+i].begin()+3);
+  };
+  
+  //for(int t=0; t<T.rows(); ++t)
+  //    loop_f(t);
+  igl::parallel_for(T.rows(), loop_f);
+  
+  std::sort(TTT.begin(),TTT.end());
+  
+  //Compute TT and TTi
+  TT.setConstant(T.rows(), T.cols(), -1);
+  TTi.setConstant(T.rows(), T.cols(), -1);
+  for(int i=1; i<TTT.size(); ++i) {
+    const Array& r1 = TTT[i-1];
+    const Array& r2 = TTT[i];
+    if((r1[0]==r2[0]) && (r1[1]==r2[1]) && (r1[2]==r2[2])) {
+      TT(r1[3],r1[4]) = r2[3];
+      TT(r2[3],r2[4]) = r1[3];
+      TTi(r1[3],r1[4]) = r2[4];
+      TTi(r2[3],r2[4]) = r1[4];
     }
+  }
 }
 
 
@@ -62,6 +63,6 @@ igl::tet_tet_adjacency(
                        const Eigen::MatrixBase<DerivedT>& T,
                        Eigen::PlainObjectBase<DerivedTT>& TT)
 {
-    DerivedTT TTi;
-    tet_tet_adjacency(T, TT, TTi);
+  DerivedTT TTi;
+  tet_tet_adjacency(T, TT, TTi);
 }
