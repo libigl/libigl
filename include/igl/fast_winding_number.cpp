dd9d853fadb5de1eb05902b06e613287bab84ea3
GavinBarill <gavinpcb@gmail.com>
2018-07-16 14:39:38 -0400

    Made the changes Alec asked for
    Former-commit-id: 2bf99911002a6b3fbb6c75027b1864317b7f161d

--- include/igl/fast_winding_number.cpp
+++ include/igl/fast_winding_number.cpp
@@ -1,133 +1,131 @@
 #include "fast_winding_number.h"
-#include "build_octree.h"
-#include "knn_octree.h"
+#include "octree.h"
+#include "knn.h"
 #include "parallel_for.h"
-#include "copyleft/cgal/point_areas_and_normals.h"
 #include <vector>
 
 namespace igl {
-template <typename DerivedP, typename DerivedA, typename DerivedN,
-    typename Index, typename DerivedCM, typename DerivedR, typename DerivedEC>
+  template <typename DerivedP, typename DerivedA, typename DerivedN,
+  typename Index, typename DerivedCH, typename DerivedCM, typename DerivedR,
+  typename DerivedEC>
   IGL_INLINE void fast_winding_number(const Eigen::MatrixBase<DerivedP>& P,
-            const Eigen::MatrixBase<DerivedN>& N,
-            const Eigen::MatrixBase<DerivedA>& A,
-            const std::vector<std::vector<Index> > & point_indices,
-            const std::vector<Eigen::Matrix<Index,8,1>,
-              Eigen::aligned_allocator<Eigen::Matrix<Index,8,1> > > & children,
-            const int expansion_order,
-            Eigen::PlainObjectBase<DerivedCM>& CM,
-            Eigen::PlainObjectBase<DerivedR>& R,
-            Eigen::PlainObjectBase<DerivedEC>& EC
-            ){
-      typedef typename DerivedP::Scalar real_p;
-      typedef typename DerivedN::Scalar real_n;
-      typedef typename DerivedA::Scalar real_a;
-      typedef typename DerivedCM::Scalar real_cm;
-      typedef typename DerivedR::Scalar real_r;
-      typedef typename DerivedEC::Scalar real_ec;
-    
-      typedef Eigen::Matrix<real_p,1,3> RowVec3p;
-    
-      int m = children.size();
-      int num_terms;
-    
-      assert(expansion_order < 3 && expansion_order >= 0 && "m must be less than n");
-      if(expansion_order == 0){
-          num_terms = 3;
-      } else if(expansion_order ==1){
-          num_terms = 3 + 9;
-      } else if(expansion_order == 2){
-          num_terms = 3 + 9 + 27;
-      }
-    
-      R.resize(m);
-      CM.resize(m,3);
-      EC.resize(m,num_terms);
-      EC.setZero(m,num_terms);
-      std::function< void(const int) > helper;
-      helper = [&helper,
-                &P,&N,&A,&expansion_order,&point_indices,&children,&EC,&R,&CM]
-      (const int index)-> void
-      {
-          Eigen::Matrix<real_cm,1,3> masscenter;
-          masscenter << 0,0,0;
-          Eigen::Matrix<real_ec,1,3> zeroth_expansion;
-          zeroth_expansion << 0,0,0;
-          real_p areatotal = 0.0;
-          for(int j = 0; j < point_indices.at(index).size(); j++){
-              int curr_point_index = point_indices.at(index).at(j);
-            
-              areatotal += A(curr_point_index);
-              masscenter += A(curr_point_index)*P.row(curr_point_index);
-              zeroth_expansion += A(curr_point_index)*N.row(curr_point_index);
-          }
-        
-          masscenter = masscenter/areatotal;
-          CM.row(index) = masscenter;
-          EC.block(index,0,1,3) = zeroth_expansion;
-        
-          real_r max_norm = 0;
-          real_r curr_norm;
-        
-          for(int i = 0; i < point_indices.at(index).size(); i++){
-              //Get max distance from center of mass:
-              int curr_point_index = point_indices.at(index).at(i);
-              Eigen::Matrix<real_r,1,3> point =
-                  P.row(curr_point_index)-masscenter;
-              curr_norm = point.norm();
-              if(curr_norm > max_norm){
-                  max_norm = curr_norm;
-              }
-            
-              //Calculate higher order terms if necessary
-              Eigen::Matrix<real_ec,3,3> TempCoeffs;
-              if(EC.cols() >= (3+9)){
-                  TempCoeffs = A(curr_point_index)*point.transpose()*
-                                  N.row(curr_point_index);
-                  EC.block(index,3,1,9) +=
-                  Eigen::Map<Eigen::Matrix<real_ec,1,9> >(TempCoeffs.data(),
-                                                          TempCoeffs.size());
-              }
-            
-              if(EC.cols() == (3+9+27)){
-                  for(int k = 0; k < 3; k++){
-                      TempCoeffs = 0.5 * point(k) * (A(curr_point_index)*
-                                    point.transpose()*N.row(curr_point_index));
-                      EC.block(index,12+9*k,1,9) += Eigen::Map<
-                        Eigen::Matrix<real_ec,1,9> >(TempCoeffs.data(),
-                                                     TempCoeffs.size());
-                  }
-              }
-          }
-        
-          R(index) = max_norm;
-          if(children.at(index)(0) != -1)
-          {
-              for(int i = 0; i < 8; i++){
-                  int child = children.at(index)(i);
-                  helper(child);
-              }
-          }
-      };
-      helper(0);
+                                      const Eigen::MatrixBase<DerivedN>& N,
+                                      const Eigen::MatrixBase<DerivedA>& A,
+                                      const std::vector<std::vector<Index> > & point_indices,
+                                      const Eigen::MatrixBase<DerivedCH>& CH,
+                                      const int expansion_order,
+                                      Eigen::PlainObjectBase<DerivedCM>& CM,
+                                      Eigen::PlainObjectBase<DerivedR>& R,
+                                      Eigen::PlainObjectBase<DerivedEC>& EC)
+  {
+    typedef typename DerivedP::Scalar real_p;
+    typedef typename DerivedN::Scalar real_n;
+    typedef typename DerivedA::Scalar real_a;
+    typedef typename DerivedCM::Scalar real_cm;
+    typedef typename DerivedR::Scalar real_r;
+    typedef typename DerivedEC::Scalar real_ec;
+  
+    typedef Eigen::Matrix<real_p,1,3> RowVec3p;
+  
+    int m = CH.size();
+    int num_terms;
+  
+    assert(expansion_order < 3 && expansion_order >= 0 && "m must be less than n");
+    if(expansion_order == 0){
+        num_terms = 3;
+    } else if(expansion_order ==1){
+        num_terms = 3 + 9;
+    } else if(expansion_order == 2){
+        num_terms = 3 + 9 + 27;
+    }
+  
+    R.resize(m);
+    CM.resize(m,3);
+    EC.resize(m,num_terms);
+    EC.setZero(m,num_terms);
+    std::function< void(const int) > helper;
+    helper = [&helper,
+              &P,&N,&A,&expansion_order,&point_indices,&CH,&EC,&R,&CM]
+    (const int index)-> void
+    {
+        Eigen::Matrix<real_cm,1,3> masscenter;
+        masscenter << 0,0,0;
+        Eigen::Matrix<real_ec,1,3> zeroth_expansion;
+        zeroth_expansion << 0,0,0;
+        real_p areatotal = 0.0;
+        for(int j = 0; j < point_indices.at(index).size(); j++){
+            int curr_point_index = point_indices.at(index).at(j);
+          
+            areatotal += A(curr_point_index);
+            masscenter += A(curr_point_index)*P.row(curr_point_index);
+            zeroth_expansion += A(curr_point_index)*N.row(curr_point_index);
+        }
+      
+        masscenter = masscenter/areatotal;
+        CM.row(index) = masscenter;
+        EC.block(index,0,1,3) = zeroth_expansion;
+      
+        real_r max_norm = 0;
+        real_r curr_norm;
+      
+        for(int i = 0; i < point_indices.at(index).size(); i++){
+            //Get max distance from center of mass:
+            int curr_point_index = point_indices.at(index).at(i);
+            Eigen::Matrix<real_r,1,3> point =
+                P.row(curr_point_index)-masscenter;
+            curr_norm = point.norm();
+            if(curr_norm > max_norm){
+                max_norm = curr_norm;
+            }
+          
+            //Calculate higher order terms if necessary
+            Eigen::Matrix<real_ec,3,3> TempCoeffs;
+            if(EC.cols() >= (3+9)){
+                TempCoeffs = A(curr_point_index)*point.transpose()*
+                                N.row(curr_point_index);
+                EC.block(index,3,1,9) +=
+                Eigen::Map<Eigen::Matrix<real_ec,1,9> >(TempCoeffs.data(),
+                                                        TempCoeffs.size());
+            }
+          
+            if(EC.cols() == (3+9+27)){
+                for(int k = 0; k < 3; k++){
+                    TempCoeffs = 0.5 * point(k) * (A(curr_point_index)*
+                                  point.transpose()*N.row(curr_point_index));
+                    EC.block(index,12+9*k,1,9) += Eigen::Map<
+                      Eigen::Matrix<real_ec,1,9> >(TempCoeffs.data(),
+                                                   TempCoeffs.size());
+                }
+            }
+        }
+      
+        R(index) = max_norm;
+        if(CH(index,0) != -1)
+        {
+            for(int i = 0; i < 8; i++){
+                int child = CH(index,i);
+                helper(child);
+            }
+        }
+    };
+    helper(0);
   }
   
   template <typename DerivedP, typename DerivedA, typename DerivedN,
-    typename Index, typename DerivedCM, typename DerivedR, typename DerivedEC,
-    typename DerivedQ, typename BetaType, typename DerivedWN>
+  typename Index, typename DerivedCH, typename DerivedCM, typename DerivedR,
+  typename DerivedEC, typename DerivedQ, typename BetaType,
+  typename DerivedWN>
   IGL_INLINE void fast_winding_number(const Eigen::MatrixBase<DerivedP>& P,
-            const Eigen::MatrixBase<DerivedN>& N,
-            const Eigen::MatrixBase<DerivedA>& A,
-            const std::vector<std::vector<Index> > & point_indices,
-            const std::vector<Eigen::Matrix<Index,8,1>,
-              Eigen::aligned_allocator<Eigen::Matrix<Index,8,1> > > & children,
-            const Eigen::MatrixBase<DerivedCM>& CM,
-            const Eigen::MatrixBase<DerivedR>& R,
-            const Eigen::MatrixBase<DerivedEC>& EC,
-            const Eigen::MatrixBase<DerivedQ>& Q,
-            const BetaType beta,
-            Eigen::PlainObjectBase<DerivedWN>& WN
-            ){
+                        const Eigen::MatrixBase<DerivedN>& N,
+                        const Eigen::MatrixBase<DerivedA>& A,
+                        const std::vector<std::vector<Index> > & point_indices,
+                        const Eigen::MatrixBase<DerivedCH>& CH,
+                        const Eigen::MatrixBase<DerivedCM>& CM,
+                        const Eigen::MatrixBase<DerivedR>& R,
+                        const Eigen::MatrixBase<DerivedEC>& EC,
+                        const Eigen::MatrixBase<DerivedQ>& Q,
+                        const BetaType beta,
+                        Eigen::PlainObjectBase<DerivedWN>& WN){
   
     typedef typename DerivedP::Scalar real_p;
     typedef typename DerivedN::Scalar real_n;
@@ -177,7 +175,8 @@ template <typename DerivedP, typename DerivedA, typename DerivedN,
               Eigen::Matrix<real_ec,3,3> RowCol;
               RowCol.setZero(3,3);
               RowCol.row(i) = loc;
-              RowCol = RowCol + RowCol.transpose();
+              Eigen::Matrix<real_ec,3,3> RowColT = RowCol.transpose();
+              RowCol = RowCol + RowColT;
               ThirdDerivative +=
                   -3.0/(4.0*M_PI*std::pow(r,5))*(RowCol+Diagonal);
               Eigen::Matrix<real_ec,1,9> derivative_vector =
@@ -196,7 +195,7 @@ template <typename DerivedP, typename DerivedA, typename DerivedN,
     std::function< real_wn(const RowVec, const std::vector<int>) > helper;
     helper = [&helper,
               &P,&N,&A,
-              &point_indices,&children,
+              &point_indices,&CH,
               &CM,&R,&EC,&beta,
               &direct_eval,&expansion_eval]
     (const RowVec query, const std::vector<int> near_indices)-> real_wn
@@ -206,7 +205,7 @@ template <typename DerivedP, typename DerivedA, typename DerivedN,
       for(int i = 0; i < near_indices.size(); i++){
         int index = near_indices.at(i);
         //Leaf Case, Brute force
-        if(children.at(index)(0) == -1){
+        if(CH(index,0) == -1){
           for(int j = 0; j < point_indices.at(index).size(); j++){
             int curr_row = point_indices.at(index).at(j);
             wn += direct_eval(P.row(curr_row)-query,
@@ -216,10 +215,10 @@ template <typename DerivedP, typename DerivedA, typename DerivedN,
         //Non-Leaf Case
         else {
           for(int child = 0; child < 8; child++){
-              int child_index = children.at(index)(child);
+              int child_index = CH(index,child);
               if(point_indices.at(child_index).size() > 0){
                 if((CM.row(child_index)-query).norm() > beta*R(child_index)){
-                  if(children.at(child_index)(0) == -1){
+                  if(CH(child_index,0) == -1){
                     for(int j=0;j<point_indices.at(child_index).size();j++){
                       int curr_row = point_indices.at(child_index).at(j);
                       wn += direct_eval(P.row(curr_row)-query,
@@ -270,24 +269,20 @@ template <typename DerivedP, typename DerivedA, typename DerivedN,
                                       Eigen::PlainObjectBase<DerivedWN>& WN
                                       ){
     typedef typename DerivedWN::Scalar real;
-    typedef typename Eigen::Matrix<real,1,3> RowVec;
-  
+    
     std::vector<std::vector<int> > point_indices;
-    std::vector<Eigen::Matrix<int,8,1>,
-      Eigen::aligned_allocator<Eigen::Matrix<int,8,1> > > children;
-    std::vector<RowVec, Eigen::aligned_allocator<RowVec> > centers;
-    std::vector<real> widths;
-    Eigen::MatrixXi I;
-    Eigen::MatrixXd NotUsed;
+    Eigen::Matrix<int,Eigen::Dynamic,8> CH;
+    Eigen::Matrix<real,Eigen::Dynamic,3> CN;
+    Eigen::Matrix<real,Eigen::Dynamic,1> W;
   
-    build_octree(P,point_indices,children,centers,widths);
+    octree(P,point_indices,CH,CN,W);
   
-    Eigen::MatrixXd EC, CM;
-    Eigen::VectorXd R;
+    Eigen::Matrix<real,Eigen::Dynamic,Eigen::Dynamic> EC;
+    Eigen::Matrix<real,Eigen::Dynamic,3> CM;
+    Eigen::Matrix<real,Eigen::Dynamic,1> R;
   
-    fast_winding_number(P,N,A,point_indices,children,
-                          expansion_order,CM,R,EC);
-    fast_winding_number(P,N,A,point_indices,children,CM,R,EC,Q,beta,WN);
+    fast_winding_number(P,N,A,point_indices,CH,expansion_order,CM,R,EC);
+    fast_winding_number(P,N,A,point_indices,CH,CM,R,EC,Q,beta,WN);
   }
   
   template <typename DerivedP, typename DerivedA, typename DerivedN,
@@ -299,49 +294,6 @@ template <typename DerivedP, typename DerivedA, typename DerivedN,
                                       Eigen::PlainObjectBase<DerivedWN>& WN
                                       ){
     fast_winding_number(P,N,A,Q,2,2.0,WN);
-  }
-  
-  template <typename DerivedP, typename DerivedN, typename DerivedQ,
-    typename BetaType, typename DerivedWN>
-  IGL_INLINE void fast_winding_number(const Eigen::MatrixBase<DerivedP>& P,
-                                      const Eigen::MatrixBase<DerivedN>& N,
-                                      const Eigen::MatrixBase<DerivedQ>& Q,
-                                      const int expansion_order,
-                                      const BetaType beta,
-                                      Eigen::PlainObjectBase<DerivedWN>& WN
-                                      ){
-    typedef typename DerivedWN::Scalar real;
-    typedef typename Eigen::Matrix<real,1,3> RowVec;
-  
-    std::vector<std::vector<int> > point_indices;
-    std::vector<Eigen::Matrix<int,8,1>,
-      Eigen::aligned_allocator<Eigen::Matrix<int,8,1> > > children;
-    std::vector<RowVec, Eigen::aligned_allocator<RowVec> > centers;
-    std::vector<real> widths;
-    Eigen::MatrixXi I;
-    Eigen::Matrix<real,Eigen::Dynamic,Eigen::Dynamic> NotUsed;
-    Eigen::Matrix<real,Eigen::Dynamic,1> A;
-  
-    build_octree(P,point_indices,children,centers,widths);
-    knn_octree(P,21,point_indices,children,centers,widths,I);
-    copyleft::cgal::point_areas_and_normals(P,I,N,A,NotUsed);
-  
-    Eigen::MatrixXd EC, CM;
-    Eigen::VectorXd R;
-
-    fast_winding_number(P,N,A,point_indices,children,expansion_order,CM,R,EC);
-    fast_winding_number(P,N,A,point_indices,children,CM,R,EC,Q,beta,WN);
-  }
-  
-  template <typename DerivedP, typename DerivedN,
-    typename DerivedQ, typename DerivedWN>
-  IGL_INLINE void fast_winding_number(
-                            const Eigen::MatrixBase<DerivedP>& P,
-                            const Eigen::MatrixBase<DerivedN>& N,
-                            const Eigen::MatrixBase<DerivedQ>& Q,
-                            Eigen::PlainObjectBase<DerivedWN>& WN
-                            ){
-    fast_winding_number(P,N,Q,2,2.0,WN);
   }
 }
 
