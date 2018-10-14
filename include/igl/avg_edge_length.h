075e95bfd25e4021954b60e51c916fd7a40b07e1
Alec Jacobson <alecjacobson@gmail.com>
2018-10-14 15:14:38 -0400

    Revert "Revert "Make avg_edge_length() count interior edges only once.""
    Former-commit-id: 584d437b9ae4f809dd0832ac11b3ba5d03a31b44

--- include/igl/avg_edge_length.h
+++ include/igl/avg_edge_length.h
@@ -1,9 +1,9 @@
 // This file is part of libigl, a simple c++ geometry processing library.
-// 
+//
 // Copyright (C) 2013 Alec Jacobson <alecjacobson@gmail.com>
-// 
-// This Source Code Form is subject to the terms of the Mozilla Public License 
-// v. 2.0. If a copy of the MPL was not distributed with this file, You can 
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License
+// v. 2.0. If a copy of the MPL was not distributed with this file, You can
 // obtain one at http://mozilla.org/MPL/2.0/.
 #ifndef IGL_AVERAGEEDGELENGTH_H
 #define IGL_AVERAGEEDGELENGTH_H
@@ -13,7 +13,7 @@
 #include <string>
 #include <vector>
 
-namespace igl 
+namespace igl
 {
   // Compute the average edge length for the given triangle mesh
   // Templates:
