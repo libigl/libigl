b4eaa0dac7af02d88e358bad4033d1afbb128283
Qingnan Zhou <qnzhou@gmail.com>
2018-10-14 18:58:58 -0400

    Adds interface for the multi-zone mesh generation capabilities of TetGen (#923)
    * Update tetrahedralize.h
    
    * Update tetrahedralize.h
    
    * WIP: adding overload for allowing holes and regions in tetgen mesh
    
    * Update tetrahedralize.h
    
    * Update tetrahedralize.h
    
    * WIP: adding overload for allowing holes and regions in tetgen mesh
    
    * WIP: skeleton code for tetrahedralize supporting holes and regions
    
    * removes comments
    
    * call to overloaded mesh_to_tetgenio()
    
    * overload to allow for holes and regions
    
    * WIP: overload to allow for holes and regions
    
    * fixes typos
    
    * fixed function signature
    
    * adds comments
    
    * adds overload for returning regionID information
    
    * added overload for holes and regions
    
    * added Eigen wrapper
    
    * WIP: add support for neighborlist and point2tet
    
    * WIP: adding point2tet and neighborlist info
    
    * adds support for face to tet list
    
    * WIP: bug fixes, regions, holes
    
    * support for holes, regions, point2tet, face2tet, tet neighbors
    
    * Update tetgen to 1.5.1.

--- include/igl/copyleft/tetgen/tetrahedralize.h
+++ include/igl/copyleft/tetgen/tetrahedralize.h
@@ -123,7 +123,83 @@ namespace igl
         Eigen::PlainObjectBase<DerivedTT>& TT,
         Eigen::PlainObjectBase<DerivedTF>& TF,
         Eigen::PlainObjectBase<DerivedTM>& TM);
-    }
+	
+
+      // Mesh the interior of a surface mesh (V,F) using tetgen
+      //
+      // Inputs:
+      //   V  #V by 3 vertex position list
+      //   F  #F list of polygon face indices into V (0-indexed)
+      //   H  #H by 3 list of seed points inside holes
+      //   R  #R by 5 list of region attributes		
+
+      //   switches  string of tetgen options (See tetgen documentation) e.g.
+      //     "pq1.414a0.01" tries to mesh the interior of a given surface with
+      //       quality and area constraints
+      //     "" will mesh the convex hull constrained to pass through V (ignores F)
+      // Outputs:
+      //   TV  #V by 3 vertex position list
+      //   TT  #T by 4 list of tet face indices
+      //   TF  #F by 3 list of triangle face indices
+      //   TR  #T list of region ID for each tetrahedron	
+      //   TN  #T by 4 list of indices neighbors for each tetrahedron
+      //   PT  #V list of incident tetrahedron for a vertex
+      //   FT  #F by 2 list of tetrahedrons sharing a triface	
+      //   numRegions Number of regions in output mesh
+
+      // Returns status:
+      //   0 success
+      //   1 tetgen threw exception
+      //   2 tetgen did not crash but could not create any tets (probably there are
+      //     holes, duplicate faces etc.)
+      //   -1 other error
+	IGL_INLINE int tetrahedralize(
+	  const std::vector<std::vector<REAL> > &V, 
+	  const std::vector<std::vector<int> >  &F, 
+	  const std::vector<std::vector<REAL> > &H, 
+	  const std::vector<std::vector<REAL> > &R, 
+	  	  
+	  const std::string switches, 
+	  
+	  std::vector<std::vector<REAL > > & TV,
+	  std::vector<std::vector<int > >  & TT,
+	  std::vector<std::vector<int > >  & TF,
+	  std::vector<std::vector<REAL > > &TR,  
+	  std::vector<std::vector<int > > &TN, 
+	  std::vector<std::vector<int > > &PT, 
+	  std::vector<std::vector<int > > &FT, 
+	  size_t & numRegions);	     
+
+
+      // Wrapper with Eigen types
+      // Templates:
+      //   DerivedV  real-value: i.e. from MatrixXd
+      //   DerivedF  integer-value: i.e. from MatrixXi	
+      template <
+        typename DerivedV,
+   	typename DerivedF,
+	typename DerivedH,
+	typename DerivedR,
+	typename DerivedTV,
+	typename DerivedTT,
+	typename DerivedTF,
+	typename DerivedTR>	
+      IGL_INLINE int tetrahedralize(
+	const Eigen::PlainObjectBase<DerivedV>& V,
+	const Eigen::PlainObjectBase<DerivedF>& F,
+	const Eigen::PlainObjectBase<DerivedH>& H,
+	const Eigen::PlainObjectBase<DerivedR>& R,
+	const std::string switches,
+	Eigen::PlainObjectBase<DerivedTV>& TV,
+        Eigen::PlainObjectBase<DerivedTT>& TT,
+	Eigen::PlainObjectBase<DerivedTF>& TF,
+	Eigen::PlainObjectBase<DerivedTR>& TR, 
+	Eigen::PlainObjectBase<DerivedTT>& TN, 
+	Eigen::PlainObjectBase<DerivedTT>& PT, 
+	Eigen::PlainObjectBase<DerivedTT>& FT, 
+	size_t & numRegions);	      
+
+   }
   }
 }
 
