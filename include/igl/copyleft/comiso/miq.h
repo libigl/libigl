b9a2e3f9be1c403d14806d6d9147c470907497f2
Kacper Pluta <kacper.pluta@bsdlabs.com.br>
2018-10-23 16:11:55 +0300

    changed to camel notation

--- include/igl/copyleft/comiso/miq.h
+++ include/igl/copyleft/comiso/miq.h
@@ -32,13 +32,13 @@ namespace igl
     // PD1               #V by 3 first line of the Jacobian per triangle
     // PD2               #V by 3 second line of the Jacobian per triangle
     //                   (optional, if empty it will be a vector in the tangent plane orthogonal to PD1)
-    // GradientSize      global scaling for the gradient (controls the quads resolution)
-    // Stiffness         weight for the stiffness iterations (Reserved but not used!)
-    // DirectRound       greedily round all integer variables at once (greatly improves optimization speed but lowers quality)
+    // gradientSize      global scaling for the gradient (controls the quads resolution)
+    // stiffness         weight for the stiffness iterations (Reserved but not used!)
+    // directRound       greedily round all integer variables at once (greatly improves optimization speed but lowers quality)
     // iter              stiffness iterations (0 = no stiffness)
     // localIter         number of local iterations for the integer rounding
-    // DoRound           enables the integer rounding (disabling it could be useful for debugging)
-    // SingularityRound  set true/false to decide if the singularities' coordinates should be rounded to the nearest integers
+    // doRound           enables the integer rounding (disabling it could be useful for debugging)
+    // singularityRound  set true/false to decide if the singularities' coordinates should be rounded to the nearest integers
     // roundVertices     id of additional vertices that should be snapped to integer coordinates
     // hardFeatures      #H by 2 list of pairs of vertices that belongs to edges that should be snapped to integer coordinates
     //
@@ -54,18 +54,19 @@ namespace igl
       const Eigen::PlainObjectBase<DerivedV> &PD2,
       Eigen::PlainObjectBase<DerivedU> &UV,
       Eigen::PlainObjectBase<DerivedF> &FUV,
-      double GradientSize = 30.0,
-      double Stiffness = 5.0,
-      bool DirectRound = false,
+      double gradientSize = 30.0,
+      double stiffness = 5.0,
+      bool directRound = false,
       int iter = 5,
       int localIter = 5,
-      bool DoRound = true,
-      bool SingularityRound = true,
+      bool doRound = true,
+      bool singularityRound = true,
       std::vector<int> roundVertices = std::vector<int>(),
       std::vector<std::vector<int> > hardFeatures = std::vector<std::vector<int> >());
 
     // Helper function that allows to directly provided pre-combed bisectors for an already cut mesh
-    // Input;
+
+    // Input:
     // V                  #V by 3 list of mesh vertex 3D positions
     // F                  #F by 3 list of faces indices in V
 
@@ -76,14 +77,14 @@ namespace igl
     // Singular           #V list of flag that denotes if a vertex is singular or not
     // Seams              #F by 3 list of per-corner flag that denotes seams
 
-    // Input;
-    // GradientSize       global scaling for the gradient (controls the quads resolution)
-    // Stiffness          weight for the stiffness iterations (Reserved but not used!)
-    // DirectRound        greedily round all integer variables at once (greatly improves optimization speed but lowers quality)
+    // Input:
+    // gradientSize       global scaling for the gradient (controls the quads resolution)
+    // stiffness          weight for the stiffness iterations (Reserved but not used!)
+    // directRound        greedily round all integer variables at once (greatly improves optimization speed but lowers quality)
     // iter               stiffness iterations (0 = no stiffness)
     // localIter          number of local iterations for the integer rounding
-    // DoRound            enables the integer rounding (disabling it could be useful for debugging)
-    // SingularityRound   set true/false to decide if the singularities' coordinates should be rounded to the nearest integers
+    // doRound            enables the integer rounding (disabling it could be useful for debugging)
+    // singularityRound   set true/false to decide if the singularities' coordinates should be rounded to the nearest integers
     // roundVertices      id of additional vertices that should be snapped to integer coordinates
     // hardFeatures       #H by 2 list of pairs of vertices that belongs to edges that should be snapped to integer coordinates
 
@@ -97,17 +98,17 @@ namespace igl
       const Eigen::PlainObjectBase<DerivedV> &PD1_combed,
       const Eigen::PlainObjectBase<DerivedV> &PD2_combed,
       const Eigen::Matrix<int, Eigen::Dynamic, 3> &MMatch,
-      const Eigen::Matrix<int, Eigen::Dynamic, 1> &Singular,
-      const Eigen::Matrix<int, Eigen::Dynamic, 3> &Seams,
+      const Eigen::Matrix<int, Eigen::Dynamic, 1> &singular,
+      const Eigen::Matrix<int, Eigen::Dynamic, 3> &seams,
       Eigen::PlainObjectBase<DerivedU> &UV,
       Eigen::PlainObjectBase<DerivedF> &FUV,
-      double GradientSize = 30.0,
-      double Stiffness = 5.0,
-      bool DirectRound = false,
+      double gradientSize = 30.0,
+      double stiffness = 5.0,
+      bool directRound = false,
       int iter = 5,
       int localIter = 5,
-      bool DoRound = true,
-      bool SingularityRound = true,
+      bool doRound = true,
+      bool singularityRound = true,
       std::vector<int> roundVertices = std::vector<int>(),
       std::vector<std::vector<int> > hardFeatures = std::vector<std::vector<int> >());
   };
