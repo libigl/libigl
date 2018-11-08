6b2da9a7baa3d97027bd3831d181c7e4caee3e9b
Kacper Pluta <kacper.pluta@bsdlabs.com.br>
2018-11-08 10:43:06 +0200

    This PR allows for extra exceptions in the debug mode (GCC/LLVM) on Linux based OSes - it does not work on the other operating systems. Relevant documentation: https://linux.die.net/man/3/feenableexcept

--- tests/include/igl/mosek/main.cpp
+++ tests/include/igl/mosek/main.cpp
@@ -1,6 +1,19 @@
 #include <gtest/gtest.h>
 
+#ifndef NDEBUG
+#ifdef __linux__
+#include <fenv.h>
+#endif
+#endif
+
+
 int main(int argc, char **argv) {
-      ::testing::InitGoogleTest(&argc, argv);
-        return RUN_ALL_TESTS();
+    
+#ifndef NDEBUG
+#ifdef __linux__
+  feenableexcept(FE_DIVBYZERO | FE_INVALID | FE_OVERFLOW);
+#endif
+#endif     
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
