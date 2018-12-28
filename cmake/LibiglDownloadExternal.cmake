a5cd601cb57d68fab017b60d4674a27b8f247304
Jérémie Dumas <jeremie.dumas@ens-lyon.org>
2018-12-28 15:19:11 +0100

    Fix default cmake variables.

--- cmake/LibiglDownloadExternal.cmake
+++ cmake/LibiglDownloadExternal.cmake
@@ -50,8 +50,8 @@ endfunction()
 ## Eigen
 # 3.2.10 8ad10ac703a78143a4062c9bda9d8fd3
 # 3.3.7  f2a417d083fe8ca4b8ed2bc613d20f07
-option(LIBIGL_EIGEN_VERSION "Default version of Eigen used by libigl." 3.2.10)
-option(LIBIGL_EIGEN_MD5     "md5sum of the tar.gz archive corresponding to this version." 8ad10ac703a78143a4062c9bda9d8fd3)
+set(LIBIGL_EIGEN_VERSION 3.2.10                           CACHE STRING "Default version of Eigen used by libigl.")
+set(LIBIGL_EIGEN_MD5     8ad10ac703a78143a4062c9bda9d8fd3 CACHE STRING "md5sum of the tar.gz archive corresponding to this version.")
 function(igl_download_eigen)
 	igl_download_project(eigen
 		URL           http://bitbucket.org/eigen/eigen/get/${LIBIGL_EIGEN_VERSION}.tar.gz
