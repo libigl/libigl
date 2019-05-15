1408bd36b0070e9a36af889d92b3c1a6262e4963
Nico Brügel <nico.bruegel@htwg-konstanz.de>
2019-05-15 11:33:20 +0200

    fix unit test for path_to_executable() on windows by taking into account that it has a '.exe' suffix.

--- tests/include/igl/path_to_executable.cpp
+++ tests/include/igl/path_to_executable.cpp
@@ -8,8 +8,8 @@ TEST_CASE("path_to_executable: example", "[igl]")
 {
   std::string path_to_executable = igl::path_to_executable();
   REQUIRE(0 < path_to_executable.size());
-  // check if path_to_executable ends with correct file name
-  std::string ending = "libigl_tests";
-  REQUIRE(ending.size() < path_to_executable.size());
-  REQUIRE(std::equal(ending.rbegin(), ending.rend(), path_to_executable.rbegin()));
+  // check if path_to_executable ends with correct file name, on windows .exe suffix is added.
+  std::string executable = "libigl_tests";
+  int pos = path_to_executable.length()-(executable.length() + 4/*".exe"*/);
+  REQUIRE( std::string::npos != path_to_executable.find(executable, pos));
 }
