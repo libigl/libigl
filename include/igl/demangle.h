4e0eebb748eb737d6939b76eceba31c13b50dc06
Jérémie Dumas <jeremie.dumas@ens-lyon.org>
2019-05-18 12:24:41 -0400

    Hotfix

--- include/igl/demangle.h
+++ include/igl/demangle.h
@@ -31,6 +31,8 @@ namespace igl
   inline void replaceString(std::string& str) {
     replaceAll(str, "std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > >", "std::string>");
     replaceAll(str, "std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >", "std::string");
+    replaceAll(str, "std::__1::basic_string<char, std::__1::char_traits<char>, std::__1::allocator<char> > >", "std::string>");
+    replaceAll(str, "std::__1::basic_string<char, std::__1::char_traits<char>, std::__1::allocator<char> >", "std::string");
   }
 
 #ifdef _MSC_VER
