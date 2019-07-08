ddbbb727feb2a82e7d771c2dacae60fe0aa7c4cd
KazutakaNakashima <kazutaka.nakashima@n-taka.info>
2019-07-09 01:41:42 +0200

    bugfix: readWRL

--- include/igl/readWRL.cpp
+++ include/igl/readWRL.cpp
@@ -48,6 +48,24 @@ IGL_INLINE bool igl::readWRL(
     }
     haystack = string(line);
     still_comments = string::npos == haystack.find(needle);
+
+    // in case first vertex position is written in the same line.
+    // i.e. "point[ %lf %lf %lf,"
+    if(!still_comments)
+    {
+      double x,y,z;
+      line = line+7;
+      int floats_read = sscanf(line," %lf %lf %lf,",&x,&y,&z);
+      if(floats_read == 3)
+      {
+      vector<Scalar > point;
+      point.resize(3);
+      point[0] = x;
+      point[1] = y;
+      point[2] = z;
+      V.push_back(point);
+      }
+    }
   }
 
   // read points in sets of 3
@@ -80,6 +98,36 @@ IGL_INLINE bool igl::readWRL(
     fgets(line,1000,wrl_file);
     haystack = string(line);
     still_comments = string::npos == haystack.find(needle);
+
+    line = line+12;
+    // in case first vertex position is written in the same line.
+    // i.e. "coordIndex [ %d %d %d -1,"
+    if(!still_comments)
+    {
+      int ints_read = 1;
+      // read new face indices (until hit -1)
+      vector<Index > face;
+      while(true)
+      {
+        // indices are 0-indexed
+        int i;
+        ints_read = fscanf(line," %d,",&i);
+        if(ints_read > 0)
+        {
+          if(i>=0)
+          {
+            face.push_back(i);
+          }else
+          {
+            F.push_back(face);
+            break;
+          }
+        }else
+        {
+          break;
+        }
+      }
+    }
   }
   // read F
   int ints_read = 1;
