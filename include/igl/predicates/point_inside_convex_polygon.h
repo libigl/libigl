71d1553a01f2a013ee56d2ce2f5a691f072a4719
hanxiao <hankstag@users.noreply.github.com>
2019-08-12 11:04:09 -0400

    Update include/igl/predicates/point_inside_convex_polygon.h
    Co-Authored-By: Zhongshi <zj495@nyu.edu>

--- include/igl/predicates/point_inside_convex_polygon.h
+++ include/igl/predicates/point_inside_convex_polygon.h
@@ -6,7 +6,7 @@
 // v. 2.0. If a copy of the MPL was not distributed with this file, You can
 // obtain one at http://mozilla.org/MPL/2.0/.
 
-#ifndef PREDICATE_POINT_INSIDE_CONVEX_POLYGON_H
+#ifndef IGL_PREDICATES_POINT_INSIDE_CONVEX_POLYGON_H
 #define PREDICATE_POINT_INSIDE_CONVEX_POLYGON_H
 
 
