e84d628a1ee2ac1241013e2479f6547a0b26cb15
Alec Jacobson <alecjacobson@gmail.com>
2019-11-30 13:04:13 -0500

    template

--- include/igl/quad_grid.cpp
+++ include/igl/quad_grid.cpp
@@ -92,4 +92,6 @@ IGL_INLINE void igl::quad_grid(
 }
 
 #ifdef IGL_STATIC_LIBRARY
+// Explicit template instantiation
+template void igl::quad_grid<Eigen::Matrix<double, -1, -1, 0, -1, -1>, Eigen::Matrix<int, -1, -1, 0, -1, -1>, Eigen::Matrix<int, -1, -1, 0, -1, -1> >(int, int, Eigen::PlainObjectBase<Eigen::Matrix<double, -1, -1, 0, -1, -1> >&, Eigen::PlainObjectBase<Eigen::Matrix<int, -1, -1, 0, -1, -1> >&, Eigen::PlainObjectBase<Eigen::Matrix<int, -1, -1, 0, -1, -1> >&);
 #endif
