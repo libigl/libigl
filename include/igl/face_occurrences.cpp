0d740e6fc9a4c87f93646cd3c72b317f24262415
Mathieu Lamarre <mlamarre@ea.com>
2020-01-09 12:24:39 -0500

    Fix compilation error
    When IntegerC is long long int with gcc 7.4

--- include/igl/face_occurrences.cpp
+++ include/igl/face_occurrences.cpp
@@ -1,9 +1,9 @@
 // This file is part of libigl, a simple c++ geometry processing library.
-// 
+//
 // Copyright (C) 2013 Alec Jacobson <alecjacobson@gmail.com>
-// 
-// This Source Code Form is subject to the terms of the Mozilla Public License 
-// v. 2.0. If a copy of the MPL was not distributed with this file, You can 
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License
+// v. 2.0. If a copy of the MPL was not distributed with this file, You can
 // obtain one at http://mozilla.org/MPL/2.0/.
 #include "face_occurrences.h"
 #include "list_to_matrix.h"
@@ -48,7 +48,7 @@ IGL_INLINE void igl::face_occurrences(
   {
     // sorted face should definitely be in counts map
     assert(counts.find(sortedF[i]) != counts.end());
-    C[i] = counts[sortedF[i]];
+    C[i] = static_cast<IntegerC>(counts[sortedF[i]]);
   }
 }
 
