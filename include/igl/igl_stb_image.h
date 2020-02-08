6896a9d61facad4e74fd5d3efbf52ed709d53609
Alec Jacobson <alecjacobson@gmail.com>
2020-02-08 11:50:09 -0500

    angle include to quote include

--- include/igl/igl_stb_image.h
+++ include/igl/igl_stb_image.h
@@ -1,7 +1,7 @@
 #ifndef IGL_STB_IMAGE_H
 #define IGL_STB_IMAGE_H
 
-#include <igl_stb_image_export.h>
+#include "igl_stb_image_export.h"
 
 namespace igl {
   IGL_STB_IMAGE_EXPORT unsigned char * stbi_load(char const *filename, int *x, int *y, int *comp, int req_comp);
