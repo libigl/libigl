22c8b1e8ef5d0091b25806ffd641647d0259cfc7
Jérémie Dumas <jdumas@users.noreply.github.com>
2020-04-11 10:10:37 -0700

    Update face_occurrences.cpp

--- include/igl/face_occurrences.cpp
+++ include/igl/face_occurrences.cpp
@@ -3,7 +3,7 @@
 // Copyright (C) 2013 Alec Jacobson <alecjacobson@gmail.com>
 //
 // This Source Code Form is subject to the terms of the Mozilla Public License
-// v. 2.0. If a copy of the MPL was not distributed with this file, You can
+// v. 2.0. If a copy of the MPL was not distributed with this file, You can 
 // obtain one at http://mozilla.org/MPL/2.0/.
 #include "face_occurrences.h"
 #include "list_to_matrix.h"
