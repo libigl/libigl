f2cb6ed36819025f2f37d812b30d6b29baa95123
Jérémie Dumas <jdumas@users.noreply.github.com>
2020-04-11 10:10:23 -0700

    Update min_quad_with_fixed.cpp

--- include/igl/min_quad_with_fixed.cpp
+++ include/igl/min_quad_with_fixed.cpp
@@ -2,7 +2,7 @@
 //
 // Copyright (C) 2016 Alec Jacobson <alecjacobson@gmail.com>
 //
-// This Source Code Form is subject to the terms of the Mozilla Public License
+// This Source Code Form is subject to the terms of the Mozilla Public License 
 // v. 2.0. If a copy of the MPL was not distributed with this file, You can
 // obtain one at http://mozilla.org/MPL/2.0/.
 #include "min_quad_with_fixed.h"
