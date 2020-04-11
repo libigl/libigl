afff76cbd01fd6731ce03b3d279262643eaaab9a
Jérémie Dumas <jeremie.dumas@ens-lyon.org>
2020-04-11 10:44:30 -0700

    Update cast.

--- include/igl/flood_fill.cpp
+++ include/igl/flood_fill.cpp
@@ -1,34 +1,34 @@
 // This file is part of libigl, a simple c++ geometry processing library.
-// 
+//
 // Copyright (C) 2016 Alec Jacobson <alecjacobson@gmail.com>
-// 
-// This Source Code Form is subject to the terms of the Mozilla Public License 
-// v. 2.0. If a copy of the MPL was not distributed with this file, You can 
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License
+// v. 2.0. If a copy of the MPL was not distributed with this file, You can
 // obtain one at http://mozilla.org/MPL/2.0/.
 #include "flood_fill.h"
 #include <limits>
 
 template <typename Derivedres, typename DerivedS>
 IGL_INLINE void igl::flood_fill(
-  const Eigen::MatrixBase<Derivedres>& res, 
+  const Eigen::MatrixBase<Derivedres>& res,
   Eigen::PlainObjectBase<DerivedS> & S)
 {
   using namespace Eigen;
   using namespace std;
   typedef typename DerivedS::Scalar Scalar;
   const auto flood = [&res,&S] (
-     const int xi, 
-     const int yi, 
+     const int xi,
+     const int yi,
      const int zi,
-     const int signed_xi, 
-     const int signed_yi, 
+     const int signed_xi,
+     const int signed_yi,
      const int signed_zi,
      const Scalar s)
     {
       // flood fill this value back on this row
       for(int bxi = xi;signed_xi<--bxi;)
       {
-        S(int(bxi+res(0)*(yi + res(1)*zi))) = s;
+        S(bxi+int(res(0))*(yi + int(res(1))*zi)) = s;
       }
       // flood fill this value back on any previous rows
       for(int byi = yi;signed_yi<--byi;)
