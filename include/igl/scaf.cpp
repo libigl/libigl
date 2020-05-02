526a7f849084ed65936ee2548ce5a2bdd2900392
Jérémie Dumas <jdumas@users.noreply.github.com>
2020-05-02 12:58:26 -0700

    Update scaf.cpp

--- include/igl/scaf.cpp
+++ include/igl/scaf.cpp
@@ -1,7 +1,7 @@
 // This file is part of libigl, a simple c++ geometry processing library.
 //
 // Copyright (C) 2018 Zhongshi Jiang <jiangzs@nyu.edu>
-//
+// 
 // This Source Code Form is subject to the terms of the Mozilla Public License
 // v. 2.0. If a copy of the MPL was not distributed with this file, You can
 // obtain one at http://mozilla.org/MPL/2.0/.
