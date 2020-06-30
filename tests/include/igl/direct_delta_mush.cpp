a3c3ef157283fbd9bd130fc64642e81ff6ec415c
Alec Jacobson <alecjacobson@gmail.com>
2020-06-30 10:42:50 -0400

    update test too

--- tests/include/igl/direct_delta_mush.cpp
+++ tests/include/igl/direct_delta_mush.cpp
@@ -12,14 +12,14 @@ TEST_CASE("direct_delta_mush: cube", "[igl]")
   // For simplicity, testing on a cube of dimensions 1.0 x 1.0 x 1.0,
   // with all vertices bound strictly to one and only one imaginary bone (weight is a column of 1s)
   igl::read_triangle_mesh(test_common::data_path("cube.off"), V, F);
-  Eigen::SparseMatrix<double> W_sparse = Eigen::MatrixXd::Ones(V.rows(), 1).sparseView();
+  W = Eigen::MatrixXd::Ones(V.rows(), 1);
 
   // Parameters such as p, lambda, kappa, alpha do not matter given identity transform
-  igl::direct_delta_mush_precomputation(V, F, W_sparse, 1, 1., 0.5, 0.5, Omega);
+  igl::direct_delta_mush_precomputation(V, F, W, 1, 1., 0.5, 0.5, Omega);
 
   std::vector<Eigen::Affine3d, Eigen::aligned_allocator<Eigen::Affine3d>> T_list;
   T_list.push_back(Eigen::Affine3d::Identity());
-  igl::direct_delta_mush(V, F, T_list, Omega, U);
+  igl::direct_delta_mush(V, T_list, Omega, U);
 
   test_common::assert_near(U, V, 1e-4);
 }
