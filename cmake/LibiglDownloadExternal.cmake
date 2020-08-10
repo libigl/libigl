98a1a31641e454b082a3b5816be5883d08d99248
michelle <michelle.dimarco@mail.utoronto.ca>
2020-08-10 15:00:07 -0400

    ImGuizmo Integration (#1568)
    * First working version to automatically clone ImGuizmo into libigl
    
    * Corresponding changes to cmake files
    
    * Moved contents of slicing plugin and menu extra to new file ImGuizmoMenu in imgui directory of igl
    
    * Moved slicing plug into main, and ImGuizmo::EditTransform into ImGuizmoMenu which resides in igl
    
    * Temporary hid Local and World options because they don't do anything and refactored main for tutorial 109 for clarity with some documentation
    
    * Got orthogonal projection to render ImGuizmo normally
    
    * Removed unecessary changes
    
    * Removed snap parameter from ImGuizmo menu
    
    * Added a cmake configuration for ImGuizmo
    
    * Include imgui library in building ImGuizmo target
    
    * Added a workflow
    
    * Added this branch to workflow
    
    * Update linux packages
    
    * Explicitly include imgui header for imguizmo
    
    * Just copy in relevant imguizmo files for now as opposed to through cmake since ImGuizmo repo does not have its own CMake file
    
    * Conditional include to prevent mac build from failing
    
    * Revert files for debugging cmake build
    
    * Added build target for ImGuizmo
    
    * Expose Imgui lib to ImGuizmo with PUBLIC interface
    
    * Refactor build
    
    * Get older SHA1 commit to avoid break on macOS
    
    * Change imguizmo menu namespace
    
    * Cleaning up + fix window border size issue.
    
    Co-authored-by: Jérémie Dumas <jeremie.dumas@ens-lyon.org>

--- cmake/LibiglDownloadExternal.cmake
+++ cmake/LibiglDownloadExternal.cmake
@@ -112,6 +112,15 @@ function(igl_download_imgui)
 	)
 endfunction()
 
+## ImGuizmo
+function(igl_download_imguizmo)
+	igl_download_project(imguizmo
+		GIT_REPOSITORY https://github.com/CedricGuillemet/ImGuizmo.git
+		GIT_TAG        a23567269f6617342bcc112394bdad937b54b2d7
+		${LIBIGL_BRANCH_OPTIONS}
+	)
+endfunction()
+
 ## pybind11
 function(igl_download_pybind11)
 	igl_download_project(pybind11
