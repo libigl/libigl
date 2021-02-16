1a309313fc7c0773afd50d4a1ef171edaa7da50b
Jeremie Dumas <jedumas@adobe.com>
2021-02-15 17:56:38 -0800

    Update ImGui to 1.81+
    Fixes #1669 and #1717

--- cmake/LibiglDownloadExternal.cmake
+++ cmake/LibiglDownloadExternal.cmake
@@ -103,12 +103,12 @@ endfunction()
 function(igl_download_imgui)
 	igl_download_project(imgui
 		GIT_REPOSITORY https://github.com/ocornut/imgui.git
-		GIT_TAG        v1.76
+		GIT_TAG        61b19489f1ba35934d9114c034b24eb5bff149e7 # 1.81 + patch for #1669
 		${LIBIGL_BRANCH_OPTIONS}
 	)
 	igl_download_project(libigl-imgui
 		GIT_REPOSITORY https://github.com/libigl/libigl-imgui.git
-		GIT_TAG        99f0643089b19f6daf5b3efd9544a65c9a851966
+		GIT_TAG        7e1053e750b0f4c129b046f4e455243cb7f804f3
 	)
 endfunction()
 
