ec9fe116f4073848edaa084d309da519fc89bb60
Jérémie Dumas <jdumas@users.noreply.github.com>
2021-02-20 16:16:42 -0800

    Update libigl.cmake

--- cmake/libigl.cmake
+++ cmake/libigl.cmake
@@ -1,4 +1,4 @@
-cmake_minimum_required(VERSION 3.1)
+cmake_minimum_required(VERSION 3.8)
 
 # https://github.com/libigl/libigl/issues/751
 # http://lists.llvm.org/pipermail/llvm-commits/Week-of-Mon-20160425/351643.html
