e41e9ed5a59399f38af1072bc878200fe949e5aa
lchaooo <chaoli95nyc@gmail.com>
2021-03-28 03:28:06 -0400

    Fix the order of principal curvature

--- tutorial/203_CurvatureDirections/main.cpp
+++ tutorial/203_CurvatureDirections/main.cpp
@@ -51,12 +51,12 @@ int main(int argc, char *argv[])
   // Average edge length for sizing
   const double avg = igl::avg_edge_length(V,F);
 
-  // Draw a blue segment parallel to the minimal curvature direction
-  const RowVector3d red(0.8,0.2,0.2),blue(0.2,0.2,0.8);
-  viewer.data().add_edges(V + PD1*avg, V - PD1*avg, blue);
-
   // Draw a red segment parallel to the maximal curvature direction
-  viewer.data().add_edges(V + PD2*avg, V - PD2*avg, red);
+  const RowVector3d red(0.8,0.2,0.2),blue(0.2,0.2,0.8);
+  viewer.data().add_edges(V + PD1*avg, V - PD1*avg, red);
+
+  // Draw a blue segment parallel to the minimal curvature direction
+  viewer.data().add_edges(V + PD2*avg, V - PD2*avg, blue);
 
   // Hide wireframe
   viewer.data().show_lines = false;
