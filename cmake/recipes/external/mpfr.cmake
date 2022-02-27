67b07f6d4865f00c3359febcf728904d382e393d
Alec Jacobson <alecjacobson@gmail.com>
2022-02-26 23:55:55 -0500

    organize gmp and mpfr in _deps with others

--- cmake/recipes/external/mpfr.cmake
+++ cmake/recipes/external/mpfr.cmake
@@ -14,29 +14,34 @@ if(WIN32)
 else()
   message(STATUS "Third-party: creating target 'mpfr::mpfr'")
 
-  set(mpfr_INSTALL ${PROJECT_BINARY_DIR}/mpfr-install)
-  set(mpfr_LIBRARY ${mpfr_INSTALL}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}mpfr${CMAKE_STATIC_LIBRARY_SUFFIX})
-  set(mpfr_INCLUDE_DIR ${mpfr_INSTALL}/include)
+  include(FetchContent)
   include(ProcessorCount)
   ProcessorCount(Ncpu)
   include(ExternalProject)
+  set(prefix ${FETCHCONTENT_BASE_DIR}/mpfr)
+  set(mpfr_INSTALL ${prefix}/install)
+  set(mpfr_LIBRARY ${mpfr_INSTALL}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}mpfr${CMAKE_STATIC_LIBRARY_SUFFIX})
+  set(mpfr_INCLUDE_DIR ${mpfr_INSTALL}/include)
+
   ExternalProject_Add(mpfr
-  DEPENDS gmp
-  URL  https://ftp.gnu.org/gnu/mpfr/mpfr-4.1.0.tar.xz
-  URL_MD5 bdd3d5efba9c17da8d83a35ec552baef
-  UPDATE_DISCONNECTED true  # need this to avoid constant rebuild
-  CONFIGURE_HANDLED_BY_BUILD ON  # avoid constant reconfigure
-  CONFIGURE_COMMAND 
-    ${PROJECT_BINARY_DIR}/mpfr-prefix/src/mpfr/configure 
-    --disable-debug --disable-dependency-tracking  --disable-silent-rules --enable-cxx --with-pic
-    --with-gmp-include=${gmp_INCLUDE_DIR} --with-gmp-lib=${gmp_LIB_DIR}
-    --disable-shared
-    --prefix=${mpfr_INSTALL}
-    --disable-shared
-  BUILD_COMMAND make -j${Ncpu}
-  INSTALL_COMMAND make -j${Ncpu} install
-  TEST_COMMAND ""
-  BUILD_BYPRODUCTS ${mpfr_LIBRARY}
+    PREFIX ${prefix}
+    DEPENDS gmp
+    URL  https://ftp.gnu.org/gnu/mpfr/mpfr-4.1.0.tar.xz
+    URL_MD5 bdd3d5efba9c17da8d83a35ec552baef
+    UPDATE_DISCONNECTED true  # need this to avoid constant rebuild
+    CONFIGURE_HANDLED_BY_BUILD ON  # avoid constant reconfigure
+    CONFIGURE_COMMAND 
+      ${prefix}/src/mpfr/configure 
+      --disable-debug --disable-dependency-tracking  --disable-silent-rules --enable-cxx --with-pic
+      --with-gmp-include=${gmp_INCLUDE_DIR} --with-gmp-lib=${gmp_LIB_DIR}
+      --disable-shared
+      --prefix=${mpfr_INSTALL}
+      --disable-shared
+    BUILD_COMMAND make -j${Ncpu}
+    INSTALL_COMMAND make -j${Ncpu} install
+    INSTALL_DIR ${mpfr_INSTALL}
+    TEST_COMMAND ""
+    BUILD_BYPRODUCTS ${mpfr_LIBRARY}
   )
   #PATCH_COMMAND  curl "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff" "|" sed -e "s/configure.orig/configure/g" "|" git apply -v
   ExternalProject_Get_Property(mpfr SOURCE_DIR)
