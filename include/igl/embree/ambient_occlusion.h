4fff4670d30bb4653a39e0925051edfc2916d2fe
Alec Jacobson <alecjacobson@gmail.com>
2022-04-01 19:28:14 -0400

    Update ambient_occlusion.h

--- include/igl/embree/ambient_occlusion.h
+++ include/igl/embree/ambient_occlusion.h
@@ -13,30 +13,17 @@ namespace igl
 {
   namespace embree
   {
-    // Forward define
-    class EmbreeIntersector;
     // Compute ambient occlusion per given point
     //
     // Inputs:
-    //    ei  EmbreeIntersector containing (V,F)
+    //    V  #V by 3 list of mesh vertex positiosn
+    //    F  #F by 3 list of mesh triangle indices into rows of V
     //    P  #P by 3 list of origin points
     //    N  #P by 3 list of origin normals
     // Outputs:
     //    S  #P list of ambient occlusion values between 1 (fully occluded) and
     //      0 (not occluded)
     //
-    template <
-      typename DerivedP,
-      typename DerivedN,
-      typename DerivedS >
-    IGL_INLINE void ambient_occlusion(
-      const EmbreeIntersector & ei,
-      const Eigen::MatrixBase<DerivedP> & P,
-      const Eigen::MatrixBase<DerivedN> & N,
-      const int num_samples,
-      Eigen::PlainObjectBase<DerivedS> & S);
-    // Wrapper which builds new EmbreeIntersector for (V,F). That's expensive so
-    // avoid this if repeatedly calling.
     template <
       typename DerivedV,
       typename DerivedF,
@@ -46,6 +33,29 @@ namespace igl
     IGL_INLINE void ambient_occlusion(
       const Eigen::MatrixBase<DerivedV> & V,
       const Eigen::MatrixBase<DerivedF> & F,
+      const Eigen::MatrixBase<DerivedP> & P,
+      const Eigen::MatrixBase<DerivedN> & N,
+      const int num_samples,
+      Eigen::PlainObjectBase<DerivedS> & S);
+    // Wrapper which builds new EmbreeIntersector for (V,F). That's expensive so
+    // avoid this if repeatedly calling.
+    //
+    // Inputs:
+    //    ei  EmbreeIntersector containing (V,F)
+    //    P  #P by 3 list of origin points
+    //    N  #P by 3 list of origin normals
+    // Outputs:
+    //    S  #P list of ambient occlusion values between 1 (fully occluded) and
+    //      0 (not occluded)
+    //
+    // Forward define
+    class EmbreeIntersector;
+    template <
+      typename DerivedP,
+      typename DerivedN,
+      typename DerivedS >
+    IGL_INLINE void ambient_occlusion(
+      const EmbreeIntersector & ei,
       const Eigen::MatrixBase<DerivedP> & P,
       const Eigen::MatrixBase<DerivedN> & N,
       const int num_samples,
