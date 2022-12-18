2b7d8e0cac39dc749ab52cbda3bc7b7f9cf91696
Justen Di Ruscio <justend29@gmail.com>
2022-12-18 12:25:32 -0400

    add find_package() option to error message

--- cmake/libigl.cmake
+++ cmake/libigl.cmake
@@ -4,8 +4,9 @@ get_filename_component(PATH_Y "${CMAKE_CURRENT_LIST_DIR}/.." REALPATH)
 if(NOT PATH_X STREQUAL PATH_Y)
     message(FATAL_ERROR "You included libigl.cmake directly from your own project. This behavior "
                         "is not supported anymore. Please add libigl to your project via "
-                        "add_subdirectory(<path_to_libigl>). See the libigl example project for "
-                        "more information: https://github.com/libigl/libigl-example-project/")
+                        "find_package(libigl CONFIG) or add_subdirectory(<path_to_libigl>). See "
+                        "the libigl example project for more information: "
+                        "https://github.com/libigl/libigl-example-project/")
 endif()
 
 # Global options
