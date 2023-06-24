f56099372b2b0cd67296f7a0a12d171818debca2
Alec Jacobson <alecjacobson@gmail.com>
2023-06-24 12:03:37 -0400

    lscm hessian and spectral

--- include/igl/lscm_hessian.h
+++ include/igl/lscm_hessian.h
@@ -1,15 +1,12 @@
 // This file is part of libigl, a simple c++ geometry processing library.
 //
-// Copyright (C) 2014 Daniele Panozzo <daniele.panozzo@gmail.com>
-//               2015 Alec Jacobson
+// Copyright (C) 2023 Alec Jacobson
 //
 // This Source Code Form is subject to the terms of the Mozilla Public License
 // v. 2.0. If a copy of the MPL was not distributed with this file, You can
 // obtain one at http://mozilla.org/MPL/2.0/.
-#ifndef IGL_LSCM_H
-#define IGL_LSCM_H
-#include "igl_inline.h"
-
+#ifndef IGL_LSCM_HESSIAN_H
+#define IGL_LSCM_HESSIAN_H
 #include <Eigen/Dense>
 #include <Eigen/Sparse>
 
@@ -21,7 +18,7 @@ namespace igl
   // Generation" [Lévy et al. 2002]), though this implementation follows the
   // derivation in: "Spectral Conformal Parameterization" [Mullen et al. 2008]
   // (note, this does **not** implement the Eigen-decomposition based method in
-  // [Mullen et al. 2008], see below). Input should be a manifold
+  // [Mullen et al. 2008], which is not equivalent). Input should be a manifold
   // mesh (also no unreferenced vertices) and "boundary" (fixed vertices) `b`
   // should contain at least two vertices per connected component.
   //
@@ -35,48 +32,15 @@ namespace igl
   //   Q  #Vx2 by #Vx2 symmetric positive semi-definite matrix for computing LSCM energy
   // Returns true only on solver success.
   //
-  template <
-    typename DerivedV, 
-    typename DerivedF, 
-    typename Derivedb, 
-    typename Derivedbc, 
-    typename DerivedV_uv, 
-    typename QScalar>
-  IGL_INLINE bool lscm(
+  template <typename DerivedV, typename DerivedF, typename QScalar>
+  void lscm_hessian(
     const Eigen::MatrixBase<DerivedV> & V,
     const Eigen::MatrixBase<DerivedF> & F,
-    const Eigen::MatrixBase<Derivedb> & b,
-    const Eigen::MatrixBase<Derivedbc> & bc,
-    Eigen::PlainObjectBase<DerivedV_uv> & V_uv,
     Eigen::SparseMatrix<QScalar> & Q);
-  // Wrapper where the output Q is discarded
-  template <
-    typename DerivedV, 
-    typename DerivedF, 
-    typename Derivedb, 
-    typename Derivedbc, 
-    typename DerivedV_uv>
-  IGL_INLINE bool lscm(
-    const Eigen::MatrixBase<DerivedV> & V,
-    const Eigen::MatrixBase<DerivedF> & F,
-    const Eigen::MatrixBase<Derivedb> & b,
-    const Eigen::MatrixBase<Derivedbc> & bc,
-    Eigen::PlainObjectBase<DerivedV_uv> & V_uv);
-  // Free boundary version. "Spectral Conformal Parameterization" using Eigen
-  // decomposition. Assumes mesh is a single connected component topologically
-  // equivalent to a chunk of the plane.
-  template <
-    typename DerivedV, 
-    typename DerivedF, 
-    typename DerivedV_uv>
-  IGL_INLINE bool lscm(
-    const Eigen::MatrixBase<DerivedV> & V,
-    const Eigen::MatrixBase<DerivedF> & F,
-    Eigen::PlainObjectBase<DerivedV_uv> & V_uv);
 }
 
 #ifndef IGL_STATIC_LIBRARY
-#  include "lscm.cpp"
+#include "lscm_hessian.cpp"
 #endif
 
-#endif
+#endif 
