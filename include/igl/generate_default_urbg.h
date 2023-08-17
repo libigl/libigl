a3c90256c3f4524b32da2cb546f9593ff8b23dba
Alec Jacobson <alecjacobson@gmail.com>
2023-08-17 10:42:13 -0400

    revert to jdumas std::rand seed for default

--- include/igl/generate_default_urbg.h
+++ include/igl/generate_default_urbg.h
@@ -6,8 +6,7 @@ namespace igl {
   using DEFAULT_URBG = std::mt19937;
   inline DEFAULT_URBG generate_default_urbg() 
   {
-    std::random_device rd;
-    return DEFAULT_URBG(rd());
+    return DEFAULT_URBG(std::rand());
   }
 }
 #endif
