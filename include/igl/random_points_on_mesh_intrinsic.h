62fe771ae1345bc0695020ac503fe05304c67a26
Alec Jacobson <alecjacobson@gmail.com>
2023-08-17 23:43:54 -0400

    Fix blue_noise and random_points_on_mesh reproducibility (#2235)
    * Fix `blue_noise` and `random_points_on_mesh` reproducibility
    
    - added new overload for `blue_noise` and `random_points_on_mesh` for that accept an UnformRandomBitGenerator as input. The default signature (ie without URBG) still works, and uses by default std::minstd_rand initialized with random seed generated by std::rand(). This is following the existing behaviour of `randperm`.
    - added reproducibility test case for `blue_noise` and `random_points_on_mesh`
    
    * `blue_noise` and `random_points_on_mesh` overloads without an URBG parameter now use a fixed default seed
    
    ie `std::minstd_rand()`
    
    default seed
    
    * Fix static compilation of tutorials
    
    * revert to jdumas std::rand seed for default
    
    * missing templates on linux
    
    * more missing linux templates
    
    * use std names rather than expansions in explicit templates
    
    ---------
    
    Co-authored-by: stourneux <stourneux@buf.com>
    Co-authored-by: seb-tourneux <sebastientourneux1@gmail.com>

--- include/igl/random_points_on_mesh_intrinsic.h
+++ include/igl/random_points_on_mesh_intrinsic.h
@@ -5,8 +5,8 @@
 // This Source Code Form is subject to the terms of the Mozilla Public License 
 // v. 2.0. If a copy of the MPL was not distributed with this file, You can 
 // obtain one at http://mozilla.org/MPL/2.0/.
-#ifndef IGL_RANDOM_POINTS_ON_MESH_H
-#define IGL_RANDOM_POINTS_ON_MESH_H
+#ifndef IGL_RANDOM_POINTS_ON_MESH_INTRINSIC_H
+#define IGL_RANDOM_POINTS_ON_MESH_INTRINSIC_H
 
 #include "igl_inline.h"
 #include "generate_default_urbg.h"
@@ -18,34 +18,50 @@ namespace igl
   /// Randomly sample a mesh (V,F) n times.
   ///
   /// @param[in] n  number of samples
-  /// @param[in] V  #V by dim list of mesh vertex positions
-  /// @param[in] F  #F by 3 list of mesh triangle indices
+  /// @param[in] dblA  #F list of double areas of triangles
   /// @param[out] B  n by 3 list of barycentric coordinates, ith row are coordinates of
   ///     ith sampled point in face FI(i)
   /// @param[in] urbg An instance of UnformRandomBitGenerator (e.g.,
   ///  `std::minstd_rand(0)`)
   /// @param[out] FI  n list of indices into F 
   /// @param[in,out] urbg An instance of UnformRandomBitGenerator.
-  /// @param[out] X  n by dim list of sample positions.
   template <
-    typename DerivedV, 
-    typename DerivedF, 
+    typename DeriveddblA,
     typename DerivedB, 
     typename DerivedFI,
-    typename DerivedX,
     typename URBG = DEFAULT_URBG>
-  IGL_INLINE void random_points_on_mesh(
+  IGL_INLINE void random_points_on_mesh_intrinsic(
     const int n,
-    const Eigen::MatrixBase<DerivedV > & V,
-    const Eigen::MatrixBase<DerivedF > & F,
+    const Eigen::MatrixBase<DeriveddblA > & dblA,
     Eigen::PlainObjectBase<DerivedB > & B,
     Eigen::PlainObjectBase<DerivedFI > & FI,
-    Eigen::PlainObjectBase<DerivedX> & X,
     URBG && urbg = igl::generate_default_urbg());
+  /// \overload
+  ///
+  /// @param[in] num_vertices  number of vertices in mesh
+  /// @param[in] F  #F by 3 list of mesh triangle indices
+  /// @param[out] B n by num_vertices sparse matrix so that  B*V produces a list
+  ///   of sample points if dbl = doublearea(V,F)
+  ///
+  template <
+    typename DeriveddblA,
+    typename DerivedF,
+    typename ScalarB, 
+    typename DerivedFI,
+    typename URBG>
+  IGL_INLINE void random_points_on_mesh_intrinsic(
+    const int n,
+    const Eigen::MatrixBase<DeriveddblA > & dblA,
+    const int num_vertices,
+    const Eigen::MatrixBase<DerivedF> & F,
+    Eigen::SparseMatrix<ScalarB > & B,
+    Eigen::PlainObjectBase<DerivedFI > & FI,
+    URBG && urbg);
 }
 
 #ifndef IGL_STATIC_LIBRARY
-#  include "random_points_on_mesh.cpp"
+#  include "random_points_on_mesh_intrinsic.cpp"
 #endif
 
 #endif
+
