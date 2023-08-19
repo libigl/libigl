10c7130a2e71b9a5f3146c1d22f437e405541bf6
Alec Jacobson <alecjacobson@gmail.com>
2023-08-19 12:03:03 -0400

    split up 805

---
+++ tutorial/805_MeshImplicitFunction/contours.h
@@ -0,0 +1,11 @@
+#include <Eigen/Core>
+
+void contours(
+  Eigen::MatrixXd & V,
+  Eigen::MatrixXi & E,
+  Eigen::MatrixXd & VV,
+  Eigen::MatrixXi & FF,
+  Eigen::MatrixXd & NN,
+  Eigen::MatrixXd & mcV,
+  Eigen::MatrixXi & mcF,
+  Eigen::MatrixXd & mcN);
