d7954041d1f21501e3d4776464a59d0251dde184
Alec Jacobson <alecjacobson@gmail.com>
2023-08-20 20:50:36 -0400

    small changes for python to compile

--- include/igl/bijective_composite_harmonic_mapping.cpp
+++ include/igl/bijective_composite_harmonic_mapping.cpp
@@ -52,7 +52,7 @@ IGL_INLINE bool igl::bijective_composite_harmonic_mapping(
   int tries = 0;
   int nsteps = min_steps;
   Eigen::Matrix<typename Derivedbc::Scalar, Eigen::Dynamic, Eigen::Dynamic> bc0;
-  slice(V,b,1,bc0);
+  slice(V,b.col(0),1,bc0);
 
   // It's difficult to check for flips "robustly" in the sense that the input
   // mesh might not have positive/consistent sign to begin with.
@@ -83,7 +83,7 @@ IGL_INLINE bool igl::bijective_composite_harmonic_mapping(
         //mw.save(bct,"bct");
         //mw.write("numerical.mat");
         harmonic(Eigen::Matrix<typename DerivedU::Scalar, Eigen::Dynamic, Eigen::Dynamic>(U), F, b, bct, 1, U);
-        igl::slice(U,b,1,bct);
+        igl::slice(U,b.col(0),1,bct);
         nans = (U.array() != U.array()).count();
         if(test_for_flips)
         {
