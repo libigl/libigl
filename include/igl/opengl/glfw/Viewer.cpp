7acd8ba6ca32ae79c62983651ee0debd99b3bbc7
Alec Jacobson <alecjacobson@gmail.com>
2023-09-13 15:23:20 -0400

    bump runtime to 4.1 too

--- include/igl/opengl/glfw/Viewer.cpp
+++ include/igl/opengl/glfw/Viewer.cpp
@@ -147,8 +147,8 @@ namespace glfw
       return EXIT_FAILURE;
     }
     glfwWindowHint(GLFW_SAMPLES, 8);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     #ifdef __APPLE__
       glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
       glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
