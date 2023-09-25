1b07ebcf9e18c33ba342fd5c945dedbda055df78
Alec Jacobson <alecjacobson@gmail.com>
2023-09-25 16:27:01 -0400

    rm vector overloads and simplify api (#2290) [ci skip]

--- include/igl/copyleft/tetgen/tetrahedralize.cpp
+++ include/igl/copyleft/tetgen/tetrahedralize.cpp
@@ -18,22 +18,43 @@
 #include <cassert>
 #include <iostream>
 
+template <
+  typename DerivedV,
+  typename DerivedF,
+  typename DerivedH,
+  typename DerivedVM,
+  typename DerivedFM,
+  typename DerivedR,
+  typename DerivedTV,
+  typename DerivedTT,
+  typename DerivedTF,
+  typename DerivedTM,
+  typename DerivedTR,
+  typename DerivedTN,
+  typename DerivedPT,
+  typename DerivedFT>
 IGL_INLINE int igl::copyleft::tetgen::tetrahedralize(
-  const std::vector<std::vector<REAL > > & V,
-  const std::vector<std::vector<int> > & F,
+  const Eigen::MatrixBase<DerivedV>& V,
+  const Eigen::MatrixBase<DerivedF>& F,
+  const Eigen::MatrixBase<DerivedH>& H,
+  const Eigen::MatrixBase<DerivedVM>& VM,
+  const Eigen::MatrixBase<DerivedFM>& FM,
+  const Eigen::MatrixBase<DerivedR>& R,
   const std::string switches,
-  std::vector<std::vector<REAL > > & TV,
-  std::vector<std::vector<int > > & TT,
-  std::vector<std::vector<int> > & TF)
+  Eigen::PlainObjectBase<DerivedTV>& TV,
+  Eigen::PlainObjectBase<DerivedTT>& TT,
+  Eigen::PlainObjectBase<DerivedTF>& TF,
+  Eigen::PlainObjectBase<DerivedTM>& TM,
+  Eigen::PlainObjectBase<DerivedTR>& TR, 
+  Eigen::PlainObjectBase<DerivedTN>& TN, 
+  Eigen::PlainObjectBase<DerivedPT>& PT, 
+  Eigen::PlainObjectBase<DerivedFT>& FT, 
+  int & num_regions)
 {
-  using namespace std;
+  // REAL is what tetgen is using as floating point precision
+  // Prepare input
   tetgenio in,out;
-  bool success;
-  success = mesh_to_tetgenio(V,F,in);
-  if(!success)
-  {
-    return -1;
-  }
+  mesh_to_tetgenio(V,F,H,VM,FM,R,in);
   try
   {
     char * cswitches = new char[switches.size() + 1];
@@ -42,20 +63,27 @@ IGL_INLINE int igl::copyleft::tetgen::tetrahedralize(
     delete[] cswitches;
   }catch(int e)
   {
-    cerr<<"^"<<__FUNCTION__<<": TETGEN CRASHED... KABOOOM!!!"<<endl;
+    std::cerr<<"^"<<__FUNCTION__<<": TETGEN CRASHED... KABOOOM!!!"<<std::endl;
     return 1;
   }
   if(out.numberoftetrahedra == 0)
   {
-    cerr<<"^"<<__FUNCTION__<<": Tetgen failed to create tets"<<endl;
+    std::cerr<<"^"<<__FUNCTION__<<": Tetgen failed to create tets"<<std::endl;
     return 2;
   }
-  success = tetgenio_to_tetmesh(out,TV,TT,TF);
-  if(!success)
+
+  // Prepare output
+  bool ret = tetgenio_to_tetmesh(out,TV,TT,TF,TM,TR,TN,PT,FT,num_regions);
+  if(!ret)
   {
     return -1;
   }
-  boundary_facets(TT,TF);
+
+  if(TF.size() == 0)
+  {
+    boundary_facets(TT,TF);
+  }
+
   return 0;
 }
 
@@ -73,274 +101,18 @@ IGL_INLINE int igl::copyleft::tetgen::tetrahedralize(
   Eigen::PlainObjectBase<DerivedTT>& TT,
   Eigen::PlainObjectBase<DerivedTF>& TF)
 {
-  using namespace std;
-  vector<vector<REAL> > vV,vTV;
-  vector<vector<int> > vF,vTT,vTF;
-  matrix_to_list(V,vV);
-  matrix_to_list(F,vF);
-  int e = tetrahedralize(vV,vF,switches,vTV,vTT,vTF);
-  if(e == 0)
-  {
-    bool TV_rect = list_to_matrix(vTV,TV);
-    if(!TV_rect)
-    {
-      return 3;
-    }
-    bool TT_rect = list_to_matrix(vTT,TT);
-    if(!TT_rect)
-    {
-      return 3;
-    }
-    bool TF_rect = list_to_matrix(vTF,TF);
-    if(!TF_rect)
-    {
-      return 3;
-    }
-  }
-  return e;
-}
-
-template <
-  typename DerivedV,
-  typename DerivedF,
-  typename DerivedVM,
-  typename DerivedFM,
-  typename DerivedTV,
-  typename DerivedTT,
-  typename DerivedTF,
-  typename DerivedTM>
-IGL_INLINE int igl::copyleft::tetgen::tetrahedralize(
-  const Eigen::MatrixBase<DerivedV>& V,
-  const Eigen::MatrixBase<DerivedF>& F,
-  const Eigen::MatrixBase<DerivedVM>& VM,
-  const Eigen::MatrixBase<DerivedFM>& FM,
-  const std::string switches,
-  Eigen::PlainObjectBase<DerivedTV>& TV,
-  Eigen::PlainObjectBase<DerivedTT>& TT,
-  Eigen::PlainObjectBase<DerivedTF>& TF,
-  Eigen::PlainObjectBase<DerivedTM>& TM)
-{
-  using namespace std;
-  vector<vector<REAL> > vV,vTV;
-  vector<vector<int> > vF,vTT,vTF;
-  vector<int> vTM;
-
-  matrix_to_list(V,vV);
-  matrix_to_list(F,vF);
-  vector<int> vVM = matrix_to_list(VM);
-  vector<int> vFM = matrix_to_list(FM);
-  int e = tetrahedralize(vV,vF,vVM,vFM,switches,vTV,vTT,vTF,vTM);
-  if(e == 0)
-  {
-    bool TV_rect = list_to_matrix(vTV,TV);
-    if(!TV_rect)
-    {
-      return false;
-    }
-    bool TT_rect = list_to_matrix(vTT,TT);
-    if(!TT_rect)
-    {
-      return false;
-    }
-    bool TF_rect = list_to_matrix(vTF,TF);
-    if(!TF_rect)
-    {
-      return false;
-    }
-    bool TM_rect = list_to_matrix(vTM,TM);
-    if(!TM_rect)
-    {
-      return false;
-    }
-  }
-  return e;
-}
-
-IGL_INLINE int igl::copyleft::tetgen::tetrahedralize(
-  const std::vector<std::vector<REAL > > & V,
-  const std::vector<std::vector<int> > & F,
-  const std::vector<int> & VM,
-	const std::vector<int> & FM,
-  const std::string switches,
-  std::vector<std::vector<REAL > > & TV,
-  std::vector<std::vector<int > > & TT,
-  std::vector<std::vector<int> > & TF,
-  std::vector<int> & TM)
-{
-  using namespace std;
-  tetgenio in,out;
-  bool success;
-  success = mesh_to_tetgenio(V,F,in);
-  if(!success)
-  {
-    return -1;
-  }
-	in.pointmarkerlist = new int[VM.size()];
-	for (int i = 0; i < VM.size(); ++i) {
-		in.pointmarkerlist[i] = VM[i];
-	}
-  // These have already been created in mesh_to_tetgenio.
-  // Reset them here.
-	for (int i = 0; i < FM.size(); ++i) {
-		in.facetmarkerlist[i] = FM[i];
-	}
-  try
-  {
-    char * cswitches = new char[switches.size() + 1];
-    std::strcpy(cswitches,switches.c_str());
-    ::tetrahedralize(cswitches,&in, &out);
-    delete[] cswitches;
-  }catch(int e)
-  {
-    cerr<<"^"<<__FUNCTION__<<": TETGEN CRASHED... KABOOOM!!!"<<endl;
-    return 1;
-  }
-  if(out.numberoftetrahedra == 0)
-  {
-    cerr<<"^"<<__FUNCTION__<<": Tetgen failed to create tets"<<endl;
-    return 2;
-  }
-  success = tetgenio_to_tetmesh(out,TV,TT,TF);
-  if(!success)
-  {
-    return -1;
-  }
-	TM.resize(out.numberofpoints);
-	for (int i = 0; i < out.numberofpoints; ++i) {
-		TM[i] = out.pointmarkerlist[i];
-	}
-  boundary_facets(TT,TF);
-  return 0;
-}
-
-IGL_INLINE int igl::copyleft::tetgen::tetrahedralize(
-  const std::vector<std::vector<REAL > > & V,
-  const std::vector<std::vector<int> > & F,
-  const std::vector<std::vector<REAL > > & H,
-  const std::vector<std::vector<REAL > > & R,
-  const std::string switches,
-  std::vector<std::vector<REAL > > & TV,
-  std::vector<std::vector<int > > & TT,
-  std::vector<std::vector<int > > & TF,
-  std::vector<std::vector<REAL > > &TR,
-  std::vector<std::vector<int > > & TN, 
-  std::vector<std::vector<int > > & PT,
-  std::vector<std::vector<int > > & FT,
-  size_t & numRegions)
-{
-  using namespace std;
-  tetgenio in,out;
-  bool success;
-  success = mesh_to_tetgenio(V, F, H, R, in);
-  if(!success)
-  {
-    return -1;
-  }
-  try 
-  {
-    char * cswitches = new char[switches.size() + 1];
-    strcpy(cswitches, switches.c_str());
-    
-    ::tetrahedralize(cswitches, &in, &out); 
-    delete[] cswitches;
-}catch(int e)
-  {
-    cerr <<"^"<<__FUNCTION__<<": TETGEN CRASHED...KABOOM!!"<<endl;
-    return 1;
-  }
-  if(out.numberoftetrahedra == 0)
-  {
-    cerr<<"^"<<__FUNCTION__<<": Tetgen failed to create tets"<<endl;
-    return 2;	  
-  }
-   success = tetgenio_to_tetmesh(out, TV, TT, TF, TR, TN, PT, FT, numRegions
-);
-  if(!success)
-  {
-    return -1;
-  }
-    return 0;
-}
-
-template <
-  typename DerivedV, 
-  typename DerivedF, 
-  typename DerivedH,
-  typename DerivedR,
-  typename DerivedTV, 
-  typename DerivedTT, 
-  typename DerivedTF,
-  typename DerivedTR>
-IGL_INLINE int igl::copyleft::tetgen::tetrahedralize(
-  const Eigen::MatrixBase<DerivedV>& V,
-  const Eigen::MatrixBase<DerivedF>& F,
-  const Eigen::MatrixBase<DerivedH>& H,
-  const Eigen::MatrixBase<DerivedR>& R,
-  const std::string switches,
-  Eigen::PlainObjectBase<DerivedTV>& TV,
-  Eigen::PlainObjectBase<DerivedTT>& TT,
-  Eigen::PlainObjectBase<DerivedTF>& TF,
-  Eigen::PlainObjectBase<DerivedTR>& TR,
-  Eigen::PlainObjectBase<DerivedTT>& TN,
-  Eigen::PlainObjectBase<DerivedTT>& PT,
-  Eigen::PlainObjectBase<DerivedTT>& FT,
-  size_t & numRegions)
-{
-  using namespace std;
-  vector<vector<REAL> > vV, vH, vR, vTV, vTR;
-  vector<vector<int> > vF,vTT,vTF, vTN, vPT, vFT;
-  matrix_to_list(V,vV);
-  matrix_to_list(F,vF);	
-  matrix_to_list(H, vH);
-  matrix_to_list(R, vR);
-  
-  int e = tetrahedralize(vV,vF,vH,vR,switches,vTV,vTT,vTF,vTR,vTN,vPT,vFT, numRegions);
-  
-  if(e == 0)
-  {
-    bool TV_rect = list_to_matrix(vTV,TV);
-    if(!TV_rect)
-    {
-      return 3;
-    }
-    bool TT_rect = list_to_matrix(vTT,TT);
-    if(!TT_rect)
-    {
-      return 3;
-    }
-    bool TF_rect = list_to_matrix(vTF,TF);
-    if(!TF_rect)
-    {
-      return 3;
-    }
-    bool TR_rect = list_to_matrix(vTR, TR);
-    if(!TR_rect)
-    {
-      return 3;	    
-    }
-    bool TN_rect = list_to_matrix(vTN, TN);
-    if(!TN_rect)
-    {
-      return 3;	
-    }	    
-    bool PT_rect = list_to_matrix(vPT, PT);
-    if(!PT_rect)
-    {
-      return 3;	    
-    }	    
-    bool FT_rect = list_to_matrix(vFT, FT);
-    if(!FT_rect)
-    {
-      return 3;	    
-    }	    
-  }
-  return e;
+  Eigen::VectorXi VM,FM;
+  Eigen::MatrixXd H,R;
+  Eigen::VectorXi TM,TR,PT;
+  Eigen::MatrixXi FT,TN;
+  int numRegions;
+  return tetrahedralize(V,F,H,VM,FM,R,switches,TV,TT,TF,TM,TR,TN,PT,FT,numRegions);
 }
 
 #ifdef IGL_STATIC_LIBRARY
 // Explicit template instantiation
-template int igl::copyleft::tetgen::tetrahedralize<Eigen::Matrix<double, -1, -1, 0, -1, -1>, Eigen::Matrix<int, -1, -1, 0, -1, -1>, Eigen::Matrix<double, -1, -1, 0, -1, -1>, Eigen::Matrix<int, -1, -1, 0, -1, -1>, Eigen::Matrix<int, -1, -1, 0, -1, -1> >(Eigen::MatrixBase<Eigen::Matrix<double, -1, -1, 0, -1, -1> > const&, Eigen::MatrixBase<Eigen::Matrix<int, -1, -1, 0, -1, -1> > const&, std::basic_string<char, std::char_traits<char>, std::allocator<char> >, Eigen::PlainObjectBase<Eigen::Matrix<double, -1, -1, 0, -1, -1> >&, Eigen::PlainObjectBase<Eigen::Matrix<int, -1, -1, 0, -1, -1> >&, Eigen::PlainObjectBase<Eigen::Matrix<int, -1, -1, 0, -1, -1> >&);
-template int igl::copyleft::tetgen::tetrahedralize<Eigen::Matrix<double, -1, -1, 0, -1, -1>,Eigen::Matrix<int, -1, -1, 0, -1, -1>,Eigen::Matrix<int, -1, 1, 0, -1, 1>,Eigen::Matrix<int, -1, 1, 0, -1, 1>,Eigen::Matrix<double, -1, -1, 0, -1, -1>,Eigen::Matrix<int, -1, -1, 0, -1, -1>,Eigen::Matrix<int, -1, -1, 0, -1, -1>,Eigen::Matrix<int, -1, 1, 0, -1, 1> >(const Eigen::MatrixBase<Eigen::Matrix<double, -1, -1, 0, -1, -1> > &,const Eigen::MatrixBase<Eigen::Matrix<int, -1, -1, 0, -1, -1> > &,const Eigen::MatrixBase<Eigen::Matrix<int, -1, 1, 0, -1, 1> > &,const Eigen::MatrixBase<Eigen::Matrix<int, -1, 1, 0, -1, 1> > &,const std::basic_string<char, std::char_traits<char>, std::allocator<char> >,Eigen::PlainObjectBase<Eigen::Matrix<double, -1, -1, 0, -1, -1> > &,Eigen::PlainObjectBase<Eigen::Matrix<int, -1, -1, 0, -1, -1> > &,Eigen::PlainObjectBase<Eigen::Matrix<int, -1, -1, 0, -1, -1> > &, Eigen::PlainObjectBase<Eigen::Matrix<int, -1, 1, 0, -1, 1> > &);
-template int igl::copyleft::tetgen::tetrahedralize<Eigen::Matrix<double, -1, -1, 0, -1, -1>, Eigen::Matrix<int, -1, -1, 0, -1, -1>, Eigen::Matrix<int, -1, -1, 0, -1, -1>, Eigen::Matrix<int, -1, -1, 0, -1, -1>, Eigen::Matrix<double, -1, -1, 0, -1, -1>, Eigen::Matrix<int, -1, -1, 0, -1, -1>, Eigen::Matrix<int, -1, -1, 0, -1, -1>, Eigen::Matrix<int, -1, -1, 0, -1, -1> >(Eigen::MatrixBase<Eigen::Matrix<double, -1, -1, 0, -1, -1> > const&, Eigen::MatrixBase<Eigen::Matrix<int, -1, -1, 0, -1, -1> > const&, Eigen::MatrixBase<Eigen::Matrix<int, -1, -1, 0, -1, -1> > const&, Eigen::MatrixBase<Eigen::Matrix<int, -1, -1, 0, -1, -1> > const&, std::basic_string<char, std::char_traits<char>, std::allocator<char> >, Eigen::PlainObjectBase<Eigen::Matrix<double, -1, -1, 0, -1, -1> >&, Eigen::PlainObjectBase<Eigen::Matrix<int, -1, -1, 0, -1, -1> >&, Eigen::PlainObjectBase<Eigen::Matrix<int, -1, -1, 0, -1, -1> >&, Eigen::PlainObjectBase<Eigen::Matrix<int, -1, -1, 0, -1, -1> >&);
-template int igl::copyleft::tetgen::tetrahedralize<Eigen::Matrix<double, -1, -1, 0, -1, -1>, Eigen::Matrix<int, -1, -1, 0, -1, -1>, Eigen::Matrix<double, -1, -1, 0, -1, -1>, Eigen::Matrix<double, -1, -1, 0, -1, -1>, Eigen::Matrix<double, -1, -1, 0, -1, -1>, Eigen::Matrix<int, -1, -1, 0, -1, -1>, Eigen::Matrix<int, -1, -1, 0, -1, -1>, Eigen::Matrix<double, -1, -1, 0, -1, -1> >(Eigen::MatrixBase<Eigen::Matrix<double, -1, -1, 0, -1, -1> > const&, Eigen::MatrixBase<Eigen::Matrix<int, -1, -1, 0, -1, -1> > const&, Eigen::MatrixBase<Eigen::Matrix<double, -1, -1, 0, -1, -1> > const&, Eigen::MatrixBase<Eigen::Matrix<double, -1, -1, 0, -1, -1> > const&, std::basic_string<char, std::char_traits<char>, std::allocator<char> >, Eigen::PlainObjectBase<Eigen::Matrix<double, -1, -1, 0, -1, -1> >&, Eigen::PlainObjectBase<Eigen::Matrix<int, -1, -1, 0, -1, -1> >&, Eigen::PlainObjectBase<Eigen::Matrix<int, -1, -1, 0, -1, -1> >&, Eigen::PlainObjectBase<Eigen::Matrix<double, -1, -1, 0, -1, -1> >&, Eigen::PlainObjectBase<Eigen::Matrix<int, -1, -1, 0, -1, -1> >&, Eigen::PlainObjectBase<Eigen::Matrix<int, -1, -1, 0, -1, -1> >&, Eigen::PlainObjectBase<Eigen::Matrix<int, -1, -1, 0, -1, -1> >&, unsigned long&);
+// generated by autoexplicit.sh
+template int igl::copyleft::tetgen::tetrahedralize<Eigen::Matrix<double, -1, -1, 0, -1, -1>, Eigen::Matrix<int, -1, -1, 0, -1, -1>, Eigen::Matrix<double, -1, -1, 0, -1, -1>, Eigen::Matrix<int, -1, -1, 0, -1, -1>, Eigen::Matrix<int, -1, -1, 0, -1, -1>>(Eigen::MatrixBase<Eigen::Matrix<double, -1, -1, 0, -1, -1>> const&, Eigen::MatrixBase<Eigen::Matrix<int, -1, -1, 0, -1, -1>> const&, std::basic_string<char, std::char_traits<char>, std::allocator<char>>, Eigen::PlainObjectBase<Eigen::Matrix<double, -1, -1, 0, -1, -1>>&, Eigen::PlainObjectBase<Eigen::Matrix<int, -1, -1, 0, -1, -1>>&, Eigen::PlainObjectBase<Eigen::Matrix<int, -1, -1, 0, -1, -1>>&);
+// generated by autoexplicit.sh
+template int igl::copyleft::tetgen::tetrahedralize<Eigen::Matrix<double, -1, -1, 0, -1, -1>, Eigen::Matrix<int, -1, -1, 0, -1, -1>, Eigen::Matrix<double, -1, -1, 0, -1, -1>, Eigen::Matrix<int, -1, 1, 0, -1, 1>, Eigen::Matrix<int, -1, 1, 0, -1, 1>, Eigen::Matrix<double, -1, -1, 0, -1, -1>, Eigen::Matrix<double, -1, -1, 0, -1, -1>, Eigen::Matrix<int, -1, -1, 0, -1, -1>, Eigen::Matrix<int, -1, -1, 0, -1, -1>, Eigen::Matrix<int, -1, 1, 0, -1, 1>, Eigen::Matrix<int, -1, 1, 0, -1, 1>, Eigen::Matrix<int, -1, -1, 0, -1, -1>, Eigen::Matrix<int, -1, 1, 0, -1, 1>, Eigen::Matrix<int, -1, -1, 0, -1, -1>>(Eigen::MatrixBase<Eigen::Matrix<double, -1, -1, 0, -1, -1>> const&, Eigen::MatrixBase<Eigen::Matrix<int, -1, -1, 0, -1, -1>> const&, Eigen::MatrixBase<Eigen::Matrix<double, -1, -1, 0, -1, -1>> const&, Eigen::MatrixBase<Eigen::Matrix<int, -1, 1, 0, -1, 1>> const&, Eigen::MatrixBase<Eigen::Matrix<int, -1, 1, 0, -1, 1>> const&, Eigen::MatrixBase<Eigen::Matrix<double, -1, -1, 0, -1, -1>> const&, std::basic_string<char, std::char_traits<char>, std::allocator<char>>, Eigen::PlainObjectBase<Eigen::Matrix<double, -1, -1, 0, -1, -1>>&, Eigen::PlainObjectBase<Eigen::Matrix<int, -1, -1, 0, -1, -1>>&, Eigen::PlainObjectBase<Eigen::Matrix<int, -1, -1, 0, -1, -1>>&, Eigen::PlainObjectBase<Eigen::Matrix<int, -1, 1, 0, -1, 1>>&, Eigen::PlainObjectBase<Eigen::Matrix<int, -1, 1, 0, -1, 1>>&, Eigen::PlainObjectBase<Eigen::Matrix<int, -1, -1, 0, -1, -1>>&, Eigen::PlainObjectBase<Eigen::Matrix<int, -1, 1, 0, -1, 1>>&, Eigen::PlainObjectBase<Eigen::Matrix<int, -1, -1, 0, -1, -1>>&, int&);
 #endif
--- include/igl/copyleft/tetgen/tetrahedralize.h
+++ include/igl/copyleft/tetgen/tetrahedralize.h
@@ -28,6 +28,8 @@ namespace igl
       /// @param[in] V  #V by 3 vertex position list
       /// @param[in] F  #F list of polygon face indices into V (0-indexed)
       /// @param[in] H  #H by 3 list of seed points inside holes
+      /// @param[in] VM  #VM list of vertex markers
+      /// @param[in] FM  #FM list of face markers
       /// @param[in] R  #R by 5 list of region attributes            
       /// @param[in] switches  string of tetgen options (See tetgen documentation) e.g.
       ///     "pq1.414a0.01" tries to mesh the interior of a given surface with
@@ -35,46 +37,57 @@ namespace igl
       ///     "" will mesh the convex hull constrained to pass through V (ignores F)
       /// @param[out] TV  #TV by 3 vertex position list
       /// @param[out] TT  #TT by 4 list of tet face indices
-      /// @param[out] TF  #TF by 3 list of triangle face indices
+      /// @param[out] TF  #TF by 3 list of triangle face indices ('f', else
+      ///   `boundary_facets` is called on TT)
       /// @param[out] TR  #TT list of region ID for each tetrahedron      
-      /// @param[out] TN  #TT by 4 list of indices neighbors for each tetrahedron
-      /// @param[out] PT  #TV list of incident tetrahedron for a vertex
-      /// @param[out] FT  #TF by 2 list of tetrahedrons sharing a triface      
-      /// @param[out] numRegions Number of regions in output mesh
+      /// @param[out] TN  #TT by 4 list of indices neighbors for each tetrahedron ('n')
+      /// @param[out] PT  #TV list of incident tetrahedron for a vertex ('m')
+      /// @param[out] FT  #TF by 2 list of tetrahedrons sharing a triface ('nn')
+      /// @param[out] num_regions Number of regions in output mesh
       /// @return status:
       ///   0 success
       ///   1 tetgen threw exception
       ///   2 tetgen did not crash but could not create any tets (probably there are
       ///     holes, duplicate faces etc.)
       ///   -1 other error
+      template <
+        typename DerivedV,
+        typename DerivedF,
+        typename DerivedH,
+        typename DerivedVM,
+        typename DerivedFM,
+        typename DerivedR,
+        typename DerivedTV,
+        typename DerivedTT,
+        typename DerivedTF,
+        typename DerivedTM,
+        typename DerivedTR,
+        typename DerivedTN,
+        typename DerivedPT,
+        typename DerivedFT>
       IGL_INLINE int tetrahedralize(
-        const std::vector<std::vector<REAL> > &V, 
-        const std::vector<std::vector<int> >  &F, 
-        const std::vector<std::vector<REAL> > &H, 
-        const std::vector<std::vector<REAL> > &R, 
-        const std::string switches, 
-        std::vector<std::vector<REAL > > & TV,
-        std::vector<std::vector<int > >  & TT,
-        std::vector<std::vector<int > >  & TF,
-        std::vector<std::vector<REAL > > &TR,  
-        std::vector<std::vector<int > > &TN, 
-        std::vector<std::vector<int > > &PT, 
-        std::vector<std::vector<int > > &FT, 
-        size_t & numRegions);           
-      /// \overload
-      IGL_INLINE int tetrahedralize(
-        const std::vector<std::vector<REAL > > & V, 
-        const std::vector<std::vector<int> > & F, 
+        const Eigen::MatrixBase<DerivedV>& V,
+        const Eigen::MatrixBase<DerivedF>& F,
+        const Eigen::MatrixBase<DerivedH>& H,
+        const Eigen::MatrixBase<DerivedVM>& VM,
+        const Eigen::MatrixBase<DerivedFM>& FM,
+        const Eigen::MatrixBase<DerivedR>& R,
         const std::string switches,
-        std::vector<std::vector<REAL > > & TV, 
-        std::vector<std::vector<int > > & TT, 
-        std::vector<std::vector<int> > & TF);
+        Eigen::PlainObjectBase<DerivedTV>& TV,
+        Eigen::PlainObjectBase<DerivedTT>& TT,
+        Eigen::PlainObjectBase<DerivedTF>& TF,
+        Eigen::PlainObjectBase<DerivedTM>& TM,
+        Eigen::PlainObjectBase<DerivedTR>& TR, 
+        Eigen::PlainObjectBase<DerivedTN>& TN, 
+        Eigen::PlainObjectBase<DerivedPT>& PT, 
+        Eigen::PlainObjectBase<DerivedFT>& FT, 
+        int & num_regions);
       /// \overload
       template <
-        typename DerivedV, 
-        typename DerivedF, 
-        typename DerivedTV, 
-        typename DerivedTT, 
+        typename DerivedV,
+        typename DerivedF,
+        typename DerivedTV,
+        typename DerivedTT,
         typename DerivedTF>
       IGL_INLINE int tetrahedralize(
         const Eigen::MatrixBase<DerivedV>& V,
@@ -83,61 +96,6 @@ namespace igl
         Eigen::PlainObjectBase<DerivedTV>& TV,
         Eigen::PlainObjectBase<DerivedTT>& TT,
         Eigen::PlainObjectBase<DerivedTF>& TF);
-      /// \overload
-      IGL_INLINE int tetrahedralize(
-        const std::vector<std::vector<REAL > > & V, 
-        const std::vector<std::vector<int> > & F, 
-        const std::vector<int> & VM,
-        const std::vector<int> & FM,
-        const std::string switches,
-        std::vector<std::vector<REAL > > & TV, 
-        std::vector<std::vector<int > > & TT, 
-        std::vector<std::vector<int> > & TF,
-        std::vector<int> & TM);
-      /// \overload
-      template <
-        typename DerivedV, 
-        typename DerivedF, 
-        typename DerivedVM,
-        typename DerivedFM,
-        typename DerivedTV, 
-        typename DerivedTT, 
-        typename DerivedTF, 
-        typename DerivedTM>
-      IGL_INLINE int tetrahedralize(
-        const Eigen::MatrixBase<DerivedV>& V,
-        const Eigen::MatrixBase<DerivedF>& F,
-        const Eigen::MatrixBase<DerivedVM>& VM,
-        const Eigen::MatrixBase<DerivedFM>& FM,
-        const std::string switches,
-        Eigen::PlainObjectBase<DerivedTV>& TV,
-        Eigen::PlainObjectBase<DerivedTT>& TT,
-        Eigen::PlainObjectBase<DerivedTF>& TF,
-        Eigen::PlainObjectBase<DerivedTM>& TM);
-      /// \overload
-      template <
-        typename DerivedV,
-        typename DerivedF,
-        typename DerivedH,
-        typename DerivedR,
-        typename DerivedTV,
-        typename DerivedTT,
-        typename DerivedTF,
-        typename DerivedTR>      
-      IGL_INLINE int tetrahedralize(
-        const Eigen::MatrixBase<DerivedV>& V,
-        const Eigen::MatrixBase<DerivedF>& F,
-        const Eigen::MatrixBase<DerivedH>& H,
-        const Eigen::MatrixBase<DerivedR>& R,
-        const std::string switches,
-        Eigen::PlainObjectBase<DerivedTV>& TV,
-        Eigen::PlainObjectBase<DerivedTT>& TT,
-        Eigen::PlainObjectBase<DerivedTF>& TF,
-        Eigen::PlainObjectBase<DerivedTR>& TR, 
-        Eigen::PlainObjectBase<DerivedTT>& TN, 
-        Eigen::PlainObjectBase<DerivedTT>& PT, 
-        Eigen::PlainObjectBase<DerivedTT>& FT, 
-        size_t & numRegions);            
    }
   }
 }
