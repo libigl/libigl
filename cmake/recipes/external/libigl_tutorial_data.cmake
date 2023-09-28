3a8936585d4cbbbb86267f260f8bde60102c6763
Alec Jacobson <alecjacobson@adobe.com>
2023-09-28 11:23:43 -0400

    tata -> data

--- cmake/recipes/external/libigl_tutorial_data.cmake
+++ cmake/recipes/external/libigl_tutorial_data.cmake
@@ -6,13 +6,13 @@ message(STATUS "Third-party: creating target 'igl::tutorial_data'")
 
 include(FetchContent)
 FetchContent_Declare(
-    libigl_tutorial_tata
+    libigl_tutorial_data
     GIT_REPOSITORY https://github.com/libigl/libigl-tutorial-data
     GIT_TAG        c1f9ede366d02e3531ecbaec5e3769312f31cccd
 )
-FetchContent_MakeAvailable(libigl_tutorial_tata)
+FetchContent_MakeAvailable(libigl_tutorial_data)
 
 add_library(igl_tutorial_data INTERFACE)
 add_library(igl::tutorial_data ALIAS igl_tutorial_data)
 
-target_compile_definitions(igl_tutorial_data INTERFACE "-DTUTORIAL_SHARED_PATH=\"${libigl_tutorial_tata_SOURCE_DIR}\"")
+target_compile_definitions(igl_tutorial_data INTERFACE "-DTUTORIAL_SHARED_PATH=\"${libigl_tutorial_data_SOURCE_DIR}\"")
