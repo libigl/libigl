7991ee81d81bd8a58df261f2b6cc782a77d57ec2
Alec Jacobson <alecjacobson@gmail.com>
2023-09-28 13:12:57 -0400

    tata -> data (#2295) [ci skip]
    Co-authored-by: Alec Jacobson <alecjacobson@adobe.com>

--- cmake/recipes/external/libigl_tests_data.cmake
+++ cmake/recipes/external/libigl_tests_data.cmake
@@ -6,13 +6,13 @@ message(STATUS "Third-party: creating target 'igl::tests_data'")
 
 include(FetchContent)
 FetchContent_Declare(
-    libigl_tests_tata
+    libigl_tests_data
     GIT_REPOSITORY https://github.com/libigl/libigl-tests-data
     GIT_TAG        19cedf96d70702d8b3a83eb27934780c542356fe
 )
-FetchContent_MakeAvailable(libigl_tests_tata)
+FetchContent_MakeAvailable(libigl_tests_data)
 
 add_library(igl_tests_data INTERFACE)
 add_library(igl::tests_data ALIAS igl_tests_data)
 
-target_compile_definitions(igl_tests_data INTERFACE LIBIGL_DATA_DIR=\"${libigl_tests_tata_SOURCE_DIR}\")
+target_compile_definitions(igl_tests_data INTERFACE LIBIGL_DATA_DIR=\"${libigl_tests_data_SOURCE_DIR}\")
