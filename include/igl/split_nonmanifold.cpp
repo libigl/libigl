57feaa7ee1b270011aea49e1ccc6cc03212d9058
Alec Jacobson <alecjacobson@adobe.com>
2024-01-21 12:46:18 -0500

    before rewrite

--- include/igl/split_nonmanifold.cpp
+++ include/igl/split_nonmanifold.cpp
@@ -8,6 +8,7 @@
 #include "split_nonmanifold.h"
 #include "unique_edge_map.h"
 #include "connected_components.h"
+#include "triangle_triangle_adjacency.h"
 #include <cassert>
 #include <type_traits>
 
@@ -40,6 +41,21 @@ IGL_INLINE void igl::split_nonmanifold(
   // Mesh as if all edges got cut. 
   MatrixX3I CF = VectorXI::LinSpaced(F.size(),0,F.size()-1).reshaped(F.rows(),F.cols());
 
+  // Every edge starts as a boundary edge
+  const int m = F.rows();
+  // augh, this is following gptoolbox ordering not triangle_triangle_adjacency
+  // ordering.
+  MatrixX3I CTF = MatrixX3I::Constant(m,3,-1);
+  MatrixX3I CTI = MatrixX3I::Constant(m,3,-1);
+
+  // Is maintaining triangle adjacency enough? Or do we need edge-flaps?
+  // After we zip an edge-pair we need to know if there are other unzipped edges
+  // adjacent to the zipped edge.
+  // Triangle adjacency seems like enough to circulate around the zipped edge
+  // vertices and find incident boundary edges, which are candidates for
+  // zipping. From a candidate boundary edge we can get to EMAP, which should be
+  // enough to find candidate zip-pairs using uE*.
+
   //// Empty edge-flap data
   //MatrixX2I CEF= MatrixX2I::Constant(E.rows(),2,-1);
   //MatrixX2I CEI= MatrixX2I::Constant(E.rows(),2,-1);
@@ -55,8 +71,8 @@ IGL_INLINE void igl::split_nonmanifold(
 
   std::vector<std::pair<int,int> > R;
 
-  const int m = F.rows();
-  const auto zip = [&E,&m,&seen,&F,&CF,&R](const int e1, const int e2)
+  const auto zip = 
+    [&E,&m,&seen,&F,&CF,&R,&CTF,&CTI](const int e1, const int e2, const bool verbose = true)
   {
     assert(!seen[e1]);
     assert(!seen[e2]);
@@ -76,9 +92,12 @@ IGL_INLINE void igl::split_nonmanifold(
     const int cs2 = CF(f2,(i2+2)%3);
     const int cd1 = CF(f1,(i1+2)%3);
     const int cd2 = CF(f2,(i2+1)%3);
+    if(verbose)
+    {
     //printf("%d,%d → %d,%d\n",vs1+1,vd1+1,vs2+1,vd2+1);
     //printf("  %d,%d → %d,%d\n",cs1+1,cd1+1,cs2+1,cd2+1);
-    //printf("  %d → %d\n",f1+1,f2+1);
+    printf("  %d → %d\n",f1+1,f2+1);
+    }
 
     const int cs = std::min(cs1,cs2);
     const int cd = std::min(cd1,cd2);
@@ -90,6 +109,13 @@ IGL_INLINE void igl::split_nonmanifold(
     R.emplace_back(cs2,cs);
     R.emplace_back(cd2,cd);
 
+    CTF(f1,i1) = f2;
+    CTI(f1,i1) = i2;
+    CTF(f2,i2) = f1;
+    CTI(f2,i2) = i1;
+
+    seen[e1] = true;
+    seen[e2] = true;
 
     //std::cout<<igl::matlab_format_index(CF,"CF")<<std::endl;
     //printf("\n");
@@ -117,14 +143,45 @@ IGL_INLINE void igl::split_nonmanifold(
       if(enforce_orientability && E(e1,0) == E(e2,1))
       {
         assert(E(e1,1) == E(e2,0));
-        zip(e1,e2);
-        seen[e1] = true;
-        seen[e2] = true;
+        zip(e1,e2,false);
 #warning "is `seen` every used?"
         continue;
       }
     }// else. non-manifold. Skip for now
   }
+
+  // Cutting and Stitching: Converting Sets of Polygons to Manifold Surfaces
+
+  // Outer loop over all unique edges.
+  for(int u = 0;u<uE.rows();u++)
+  {
+    const int num_incident = uEC(u+1)-uEC(u);
+    assert(num_incident > 0);
+    // boundary or internal edge handled already.
+    if(num_incident <= 2) { continue; }
+    for(int j = uEC(u);j<uEC(u+1);j++)
+    {
+      const int e1 = uEE(j); // i = j-uEC(u);
+      if(seen[e1]) { continue; }
+      // We haven't seen this half-edge yet.
+      // Try to zip it with an opposite half-edge (and start off a cascade of
+      // zipping).
+      for(int k = j+1;k<uEC(u+1);k++)
+      {
+        const int e2 = uEE(k);
+        // Can't zip if alread seen
+        if(seen[e2]) { continue; }
+        if(enforce_orientability && E(e1,0) == E(e2,1))
+        {
+          assert(E(e1,1) == E(e2,0));
+          zip(e1,e2);
+          // Don't try any more zips with e1
+          break;
+        }
+      }
+    }
+  }
+  
 
   // Now if we resolve all the equivalence records we'd have a mesh CF which is
   // "cut" along all original non-manifold edges (and non-manifold vertices
@@ -173,7 +230,26 @@ IGL_INLINE void igl::split_nonmanifold(
       CF(i) = C(CF(i));
     }
   }
-  
+
+  //{
+  //  // Did we correctly maintain CTF and CTI?
+  //  MatrixX3I CTF2,CTI2;
+  //  std::cout<<igl::matlab_format_index(CF,"CF")<<std::endl;
+  //  igl::triangle_triangle_adjacency(CF,CTF2,CTI2);
+
+  //  {
+  //    Eigen::PermutationMatrix<3,3> perm(3);
+  //    perm.indices() = Eigen::Vector3i(1,2,0);
+  //    CTF2 = (CTF2*perm).eval();
+  //    CTI2 = (CTI2*perm).eval();
+  //    for(int i=0;i<CTI2.rows();i++)
+  //      for(int j=0;j<CTI2.cols();j++)
+  //        CTI2(i,j)=CTI2(i,j)==-1?-1:(CTI2(i,j)+3-1)%3;
+  //  }
+
+  //  assert((CTF.array() == CTF2.array()).all());
+  //  assert((CTI.array() == CTI2.array()).all());
+  //}
   //std::cout<<igl::matlab_format_index(CF,"CF")<<std::endl;
 
 
