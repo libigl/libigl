44376e888ae31127c56afb4228c9c28ce526b824
Alec Jacobson <alecjacobson@gmail.com>
2024-04-27 10:20:39 -0400

    wip predicates

---
+++ include/igl/predicates/triangle_triangle_intersect.h
@@ -0,0 +1,23 @@
+#ifndef IGL_PREDICATES_TRIANGLE_TRIANGLE_INTERSECT_H
+#define IGL_PREDICATES_TRIANGLE_TRIANGLE_INTERSECT_H
+
+namespace igl
+{
+  namespace predicates
+  {
+    template <typename Vector3D>
+    bool triangle_triangle_intersect(
+      const Vector3D & a1,
+      const Vector3D & a2,
+      const Vector3D & a3,
+      const Vector3D & b1,
+      const Vector3D & b2,
+      const Vector3D & b3);
+  }
+}
+
+#ifndef IGL_STATIC_LIBRARY
+#include "triangle_triangle_intersect.cpp"
+#endif
+
+#endif 
