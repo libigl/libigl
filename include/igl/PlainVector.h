c712110fc40154ad134834a2ba18e380a4436ee0
Alec Jacobson <alecjacobson@gmail.com>
2024-11-09 21:50:40 -0500

    options

--- include/igl/PlainVector.h
+++ include/igl/PlainVector.h
@@ -9,23 +9,27 @@
 #ifndef IGL_PLAINVECTOR_H
 #define IGL_PLAINVECTOR_H
 #include <Eigen/Core>
+#include "PlainMatrix.h"
 
 namespace igl
 {
   // PlainVectorHelper to determine correct matrix type based on Derived and Size
-  template <typename Derived, int Size>
+  template <typename Derived, int Size, int Options>
     struct PlainVectorHelper {
       // Conditional Type: Column vector if is_column_vector is true, otherwise row vector
       using Type = Eigen::Matrix<
         typename Derived::Scalar,
         (Derived::ColsAtCompileTime == 1 && Derived::RowsAtCompileTime != 1) ? Size : 1,
         (Derived::ColsAtCompileTime == 1 && Derived::RowsAtCompileTime != 1) ? 1 : Size,
-        Derived::Options>;
+        Options>;
     };
 
   /// \see PlainMatrix
-  template <typename Derived, int Size = (Derived::ColsAtCompileTime == 1 && Derived::RowsAtCompileTime != 1) ? Derived::RowsAtCompileTime : Derived::ColsAtCompileTime>
-  using PlainVector = typename PlainVectorHelper<Derived, Size>::Type;
+  template <
+    typename Derived, 
+    int Size = (Derived::ColsAtCompileTime == 1 && Derived::RowsAtCompileTime != 1) ? Derived::RowsAtCompileTime : Derived::ColsAtCompileTime,
+    int Options = get_options<Derived>::value>
+  using PlainVector = typename PlainVectorHelper<Derived, Size, Options>::Type;
 
 }
 #endif
