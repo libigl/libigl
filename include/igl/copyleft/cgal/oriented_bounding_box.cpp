3179089fe54b73a7ac85f3d67c9d5c871db9960a
Alec Jacobson <alecjacobson@gmail.com>
2025-05-22 21:54:46 -0400

    wrapper on cgals obb

---
+++ include/igl/copyleft/cgal/oriented_bounding_box.cpp
@@ -0,0 +1,44 @@
+#include "oriented_bounding_box.h"
+#include <CGAL/Aff_transformation_3.h>
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Optimal_bounding_box/oriented_bounding_box.h>
+
+#include <array>
+#include <iostream>
+
+template <typename DerivedP, typename DerivedR>
+  IGL_INLINE void igl::copyleft::cgal::oriented_bounding_box(
+    const Eigen::MatrixBase<DerivedP>& P,
+    Eigen::PlainObjectBase<DerivedR> & R)
+{
+  typedef typename DerivedP::Scalar Scalar;
+  typedef CGAL::Simple_cartesian<Scalar> K;
+  typedef typename K::Point_3 Point;
+  std::vector<Point> points(P.rows());
+  for (int i = 0; i < P.rows(); ++i)
+  {
+    points[i] = Point(P(i, 0), P(i, 1), P(i, 2));
+  }
+
+  std::array<Point, 8> obb_points;
+  CGAL::Aff_transformation_3<K> affine;
+  CGAL::oriented_bounding_box(
+    points, 
+    affine,
+    CGAL::parameters::use_convex_hull(true));
+  // Convert to Eigen affine transformation
+  R.resize(3,3);
+  for(int i = 0; i < 3; ++i)
+  {
+    for (int j = 0; j < 3; ++j)
+    {
+      R(i, j) = affine.m(i, j);
+    }
+  }
+}
+
+#ifdef IGL_STATIC_LIBRARY
+// Explicit instantiation of template function
+template void igl::copyleft::cgal::oriented_bounding_box<Eigen::Matrix<double, -1, -1, 0, -1, -1>, Eigen::Matrix<double, 3, 3, 0, 3, 3>>(Eigen::MatrixBase<Eigen::Matrix<double, -1, -1, 0, -1, -1>> const&, Eigen::PlainObjectBase<Eigen::Matrix<double, 3, 3, 0, 3, 3>>&);
+#endif
+
