73a2e0de9b0bcfac83dc5bb661049c516a73650f
Alec Jacobson <alecjacobson@gmail.com>
2025-06-16 09:47:46 -0400

    split functions into files; add vectorized orient3d (#2479)

--- include/igl/predicates/point_inside_convex_polygon.cpp
+++ include/igl/predicates/point_inside_convex_polygon.cpp
@@ -7,6 +7,7 @@
 // obtain one at http://mozilla.org/MPL/2.0/.
 
 #include "point_inside_convex_polygon.h"
+#include "orient2d.h"
 
 template <typename DerivedP, typename DerivedQ>
 IGL_INLINE bool igl::predicates::point_inside_convex_polygon(
