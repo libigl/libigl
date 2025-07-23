9a76c4ca5dd05224f78409c87594ff9eddcaa188
Alec Jacobson <alecjacobson@gmail.com>
2025-07-23 08:56:54 -0400

    missing include

--- include/igl/predicates/orient2d.cpp
+++ include/igl/predicates/orient2d.cpp
@@ -6,6 +6,7 @@
 // v. 2.0. If a copy of the MPL was not distributed with this file, You can
 // obtain one at http://mozilla.org/MPL/2.0/.
 #include "orient2d.h"
+#include "exactinit.h"
 #include <predicates.h>
 
 namespace igl {
